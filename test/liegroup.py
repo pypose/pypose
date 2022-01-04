221cf22169e50f5887bda6888a0bd1ac103e1c6c
Chen Wang <jeffsan@gmx.com>
2022-01-03 21:15:27 -0500

    return back

--- test/liegroup.py
+++ test/liegroup.py
@@ -4,18 +4,20 @@ import pypose
 from pypose.liegroup.lietorch import SO3
 
 # print(lietorch.__version__)
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
 
 phi = torch.randn(8000, 3, device='cuda', requires_grad=True)
-R = SO3.Exp(phi)
+R = SO3.exp(phi)
 
 # relative rotation matrix, SO3 ^ {8000 x 8000}
-dR = R[:,None].Inv() * R[None,:]
+dR = R[:,None].inv() * R[None,:]
 
 # 8000x8000 matrix of angles
-ang = dR.Log().norm(dim=-1)
+ang = dR.log().norm(dim=-1)
 
 # backpropogation in tangent space
 loss = ang.sum()
 loss.backward()
-print(R.grad)
+print(R.data.grad)
 print(phi.grad)
