e36163ad21901ed3b307a20a7e3f0788ba4d114f
Chen Wang <jeffsan@gmx.com>
2022-01-13 14:39:45 -0500

    add test

---
+++ test/test.py
@@ -0,0 +1,43 @@
+import torch
+from torch import nn
+import pypose as pp
+
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+
+y = pp.randn_se3(6, dtype=torch.float64, requires_grad=True)
+
+(y.Exp().Log()**2).sin().sum().backward()
+
+print(y.shape)
+print(y.grad)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+class SO3Layer(nn.Module):
+    def __init__(self, n):
+        super().__init__()
+        self.weight = pp.Parameter(pp.randn_so3(n, requires_grad=True), pp.so3_type)
+
+    def forward(self, x):
+        return self.weight.Exp() * x
+
+n = 4
+epoch = 10
+net = SO3Layer(n).cuda()
+
+optimizer = torch.optim.SGD(net.parameters(), lr = 0.1, momentum=0.9)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10,15], gamma=0.1)
+
+for i in range(epoch):
+    optimizer.step()
+    scheduler.step()
+    optimizer.zero_grad()
+    inputs = torch.randn(n, 4).to('cuda')
+    outputs = net(inputs)
+    loss = outputs.abs().sum()
+    loss.backward()
+    print(loss)
+
+print("Parameter:", count_parameters(net))
