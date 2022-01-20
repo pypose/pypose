376cd098abf8d8b0637ac5e2cefbdf39a281afa9
Chen Wang <jeffsan@gmx.com>
2022-01-20 01:34:14 -0500

    clean all files

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ setup(
     url = 'pypose.org',
     author_email = 'chenwang@dr.com',
     ext_modules=[
-        CUDAExtension('lietorch_backends', 
+        CUDAExtension('liegroup_backends', 
             include_dirs=[
-                osp.join(ROOT, 'pypose/liegroup/lietorch/include'), 
+                osp.join(ROOT, 'pypose/liegroup/group/include'), 
                 osp.join(ROOT, 'pypose/liegroup/eigen')],
             sources=[
-                'pypose/liegroup/lietorch/src/lietorch.cpp', 
-                'pypose/liegroup/lietorch/src/lietorch_gpu.cu',
-                'pypose/liegroup/lietorch/src/lietorch_cpu.cpp'],
+                'pypose/liegroup/group/src/liegroup.cpp', 
+                'pypose/liegroup/group/src/liegroup_gpu.cu',
+                'pypose/liegroup/group/src/liegroup_cpu.cpp'],
             extra_compile_args={
                 'cxx': ['-O2'], 
                 'nvcc': ['-O2',
@@ -34,14 +34,14 @@ setup(
                 ]
             }),
 
-        CUDAExtension('lietorch_extras', 
+        CUDAExtension('liegroup_extras', 
             sources=[
-                'pypose/liegroup/lietorch/extras/altcorr_kernel.cu',
-                'pypose/liegroup/lietorch/extras/corr_index_kernel.cu',
-                'pypose/liegroup/lietorch/extras/se3_builder.cu',
-                'pypose/liegroup/lietorch/extras/se3_inplace_builder.cu',
-                'pypose/liegroup/lietorch/extras/se3_solver.cu',
-                'pypose/liegroup/lietorch/extras/extras.cpp',
+                'pypose/liegroup/group/extras/altcorr_kernel.cu',
+                'pypose/liegroup/group/extras/corr_index_kernel.cu',
+                'pypose/liegroup/group/extras/se3_builder.cu',
+                'pypose/liegroup/group/extras/se3_inplace_builder.cu',
+                'pypose/liegroup/group/extras/se3_solver.cu',
+                'pypose/liegroup/group/extras/extras.cpp',
             ],
             extra_compile_args={
                 'cxx': ['-O2'], 
