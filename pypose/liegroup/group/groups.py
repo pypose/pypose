fix bug in broadcast shape

pypose/liegroup/group/groups.py
@@ -152,8 +152,8 @@ class GroupType:
             return (x.view(-1, xd).contiguous(), ), x.shape[:-1]
         out_shape = torch.broadcast_shapes(x.shape[:-1], y.shape[:-1])
         shape = out_shape if out_shape != torch.Size([]) else (1,)
-        x = x.expand(shape+(x.shape[-1],)).contiguous()
-        y = y.expand(shape+(y.shape[-1],)).contiguous()
+        x = x.expand(shape+(x.shape[-1],)).reshape(-1,x.shape[-1]).contiguous()
+        y = y.expand(shape+(y.shape[-1],)).reshape(-1,y.shape[-1]).contiguous()
         return (x, y), tuple(out_shape)
 
 