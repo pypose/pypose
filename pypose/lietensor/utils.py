4d38f02132abda68219a9352a2293aff350c963f
jennygeng <junyigeng@gmail.com>
2022-04-28 11:04:14 -0400

    Rephased the description of the algorithm for computing Jinvp^-1 of Sim3

--- pypose/lietensor/utils.py
+++ pypose/lietensor/utils.py
@@ -1174,11 +1174,6 @@ def Jinvp(input, p):
 
     * If input (:math:`\mathbf{x}`, :math:`\mathbf{p}`)'s :obj:`ltype` are :obj:`Sim3_type` and :obj:`sim3_type`
       (input :math:`\mathbf{x}` is an instance of :meth:`Sim3`, :math:`\mathbf{p}` is an instance of :meth:`sim3`).
-      Let :math:`\boldsymbol{X}_i` be the Adjoint matrix of :math:`\mathbf{x}_i`, e.g.,
-      :math:`\boldsymbol{X}_i = \mathrm{Adj}(\mathbf{x}_i)`, :math:`\boldsymbol{\xi}_i` be the corresponding Lie Algebra
-      of :math:`\mathbf{x}_i`. Notate :math:`\boldsymbol{\xi}_i^{\curlywedge} = \mathrm{adj}(\boldsymbol{\xi}_i^{\wedge})`.
-      Notice that :math:`\boldsymbol{\xi}_i` is also the Lie algebra of :math:`\boldsymbol{X}_i`,
-      e.g., :math:`\boldsymbol{X}_i = \mathrm{Exp}(\boldsymbol{\xi}_i^{\curlywedge})`.
       The inverse of left Jacobian can be approximated as:
 
         .. math::
@@ -1186,6 +1181,13 @@ def Jinvp(input, p):
 
         where :math:`B_n` is the Bernoulli number: :math:`B_0 = 1`, :math:`B_1 = -\frac{1}{2}`,
         :math:`B_2 = \frac{1}{6}`, :math:`B_3 = 0`, :math:`B_4 = -\frac{1}{30}`.
+        :math:`\boldsymbol{\xi}_i^{\curlywedge} = \mathrm{adj}(\boldsymbol{\xi}_i^{\wedge})` and :math:`\mathrm{adj}` 
+        is the adjoint of the Lie algebra :math:`\mathfrak{sim}(3)`, e.g., :math:`\boldsymbol{\xi}_i \in \mathfrak{sim}(3)`.
+        Notice that if notate :math:`\boldsymbol{X}_i = \mathrm{Adj}(\mathbf{x}_i)` and :math:`\mathrm{Adj}` 
+        is the adjoint of the Lie group :math:`\mathrm{Sim}(3)`, there is a nice property:
+        :math:`\mathrm{Adj}(\mathrm{Exp}(\boldsymbol{\xi}_i^{\curlywedge})) = \mathrm{Exp}(\mathrm{adj}(\boldsymbol{\xi}_i^{\wedge}))`, 
+        or :math:`\boldsymbol{X}_i = \mathrm{Exp}(\boldsymbol{\xi}_i^{\curlywedge})`.
+        
 
     * If input (:math:`\mathbf{x}`, :math:`\mathbf{p}`)'s :obj:`ltype` are :obj:`RxSO3_type` and :obj:`rxso3_type`
       (input :math:`\mathbf{x}` is an instance of :meth:`RxSO3`, :math:`\mathbf{p}` is an instance of :meth:`rxso3`).
