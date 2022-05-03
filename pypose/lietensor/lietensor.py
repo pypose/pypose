b7856ece2e2e6cb7c256d9cb352c7f25476dd9a7
xukuanHIT <403282859@qq.com>
2022-05-03 08:22:43 +0000

    correct right_jacobian calculation in python version

--- pypose/lietensor/lietensor.py
+++ pypose/lietensor/lietensor.py
@@ -18,6 +18,7 @@ HANDLED_FUNCTIONS = ['__getitem__', '__setitem__', 'cpu', 'cuda', 'float', 'doub
                      'index_select', 'masked_select', 'index_copy', 'index_copy_',
                      'select', 'select_scatter', 'index_put','index_put_']
 
+LIETENSOR_EPS = 1e-6
 
 class LieType:
     '''LieTensor Type Base Class'''
@@ -254,11 +255,12 @@ class so3Type(LieType):
         https://github.com/XueLianjie/BA_schur/blob/3af9a94248d4a272c53cfc7acccea4d0208b77f7/thirdparty/Sophus/sophus/so3.hpp#L113
         """
         I = torch.eye(3, device=x.device, dtype=x.dtype).expand(x.shape[:-1]+(3,3))
-        theta = linalg.norm(x)
-        if theta < 1e-5:
-            return I
-        K = vec2skew(torch.nn.functional.normalize(x, dim=-1))
-        return I - (1-theta.cos())/theta**2 * K + (theta - theta.sin())/linalg.norm(x**3) * K@K
+        theta = torch.linalg.norm(x, dim=-1)
+        theta = theta.unsqueeze(1).unsqueeze(2)
+        K = vec2skew(x)
+        J_larger = I - (1-theta.cos())/theta**2 * K + (theta - theta.sin())/theta**3 * K@K
+        J = torch.where(theta>LIETENSOR_EPS, J_larger, I)
+        return J
 
 
 class SE3Type(LieType):
