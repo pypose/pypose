9999fff84c9525231b59be703677d578edee318d
Chen Wang <jeffsan@gmx.com>
2022-05-09 22:01:31 -0400

    minor fix for (*) dimension

--- pypose/lietensor/utils.py
+++ pypose/lietensor/utils.py
@@ -23,9 +23,9 @@ def _LieTensor_wrapper_add_docstr(wrapper: functools.partial, embedding_doc):
             from ':obj:`int`...', which defines tensor shape.
 
             The shape of :obj:`Tensor` object must be `(*, {type_dim})`,
-            where `*` is zero or more batch dimensions (the
-            :obj:`~LieTensor.lshape` of this LieTensor). Otherwise error will
-            be thrown.
+            where `*` is empty, one, or more batched dimensions (the
+            :obj:`~LieTensor.lshape` of this LieTensor), otherwise error will
+            be raised.
 
     {embedding_doc}
 
