23961d7b725ec4143c99e50ab6074d59f41e7eab
haleqiu <yuhengq@andrew.cmu.edu>
2022-05-13 06:08:07 +0000

    update cumprod doc

--- pypose/lietensor/basics.py
+++ pypose/lietensor/basics.py
@@ -137,6 +137,11 @@ def cummul(input, dim):
 def cumprod(input, dim, left = True):
     r"""Returns the cumulative product (@) of LieTensor along a dimension.
 
+    * Left product:
+    .. math::
+        y_i = x_i ~@~ x_{i-1} ~@~ \cdots ~@~ x_1,
+    
+    * Right product:
     .. math::
         y_i = x_1 ~@~ x_2 ~@~ \cdots ~@~ x_i,
 
@@ -146,6 +151,8 @@ def cumprod(input, dim, left = True):
     Args:
         input (LieTensor): the input LieTensor
         dim (int): the dimension to do the operation over
+        left (bool, optional): whether perform left product in :obj:`cumprod`. /
+            If set it to :obj:`False`, this function performs right product. Defaul: True
 
     Returns:
         LieTensor: The LieTensor
@@ -155,11 +162,20 @@ def cumprod(input, dim, left = True):
           :math:`N` is the LieTensor size along the :obj:`dim` dimension.
 
     Examples:
+
+    * Left product with :math:`input \in` :meth:`SE3`
         >>> input = pp.randn_SE3(2)
         >>> pp.cumprod(input, dim=0)
         SE3Type LieTensor:
         tensor([[-1.9615, -0.1246,  0.3666,  0.0165,  0.2853,  0.3126,  0.9059],
                 [ 0.7139,  1.3988, -0.1909, -0.1780,  0.4405, -0.6571,  0.5852]])
+
+    * Right product with :math:`input \in` :meth:`SO3`
+        >>> input = pp.randn_SO3(1,2)
+        >>> pp.cumprod(input, dim=1, left=False)
+        SO3Type LieTensor:
+        tensor([[[ 0.5798, -0.1189, -0.2429,  0.7686],
+                [ 0.7515, -0.1920,  0.5072,  0.3758]]])
     """
     if left:
         return cumops(input, dim, lambda a, b : a @ b)
