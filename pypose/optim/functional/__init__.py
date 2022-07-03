8a7acc3cea541033eaaea7ce319977b9917cd1ce
Chen Wang <jeffsan@gmx.com>
2022-07-02 20:28:16 -0400

    reformat file structure of optim

--- pypose/optim/functional/__init__.py
+++
@@ -1,1 +0,0 @@
-from .jacobian import modjac
--- pypose/optim/optimizer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .gauss_newton import GaussNewton
-from .gauss_newton import GaussNewton as GN
-from .levenberg_marquardt import LevenbergMarquardt
-from .levenberg_marquardt import LevenbergMarquardt as LM
