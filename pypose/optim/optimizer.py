b024a83edfa35cfbd86ec6b5b2795a9521c416a0
Chen Wang <jeffsan@gmx.com>
2022-07-03 21:28:54 -0400

    fix doc of Gauss-Newton

--- pypose/optim/optimizer.py
+++ pypose/optim/optimizer.py
@@ -5,10 +5,9 @@ from torch.optim import Optimizer
 
 class GaussNewton(Optimizer):
     r'''
-    The `Gauss-Newton (GN) algorithm <https://en.wikipedia.org/wiki/Gauss-Newton_algorithm>`_ for
-    solving non-linear least squares problems. This implementation is for optimizing the model
-    parameters to approximate the targets, which can be a Tensor/LieTensor or a tuple of
-    Tensors/LieTensors.
+    The Gauss-Newton (GN) algorithm solving non-linear least squares problems. This implementation
+    is for optimizing the model parameters to approximate the targets, which can be a
+    Tensor/LieTensor or a tuple of Tensors/LieTensors.
 
     .. math::
         \bm{\theta}^* = \arg\min_{\bm{\theta}}\sum_i \|\bm{y}_i - \bm{f}(\bm{\theta}, \bm{x}_i)\|^2,
@@ -25,7 +24,7 @@ class GaussNewton(Optimizer):
             &\textbf{for} \: t=1 \: \textbf{to} \: \ldots \: \textbf{do}                         \\
             &\hspace{5mm} \mathbf{J} \leftarrow {\dfrac {\partial \bm{f}}
                 {\partial \bm{\theta}_{t-1}}}                                                    \\
-            &\hspace{5mm} \mathbf{A} \leftarrow \mathbf{J}^T \mathbf{J} + \lambda \mathbf{I}     \\
+            &\hspace{5mm} \mathbf{A} \leftarrow \mathbf{J}^T \mathbf{J}                          \\
             &\hspace{5mm} \mathbf{E} = \bm{y} - \bm{f(\bm{\theta}_{t-1}, \bm{x})}                \\
             &\hspace{5mm} \bm{\delta}=\mathrm{pseudo\_inverse}(\mathbf{A})\mathbf{J}^T\mathbf{E} \\
             &\hspace{5mm} \bm{\theta}_t \leftarrow \bm{\theta}_{t-1} + \bm{\delta}               \\
@@ -36,10 +35,39 @@ class GaussNewton(Optimizer):
 
     Args:
         model (nn.Module): a module containing learnable parameters.
+        rcond (float, optional): used to determine the effective rank of :math:`\mathbf{A}`. If
+            ``None``, rcond is set to the machine precision of the dtype of :math:`\mathbf{A}`.
+            Default: ``None``.
+        driver (string, optional): chooses the LAPACK/MAGMA function that will be used. For CPU
+            users, the valid values are ``gels``, ``gelsy``, ``gelsd``, ``gelss``. For CUDA users,
+            the only valid driver is ``gels``, which assumes that :math:`\mathbf{A}` is full-rank.
+            If ``None``, ``gelsy`` is used for CPU inputs and ``gels`` for CUDA inputs. Default:
+            ``None``. To choose the best driver on CPU consider:
+
+            - If :math:`\mathbf{A}` is well-conditioned (its `condition number
+              <https://en.wikipedia.org/wiki/Condition_number>`_ is not too large), or you do not
+              mind some precision loss.
+
+                - For a general matrix: ``gelsy`` (QR with pivoting) (default)
+
+                - If A is full-rank: ``gels`` (QR)
+
+            - If :math:`\mathbf{A}` is not well-conditioned.
+
+                - `gelsd` (tridiagonal reduction and SVD)
+
+                - But if you run into memory issues: ``gelss`` (full SVD).
+
+                See full description of `drivers <https://www.netlib.org/lapack/lug/node27.html>`_.
+
+    See more details of `pseudo inversion
+    <https://pytorch.org/docs/stable/generated/torch.linalg.lstsq.html>`_ using
+    :obj:`torch.linalg.lstsq`.
     '''
-    def __init__(self, model):
+    def __init__(self, model, rcond=None, driver=None):
         self.model = model
-        super().__init__(model.parameters(), defaults={})
+        defaults = dict(rcond=rcond, driver=driver)
+        super().__init__(model.parameters(), defaults=defaults)
 
     @torch.no_grad()
     def step(self, inputs, targets=None):
@@ -59,6 +87,10 @@ class GaussNewton(Optimizer):
             `SGD <https://pytorch.org/docs/stable/generated/torch.optim.SGD.html>`_, where the model
             error has to be a scalar, the model output of :obj:`LM` can be a Tensor/LieTensor or a
             tuple of Tensors/LieTensors.
+
+            See more details of
+            `Gauss-Newton (GN) algorithm <https://en.wikipedia.org/wiki/Gauss-Newton_algorithm>`_ on
+            Wikipedia.
 
         Example:
             Optimizing a simple module to **approximate pose inversion**.
@@ -92,7 +124,8 @@ class GaussNewton(Optimizer):
         for pg in self.param_groups:
             numels = [p.numel() for p in pg['params'] if p.requires_grad]
             J = modjac(self.model, inputs, flatten=True)
-            D = (J.T @ J).pinverse() @ (J.T @ E)
+            D = torch.linalg.lstsq(J.T@J, J.T@E, rcond=pg['rcond'], driver=pg['driver']).solution
+            # Equal to `D = (J.T @ J).pinverse() @ (J.T @ E)`, but faster and numerically stable.
             D = torch.split(D, numels)
             [p.add_(d.view(p.shape)) for p, d in zip(pg['params'], D) if p.requires_grad]
         return self._residual(inputs, targets).norm()
@@ -115,11 +148,10 @@ class GaussNewton(Optimizer):
 
 class LevenbergMarquardt(GaussNewton):
     r'''
-    The `Levenberg-Marquardt (LM) algorithm
-    <https://en.wikipedia.org/wiki/Levenberg-Marquardt_algorithm>`_, which is also known as the
-    damped least-squares (DLS) method for solving non-linear least squares problems. This
-    implementation is for optimizing the model parameters to approximate the targets, which can
-    be a Tensor/LieTensor or a tuple of Tensors/LieTensors.
+    The Levenberg-Marquardt (LM) algorithm, which is also known as the damped least-squares (DLS)
+    method for solving non-linear least squares problems. This implementation is for optimizing the
+    model parameters to approximate the targets, which can be a Tensor/LieTensor or a tuple of
+    Tensors/LieTensors.
 
     .. math::
         \bm{\theta}^* = \arg\min_{\bm{\theta}}\sum_i \|\bm{y}_i - \bm{f}(\bm{\theta}, \bm{x}_i)\|^2,
@@ -179,118 +211,8 @@ class LevenbergMarquardt(GaussNewton):
             in the residual, :math:`\lambda` can be increased, giving a step closer to the gradient
             descent direction.
 
-        Note:
-            Different from PyTorch optimizers like
-            `SGD <https://pytorch.org/docs/stable/generated/torch.optim.SGD.html>`_, where the model
-            error has to be a scalar, the model output of :obj:`LM` can be a Tensor/LieTensor or a
-            tuple of Tensors/LieTensors.
-
-        Example:
-            Optimizing a simple module to **approximate pose inversion**.
-
-            >>> class PoseInv(nn.Module):
-            ...     def __init__(self, *dim):
-            ...         super().__init__()
-            ...         self.pose = pp.Parameter(pp.randn_se3(*dim))
-            ...
-            ...     def forward(self, inputs):
-            ...         return (self.pose.Exp() @ inputs).Log()
-            ...
-            >>> posinv = PoseInv(2, 2)
-            >>> inputs = pp.randn_SE3(2, 2)
-            >>> optimizer = pp.optim.LM(posinv, damping=1e-6)
-            ...
-            >>> for idx in range(10):
-            ...     loss = optimizer.step(inputs)
-            ...     print('Pose Inversion loss %.7f @ %d it'%(loss, idx))
-            ...     if loss < 1e-5:
-            ...         print('Early Stoping with loss:', loss.item())
-            ...         break
-            ...
-            Pose Inversion error: 1.6600330 @ 0 it
-            Pose Inversion error: 0.1296970 @ 1 it
-            Pose Inversion error: 0.0008593 @ 2 it
-            Pose Inversion error: 0.0000004 @ 3 it
-            Early Stoping with error: 4.443569991963159e-07
-        '''
-        E = self._residual(inputs, targets)
-        for pg in self.param_groups:
-            numels = [p.numel() for p in pg['params'] if p.requires_grad]
-            J = modjac(self.model, inputs, flatten=True)
-            A = J.T @ J
-            A.diagonal().add_(pg['damping'] * A.diagonal().clamp(pg['min'], pg['max']))
-            D = (J.T @ E).cholesky_solve(torch.linalg.cholesky(A))
-            D = torch.split(D, numels)
-            [p.add_(d.view(p.shape)) for p, d in zip(pg['params'], D) if p.requires_grad]
-        return self._residual(inputs, targets).norm()
-
-
-
-class LevenbergMarquardt(GaussNewton):
-    r'''
-    The `Levenberg-Marquardt (LM) algorithm
-    <https://en.wikipedia.org/wiki/Levenberg-Marquardt_algorithm>`_, which is also known as the
-    damped least-squares (DLS) method for solving non-linear least squares problems. This
-    implementation is for optimizing the model parameters to approximate the targets, which can
-    be a Tensor/LieTensor or a tuple of Tensors/LieTensors.
-
-    .. math::
-        \bm{\theta}^* = \arg\min_{\bm{\theta}}\sum_i \|\bm{y}_i - \bm{f}(\bm{\theta}, \bm{x}_i)\|^2,
-
-    where :math:`\bm{f}(\bm{\theta}, \bm{x})` is the model, :math:`\bm{\theta}` is the parameters
-    to be optimized, and :math:`\bm{x}` is the model inputs.
-
-    .. math::
-       \begin{aligned}
-            &\rule{113mm}{0.4pt}                                                                 \\
-            &\textbf{input}: \lambda \geq 0~\text{(damping)}, \bm{\theta}_0~\text{(params)},
-                \bm{f}~\text{(model)}, \bm{x}~(\text{inputs}), \bm{y}~(\text{targets})           \\
-            &\rule{113mm}{0.4pt}                                                                 \\
-            &\textbf{for} \: t=1 \: \textbf{to} \: \ldots \: \textbf{do}                         \\
-            &\hspace{5mm} \mathbf{J} \leftarrow {\dfrac {\partial \bm{f}}
-                {\partial \bm{\theta}_{t-1}}}                                                    \\
-            &\hspace{5mm} \mathbf{A} \leftarrow \mathbf{J}^T \mathbf{J} 
-                       + \lambda \mathrm{diag}(\mathbf{J}^T \mathbf{J}).\mathrm{clamp(min, max)} \\
-            &\hspace{5mm} \mathbf{E} = \bm{y} - \bm{f(\bm{\theta}_{t-1}, \bm{x})}                \\
-            &\hspace{5mm} \mathbf{L} = \mathrm{cholesky\_decomposition}(\mathbf{A})              \\
-            &\hspace{5mm} \bm{\delta}=\mathrm{cholesky\_solve}(\mathbf{J}^T \mathbf{E}, \bm{L})  \\
-            &\hspace{5mm} \bm{\theta}_t \leftarrow \bm{\theta}_{t-1} + \bm{\delta}               \\
-            &\rule{113mm}{0.4pt}                                                          \\[-1.ex]
-            &\bf{return} \:  \theta_t                                                     \\[-1.ex]
-            &\rule{113mm}{0.4pt}                                                          \\[-1.ex]
-       \end{aligned}
-
-    Args:
-        model (nn.Module): a module containing learnable parameters.
-        damping (float): Levenberg's damping factor (positive number).
-        min (float, optional): the lower-bound of the matrix diagonal to inverse.
-        max (float, optional): the upper-bound of the matrix diagonal to inverse.
-    '''
-    def __init__(self, model, damping, min=1e-6, max=1e32):
-        self.model = model
-        assert damping > 0, ValueError("Invalid damping factor: {}".format(damping))
-        defaults = dict(damping=damping, min=min, max=max)
-        Optimizer.__init__(self, params=model.parameters(), defaults=defaults)
-
-    @torch.no_grad()
-    def step(self, inputs, targets=None):
-        r'''
-        Performs a single optimization step.
-
-        Args:
-            inputs (Tensor/LieTensor or tuple of Tensors/LieTensors): the inputs to the model.
-            targets (Tensor/LieTensor or tuple of Tensors/LieTensors): the model targets to optimize.
-                If not given, the squared model outputs are minimized. Defaults: ``None``.
-
-        Return:
-            Tensor: the minimized model error, i.e., :math:`\|\bm{y} - \bm{f}(\bm{\theta}, \bm{x})\|^2`.
-
-        Note:
-            The (non-negative) damping factor :math:`\lambda` can be adjusted at each iteration. If
-            reduction of the residual is rapid, a smaller value can be used, bringing the algorithm
-            closer to the Gauss-Newton algorithm, whereas if an iteration gives insufficient reduction
-            in the residual, :math:`\lambda` can be increased, giving a step closer to the gradient
-            descent direction.
+            See more details of `Levenberg-Marquardt (LM) algorithm
+            <https://en.wikipedia.org/wiki/Levenberg-Marquardt_algorithm>`_ on Wikipedia.
 
         Note:
             Different from PyTorch optimizers like
