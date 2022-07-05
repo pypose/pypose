303703a00afb62967d7aeb60c90695d8ec6104f7
yaoyuh <yaoyuh@andrew.cmu.edu>
2022-07-05 01:22:14 -0400

    Add CuPy support for solving linear systems.

---
+++ tests/unit_tests/test_cu_sparse_block_matrix.py
@@ -0,0 +1,150 @@
+# Unit test tools.
+# from cgi import test
+# import functools
+# import inspect
+import unittest
+
+# System tools.
+import copy
+import numpy as np
+import cupy as cp
+
+# pypose.
+import pypose as pp
+from pypose.optim.sparse_block_matrix import (
+    SparseBlockMatrix, sbm_to_bsr_cpu )
+from pypose.optim.cu_sparse_block_matrix import sbm_to_cupy
+
+# Test utils.
+from tests.unit_tests.common import ( torch_equal, show_delimeter )
+
+# PyTorch
+import torch
+
+class Test_cu_SparseBlockMatrix(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        #      0,  1,  2,  3,  4,  5,  6,  7,  8
+        # ======================================
+        # 0 |  0,  1,  2,  x,  x,  x,  6,  7,  8
+        # 1 |  3,  4,  5,  x,  x,  x,  9, 10, 11
+        # 2 |  x,  x,  x, 12, 13, 14,  x,  x,  x
+        # 3 |  x,  x,  x, 15, 16, 17,  x,  x,  x
+        # 4 | 18, 19, 20,  x,  x,  x, 24, 25, 26
+        # 5 | 21, 22, 23,  x,  x,  x, 27, 28, 29
+
+        #     30, 32, 34 = 0,  1,  2 + 30, 31, 32
+        #     36, 38, 40   3,  4,  5   33, 34, 35
+
+        #      0,  1,  2,  3,  4,  5,  6,  7,  8
+        # ======================================
+        # 0 |  0,  1,  2,  x,  x,  x, 18, 19, 20
+        # 1 |  3,  4,  5,  x,  x,  x, 21, 22, 23
+        # 2 |  x,  x,  x, 12, 13, 14,  x,  x,  x
+        # 3 |  x,  x,  x, 15, 16, 17,  x,  x,  x
+        # 4 |  6,  7,  8,  x,  x,  x, 24, 25, 26
+        # 5 |  9, 10, 11,  x,  x,  x, 27, 28, 29
+
+        cls.block_shape = (2, 3)
+        cls.layout = [
+            [ 2, 6, 4, 2, 6 ],
+            [ 3, 3, 6, 9, 9 ]
+        ]
+        cls.block_indices = [
+            [ 0, 2, 1, 0, 2 ],
+            [ 0, 0, 1, 2, 2 ]
+        ]
+
+        cls.bsr_indptr = np.array([ 0, 2, 3, 5 ])
+        cls.bsr_indices = np.array([ 0, 2, 1, 0, 2 ])
+
+        cls.rows = max(cls.layout[0])
+        cls.cols = max(cls.layout[1])
+        cls.shape = ( cls.rows, cls.cols )
+
+        cls.rows_block = cls.rows // cls.block_shape[0]
+        cls.cols_block = cls.cols // cls.block_shape[1]
+        cls.shape_blocks = ( cls.rows_block, cls.cols_block )
+
+        cls.rows_block_0 = 2
+        cls.cols_block_0 = 3
+        cls.rows_block_last = 2
+        cls.cols_block_last = 3
+
+        cls.row_base_block_0 = 0
+        cls.col_base_block_0 = 0
+        cls.row_base_block_last = 4
+        cls.col_base_block_last = 6
+
+        cls.last_block_row_idx = cls.rows_block - 1
+        cls.last_block_col_idx = cls.cols_block - 1
+
+        shape_values = ( len(cls.layout[0]), *cls.block_shape )
+        cls.values_raw = torch.arange(30, dtype=torch.float32).view( shape_values )
+        cls.bsr_data = torch.index_select( cls.values_raw, 0, torch.Tensor([0, 3, 2, 1, 4]).to(dtype=torch.int32) ).numpy()
+
+    def test_sbm_to_cupy(self):
+        print()
+        show_delimeter('Test sbm to CuPy conversion. ')
+
+        # Creawte the SparseblockMatrix.
+        device = 'cuda'
+        values = Test_cu_SparseBlockMatrix.values_raw.to(device)
+        sbm = SparseBlockMatrix(Test_cu_SparseBlockMatrix.block_shape, dtype=torch.float32, device=device)
+        sbm.create(shape_blocks=Test_cu_SparseBlockMatrix.shape_blocks, block_indices=Test_cu_SparseBlockMatrix.block_indices)
+        sbm.set_block_storage(values, clone=False)
+
+        # Convert to CuPy.
+        cu_sm = sbm_to_cupy(sbm)
+
+        # Convert cu_sm to NumPy array.
+        np_cu = cp.asnumpy( cu_sm.toarray() )
+
+        print(f'np_cu = \n{np_cu}')
+
+        # Convert sbm to SciPy BSR.
+        bsr = sbm_to_bsr_cpu(sbm)
+
+        # Convert bsr to NumPy array. 
+        np_bsm = bsr.toarray()
+
+        # Compare.
+        self.assertTrue( np.allclose( np_cu, np_bsm ), f'test_sbm_to_cupy failed. ' )
+
+    def test_sbm_to_cupy_duplicate_block_index(self):
+        print()
+        show_delimeter('Test sbm to CuPy conversion with duplicated block index. ')
+
+        indices = copy.deepcopy( Test_cu_SparseBlockMatrix.block_indices )
+        indices[0][-1] = 0
+        indices[1][-1] = 0
+
+        # Creawte the SparseblockMatrix.
+        device = 'cuda'
+        values = Test_cu_SparseBlockMatrix.values_raw.to(device)
+        sbm = SparseBlockMatrix(Test_cu_SparseBlockMatrix.block_shape, dtype=torch.float32, device=device)
+        sbm.create(shape_blocks=Test_cu_SparseBlockMatrix.shape_blocks, block_indices=indices)
+        sbm.set_block_storage(values, clone=False)
+
+        # Convert to CuPy.
+        cu_sm = sbm_to_cupy(sbm)
+
+        # Convert cu_sm to NumPy array.
+        np_cu = cp.asnumpy( cu_sm.toarray() )
+
+        print(f'np_cu = \n{np_cu}')
+
+        # Convert sbm to SciPy BSR.
+        bsr = sbm_to_bsr_cpu(sbm)
+
+        # Convert bsr to NumPy array. 
+        np_bsm = bsr.toarray()
+
+        # Compare.
+        self.assertTrue( np.allclose( np_cu, np_bsm ), f'test_sbm_to_cupy_duplicate_block_index failed. ' )
+
+if __name__ == '__main__':
+    import os
+    print('Run %s. ' % (os.path.basename(__file__)))
+    unittest.main()
---
+++ tests/unit_tests/test_linear_solver_cu_sparse.py
@@ -0,0 +1,112 @@
+# Unit test tools.
+# from cgi import test
+# import functools
+# import inspect
+import unittest
+
+# System tools.
+import copy
+import numpy as np
+import cupy as cp
+
+# pypose.
+import pypose as pp
+from pypose.optim.sparse_block_matrix import (
+    SparseBlockMatrix )
+from pypose.optim.cu_sparse_block_matrix import ( 
+    sbm_to_cupy, torch_to_cupy, cupy_to_torch )
+from pypose.optim.linear_solver_cu_sparse import LinearSolverCuSparse
+
+# Test utils.
+from tests.unit_tests.common import ( torch_equal, torch_equal_rough, show_delimeter )
+
+# PyTorch
+import torch
+
+class Test_LinearSolverCuSparse(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        #      0,  1,  2,  3,  4,  5
+        # ==========================
+        # 0 |  0,  1,  x,  x,  4,  5
+        # 1 |  2,  3,  x,  x,  6,  7
+        # 2 |  x,  x,  8,  9,  x,  x
+        # 3 |  x,  x, 10, 11,  x,  x
+        # 4 | 12, 13,  x,  x, 16, 17
+        # 5 | 14, 15,  x,  x, 18, 19
+
+        cls.block_shape = (2, 2)
+
+        cls.block_indices = [
+            [ 0, 0, 1, 2, 2 ],
+            [ 0, 2, 1, 0, 2 ]
+        ]
+
+        cls.rows = ( max(cls.block_indices[0]) + 1 ) * cls.block_shape[0]
+        cls.cols = ( max(cls.block_indices[1]) + 1 ) * cls.block_shape[1]
+        cls.shape = ( cls.rows, cls.cols )
+
+        cls.rows_block = cls.rows // cls.block_shape[0]
+        cls.cols_block = cls.cols // cls.block_shape[1]
+        cls.shape_blocks = ( cls.rows_block, cls.cols_block )
+
+        shape_values = ( len(cls.block_indices[0]), *cls.block_shape )
+        cls.values_raw = torch.arange(20, dtype=torch.float32).view( shape_values )
+
+        # Randomize the blocks.
+        for i in range( cls.values_raw.shape[0] ):
+            r = torch.rand(2).to(dtype=torch.float32).view((2,1))
+            cls.values_raw[i, 0, 0] = r[0] + 1
+            cls.values_raw[i, 0, 1] = r[1]
+            cls.values_raw[i, 1, 0] = r[1]
+            cls.values_raw[i, 1, 1] = r[0] + 1
+
+        cls.values_raw[3] = cls.values_raw[1]
+
+        cls.x = torch.rand((6, 1), dtype=torch.float32)
+
+    def test_solve_sym_postive_definite(self):
+        print()
+        show_delimeter('Test solving a system with symmetric postive definite coefficient matrix. ')
+
+        # Creawte the SparseblockMatrix.
+        device = 'cuda'
+        values = Test_LinearSolverCuSparse.values_raw.to(device)
+        sbm = SparseBlockMatrix(Test_LinearSolverCuSparse.block_shape, dtype=torch.float32, device=device)
+        sbm.create(shape_blocks=Test_LinearSolverCuSparse.shape_blocks, block_indices=Test_LinearSolverCuSparse.block_indices)
+        sbm.set_block_storage(values, clone=False)
+
+        # Create the rhs
+        x = Test_LinearSolverCuSparse.x.to(device=device)
+        cu_x = torch_to_cupy(x)
+        cu_A = sbm_to_cupy(sbm)
+        cu_b = cu_A.dot(cu_x)
+
+        # Show cu_A.
+        print(f'cu_b: {cu_b.reshape((1, -1))}')
+        print(f'cu_A: \n{cu_A.toarray()}')
+
+        # Convert cu_b to PyTorch.
+        b = cupy_to_torch(cu_b)
+
+        # Solve.
+        solver = LinearSolverCuSparse()
+        solver.initialize()
+        sx = solver.solve(sbm, b).view((-1, 1))
+
+        # Show some information.
+        print(f'original x: {x.view((1, -1))}')
+        print(f'solved x: {sx.view((1, -1))}')
+
+        # Compare.
+        try:
+            torch_equal( sx, x )
+        except Exception as exc:
+            print(exc)
+            self.assertTrue(False, f'test_solve_sym_postive_definite failed. ')
+
+if __name__ == '__main__':
+    import os
+    print('Run %s. ' % (os.path.basename(__file__)))
+    unittest.main()
