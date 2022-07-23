23e17846507d068e121aca6890a2c19771f84d6e
apandey02 <93224425+apandey02@users.noreply.github.com>
2022-07-23 00:03:29 -0400

    Added files with nonlinear dynamics examples and tester files
    dynamics.py -> added Cart-Pole example and Lorenz Attractor example. Cart-pole example uses specific dynamics for cart-pole system; Lorenz attractor example uses neural network for state transition.
    NNTest.py -> testing Lorenz attractor example. There seems to be an issue with the "loss.backwards( )" functionality in training the model. It works on the first iteration, but fails on the second one.
    cartpoleTest.py -> testing for cart-pole example.

---
+++ pypose/module/cartpoleTest.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch as torch
+import matplotlib.pyplot as plt
+
+from dynamics import CartPole
+
+def createTimePlot(x,y,figname="Un-named plot",title=None,xlabel=None,ylabel=None):
+    f = plt.figure(figname)
+    plt.plot(x,y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    return f
+
+if __name__ == "__main__":
+    ''' Begin Cart-Pole Test '''
+    # Create parameters for cart pole trajectory
+    dt = 0.01   # Delta t
+    len = 1.5   # Length of pole
+    m_cart = 20 # Mass of cart
+    m_pole = 10 # Mass of pole
+    g = 9.81    # Accerleration due to gravity
+    N = 1000    # Number of iterations
+    # Create dynamics solver object
+    cartPoleSolver = CartPole(dt,len,m_cart,m_pole,g)
+    # Input vector
+    input = torch.sin(torch.linspace(0,N*dt+1,N+1))
+    # Initial state
+    state = torch.tensor([0,0,np.pi,0],dtype=float)
+    ##### Calculate trajectory
+    state_all = torch.zeros(N,4,dtype=float)
+    state_all[0,:] = state
+    # Loop to calculate trajectory
+    for i in range(1,N):
+        state = cartPoleSolver.forward(state,input[i-1])
+        state_all[i,:] = state    
+
+    # Create time plots to show dynamics
+    x,xdot,theta,thetadot = state_all.T
+    time = torch.linspace(0,N*dt,N)
+    x_fig = createTimePlot(time,x,figname="x Plot",xlabel="Time",ylabel="x",title="x Plot")
+    xdot_fig = createTimePlot(time,xdot,figname="x dot Plot",xlabel="Time",ylabel="x dot",title="x dot Plot")
+    theta_fig = createTimePlot(time,theta,figname="theta Plot",xlabel="Time",ylabel="theta",title="theta Plot")
+    thetadot_fig = createTimePlot(time,thetadot,figname="theta dot Plot",xlabel="Time",ylabel="theta dot",title="theta dot Plot")
+    plt.show()
+    
