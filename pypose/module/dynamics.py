5f3956be54b1269ce3d9523d8553e46ff51d8b17
Daning Huang <huangdn.nuaa@gmail.com>
2022-08-10 18:08:49 -0400

    Edited the documentation for the _System class.

--- pypose/module/dynamics.py
+++ pypose/module/dynamics.py
@@ -5,76 +5,90 @@ from torch.autograd.functional import jacobian
 
 class _System(nn.Module):
     r'''
-    A sub-class of :obj:`torch.nn.Module` to build general dynamics.
-    
+    A sub-class of :obj:`torch.nn.Module` for the simulation and linearization of nonlinear
+    time-discrete time-varying dynamics.
+
     Args:
         time (:obj:`boolean`): Whether the system is time-varying; defaults to False, meaning time-invariant
 
-    Linearization
+    Governing Equation
     ----------
     The nonlinear state-space equation is given as:
 
     .. math::
         \begin{align}
-        \mathbf{x}_{k+1} &= \mathbf{f}(\mathbf{x}_k,\mathbf{u}_k) \\
-        \mathbf{y}_{k} &= \mathbf{g}(\mathbf{x}_k,\mathbf{u}_k)
+        \mathbf{x}_{k+1} &= \mathbf{f}(\mathbf{x}_k,\mathbf{u}_k,t_k) \\
+        \mathbf{y}_{k} &= \mathbf{g}(\mathbf{x}_k,\mathbf{u}_k,t_k)
         \end{align}
 
-    This class provides a means to linearize the system at any point along a trajectory.
+    where :math:`k` is the time step; :math:`\mathbf{x}\in\mathbb{R}^n`, :math:`\mathbf{u}\in\mathbb{R}^m`,
+    :math:`\mathbf{y}\in\mathbb{R}^p` are the states, inputs and observations, respectively;
+    :math:`\mathbf{f}:\mathbb{R}^n\times\mathbb{R}^m\times\mathbb{R}\mapsto\mathbb{R}^n` and
+    :math:`\mathbf{g}:\mathbb{R}^n\times\mathbb{R}^m\times\mathbb{R}\mapsto\mathbb{R}^p` are
+    the state transition and observation functions, respectively.
 
-    Note: 
-        The linearization can be provided for any arbitrary point, not just the equilibrium point(s).
+    Note:
+        Here we choose to work with a time-discrete system, since typically in numerical simulations and
+        control applications, the dynamical systems are usually discretized in time.  However, in future,
+        the continuous dynamical will also be considered and implemented.
 
-    Suppose we to linearize about :math:`(x^*,u^*)`
-    for an arbitrary equation :math:`h(x,u)`.
-    Through a Taylor series expansion (ignoring higher order terms), we get the following:
+    To use the class, the user should define two methods `state_transition` and `observation`, which are
+    used in the `forward` method.  The `forward` method advances the dynamical system by one time step, and
+    is the function that is back-propagated in the learning process.
+
+    Note:
+        The `forward` method implicitly increments the time step via `forward_hook`.  As a result, in the
+        implementations of `state_transition` and `observation`, one does not need to include the time as
+        the input; instead, one can directly access the current time via the data member `self.t`.
+
+    Linearization
+    ----------
+    This class provides a means to linearize the system at a reference point :math:`\chi^*=(\mathbf{x}^*,\mathbf{u}^*,t^*)`
+    along a trajectory.  Consider a point :math:`\chi=(\mathbf{x}^*+\delta\mathbf{x},\mathbf{u}^*+\delta\mathbf{u},t^*)`
+    near :math:`\chi^*`.  The goal is to obtain a linear dynamics model for :math:`\chi` that behaves
+    similarly to the nonlinear dynamics when :math:`\delta\mathbf{x}||` and :math:`\delta\mathbf{u}||` are sufficiently
+    small.
+
+    The linearization process is the same for :math:`\mathbf{f}` and :math:`\mathbf{g}`, so we only present
+    the case for :math:`\mathbf{f}` in detail below.
+    Ignoring the higher order terms, the Taylor series expansion of :math:`\mathbf{f}` at the reference point
+    :math:`\chi^*` is
 
     .. math::
         \begin{aligned}
-        h(x,u)=h(x^*+\delta x,u^*+\delta u) &= h(x^*,u^*) + 
-                                \left. \frac{\partial h}{\partial x} \right|_{x^*,u^*} \delta x +
-                                \left. \frac{\partial h}{\partial u} \right|_{x^*,u^*} \delta u \\
-        \Rightarrow
-        h(x^*,u^*) + h(\delta x,\delta u) &= h(x^*,u^*) + 
-                                \left. \frac{\partial h}{\partial x} \right|_{x^*,u^*} \delta x +
-                                \left. \frac{\partial h}{\partial u} \right|_{x^*,u^*} \delta u \\
-        \Rightarrow
-        h(\delta x,\delta u) &=  \left. \frac{\partial h}{\partial x} \right|_{x^*,u^*} \delta x +
-                                \left. \frac{\partial h}{\partial u} \right|_{x^*,u^*} \delta u \\
+        \mathbf{f}(\mathbf{x},\mathbf{u},t^*) &\approx \mathbf{f}(\mathbf{x}^*,\mathbf{u}^*,t^*) + 
+                                \left. \frac{\partial \mathbf{f}}{\partial \mathbf{x}} \right|_{\chi^*} \delta \mathbf{x} +
+                                \left. \frac{\partial \mathbf{f}}{\partial \mathbf{u}} \right|_{\chi^*} \delta \mathbf{u} \\
+        &= \mathbf{f}(\mathbf{x}^*,\mathbf{u}^*,t^*) + \mathbf{A}(\mathbf{x}-\mathbf{x}^*) + \mathbf{B}(\mathbf{u}-\mathbf{u}^*) \\
+        &= \mathbf{A}\mathbf{x} + \mathbf{B}\mathbf{u} + \mathbf{c}_1
         \end{aligned}
-    
-    Because :math:`(x^*,u^*)` is not necessarily an equilibrium point, we must add a bias term, :math:`c`. 
-    Also, to stay consistent with the linear state-space equation, we allow :math:`\delta x = x^*` and 
-    :math:`\delta u = u^*`. Thus:
+
+    where we have introduced,
 
     .. math::
-        h(x^*,u^*) = \left. \frac{\partial h}{\partial x} \right|_{x^*,u^*} x^* +
-                     \left. \frac{\partial h}{\partial u} \right|_{x^*,u^*} u^* + c \\
-    
-    Note:
-        If :math:`(x^*,u^*)` specify an equilibrium point, then :math:`c=0`.
-    Applying this to our state-space equations, i.e., Eqs. (1) and (2), we get:
+        \mathbf{A} = \left. \frac{\partial \mathbf{f}}{\partial \mathbf{x}} \right|_{\chi^*},\quad 
+        \mathbf{B} = \left. \frac{\partial \mathbf{f}}{\partial \mathbf{u}} \right|_{\chi^*},\quad
+        \mathbf{c}_1 = \mathbf{f}(\mathbf{x}^*,\mathbf{u}^*,t^*) - \mathbf{A}\mathbf{x}^* - \mathbf{B}\mathbf{u}^*
+
+    Similarly, one can linearize :math:`\mathbf{g}` as
 
     .. math::
-        \begin{align}
-        \mathbf{x}_{k+1} &= \mathbf{f}(\mathbf{x}_k,\mathbf{u}_k) = 
-        \left. \frac{\partial \mathbf{f}}{\partial \mathbf{x}} \right|_{\mathbf{x}_k,\mathbf{u}_k} \mathbf{x}_k +
-        \left. \frac{\partial \mathbf{f}}{\partial \mathbf{u}} \right|_{\mathbf{x}_k,\mathbf{u}_k} \mathbf{u}_k + 
-        \mathbf{c_1} \\
-        \mathbf{y}_{k} &= \mathbf{g}(\mathbf{x}_k,\mathbf{u}_k) = 
-        \left. \frac{\partial \mathbf{g}}{\partial \mathbf{x}} \right|_{\mathbf{x}_k,\mathbf{u}_k} \mathbf{x}_k +
-        \left. \frac{\partial \mathbf{g}}{\partial \mathbf{u}} \right|_{\mathbf{x}_k,\mathbf{u}_k} \mathbf{u}_k +
-        \mathbf{c_2}
-        \end{align} \\
-        
+        \mathbf{g}(\mathbf{x},\mathbf{u},t^*) \approx \mathbf{C}\mathbf{x} + \mathbf{D}\mathbf{u} + \mathbf{c}_2
 
-    Eqs. (3) and (4) are now in the form of linear state-space equations, where:
+    where
 
     .. math::
-        \bf{A} = \left. \frac{\partial \mathbf{f}}{\partial \mathbf{x}} \right|_{\mathbf{x}_k,\mathbf{u}_k} , 
-        \bf{B} = \left. \frac{\partial \mathbf{f}}{\partial \mathbf{u}} \right|_{\mathbf{x}_k,\mathbf{u}_k} , 
-        \bf{C} = \left. \frac{\partial \mathbf{g}}{\partial \mathbf{x}} \right|_{\mathbf{x}_k,\mathbf{u}_k} , 
-        \bf{D} = \left. \frac{\partial \mathbf{g}}{\partial \mathbf{u}} \right|_{\mathbf{x}_k,\mathbf{u}_k}
+        \mathbf{C} = \left. \frac{\partial \mathbf{g}}{\partial \mathbf{x}} \right|_{\chi^*},\quad 
+        \mathbf{D} = \left. \frac{\partial \mathbf{g}}{\partial \mathbf{u}} \right|_{\chi^*},\quad
+        \mathbf{c}_2 = \mathbf{g}(\mathbf{x}^*,\mathbf{u}^*,t^*) - \mathbf{C}\mathbf{x}^* - \mathbf{D}\mathbf{u}^*
+
+    Note: 
+        The notion of linearization is slightly different from that in dynamical system theory.
+        First, the linearization is done for any arbitrary point, not just at the equilibrium point(s), and
+        therefore the extra ``bias'' terms :math:`\mathbf{c}_1` and :math:`\mathbf{c}_2` are produced.
+        Second, the linearized equations are written for the full states and inputs, :math:`\mathbf{x}` and :math:`\mathbf{u}`,
+        instead of their perturbation terms, :math:`\delta \mathbf{x}` and :math:`\delta \mathbf{u}`
+        so that the model is consistent with, e.g., the `LTI` model and the iterative LQR solver.
     '''
 
     def __init__(self, time=False):
@@ -93,9 +107,9 @@ class _System(nn.Module):
         Parameters
         ----------
         state : Tensor
-                The state of the dynamic system
+                The state of the dynamical system
         input : Tensor
-                The input to the dynamic system
+                The input to the dynamical system
 
         Returns
         -------
@@ -114,10 +128,13 @@ class _System(nn.Module):
         Parameters
         ----------
         state : Tensor
-                The state of the dynamic system
+                The state of the dynamical system
         input : Tensor
-                The input to the dynamic system
-                
+                The input to the dynamical system
+        
+        Note:
+            One can access the current time via the data member `self.t`.
+
         Returns
         ----------
         new_state   : Tensor
@@ -130,9 +147,12 @@ class _System(nn.Module):
         Parameters
         ----------
         state : Tensor
-                The state of the dynamic system
+                The state of the dynamical system
         input : Tensor
-                The input to the dynamic system
+                The input to the dynamical system
+        
+        Note:
+            One can access the current time via the data member `self.t`.
 
         Returns
         ----------
@@ -144,22 +164,24 @@ class _System(nn.Module):
     def reset(self,t=0):
         self.t.fill_(0) 
 
-    def set_linearization_point(self, state, input):
+    def set_linearization_point(self, state, input, t):
         r'''
         Function to set the point about which the system is to be linearized.
 
         Parameters
         ----------
         state : Tensor
-                The state of the dynamic system
+                The state of the dynamical system
         input : Tensor
-                The input to the dynamic system
+                The input to the dynamical system
+        t     : Tensor
+                The time step of the dynamical system
 
         Returns
         ----------
         None
         '''
-        self.state, self.input = state, input
+        self.state, self.input, self.t = state, input, t
 
     @property
     def A(self):
@@ -168,9 +190,9 @@ class _System(nn.Module):
         '''
         if hasattr(self, '_A'):
             return self._A
-        else:
-            func = lambda x: self.state_transition(x, self.input)
-            return jacobian(func, self.state, **self.jacargs)
+        func = lambda x: self.state_transition(x, self.input)
+        self._A = jacobian(func, self.state, **self.jacargs)
+        return self._A
 
     @property
     def B(self):
@@ -179,9 +201,9 @@ class _System(nn.Module):
         '''
         if hasattr(self, '_B'):
             return self._B
-        else:
-            func = lambda x: self.state_transition(self.state, x)
-            return jacobian(func, self.input, **self.jacargs)
+        func = lambda x: self.state_transition(self.state, x)
+        self._B = jacobian(func, self.input, **self.jacargs)
+        return self._B
 
     @property
     def C(self):
@@ -190,10 +212,10 @@ class _System(nn.Module):
         '''
         if hasattr(self, '_C'):
             return self._C
-        else:
-            func = lambda x: self.observation(x, self.input)
-            return jacobian(func, self.state, **self.jacargs)
- 
+        func = lambda x: self.observation(x, self.input)
+        self._C = jacobian(func, self.state, **self.jacargs)
+        return self._C
+
     @property
     def D(self):
         r'''
@@ -201,9 +223,9 @@ class _System(nn.Module):
         '''
         if hasattr(self, '_D'):
             return self._D
-        else:
-            func = lambda x: self.observation(self.state, x)
-            return jacobian(func, self.input, **self.jacargs)
+        func = lambda x: self.observation(self.state, x)
+        self._D = jacobian(func, self.input, **self.jacargs)
+        return self._D
     
     @property
     def c1(self):
@@ -212,9 +234,9 @@ class _System(nn.Module):
         '''
         if hasattr(self,'_c1'):
             return self._c1
-        else:
-            return self.state_transition(self.state,self.input)-(self.state).matmul(self.A.mT)-(self.input).matmul(self.B.mT)
-    
+        self._c1 = self.state_transition(self.state,self.input)-(self.state).matmul(self.A.mT)-(self.input).matmul(self.B.mT)
+        return self._c1
+
     @property
     def c2(self):
         r'''
@@ -222,8 +244,8 @@ class _System(nn.Module):
         '''
         if hasattr(self,'_c2'):
             return self._c2
-        else:
-            return self.observation(self.state,self.input)-(self.state).matmul(self.C.mT)-(self.input).matmul(self.D.mT)
+        self._c2 = self.observation(self.state,self.input)-(self.state).matmul(self.C.mT)-(self.input).matmul(self.D.mT)
+        return self._c2
 
 class LTI(_System):
     r'''
