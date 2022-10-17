6139520ddf6593808b96ad200a5e1fc8bb302abc
Bowen Li <bowenli@perceptron.cm.cluster>
2022-10-16 22:18:20 -0400

    sigma can also be int

--- pypose/lietensor/lietensor.py
+++ pypose/lietensor/lietensor.py
@@ -302,7 +302,7 @@ class so3Type(LieType):
         return SO3_type.Log(SO3_type.identity(*size, **kwargs))
 
     def randn(self, *size, sigma=1.0, requires_grad=False, **kwargs):
-        assert isinstance(sigma, float), 'so3 and SO3 type only accepts single float sigma input'
+        assert isinstance(sigma, float) or isinstance(sigma, int), 'so3 and SO3 type only accepts single float sigma input'
         size = self.to_tuple(size)
         data = torch.randn(*(size + torch.Size([3])), **kwargs)
         dist = data.norm(dim=-1, keepdim=True)
