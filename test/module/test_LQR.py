805f00560f589b6884044356ca1bc4991a77717f
hakunanh <hannahh6069@gmail.com>
2022-10-20 01:35:53 -0400

    add LQR test file

--- test/module/test_LQR.py
+++ test/module/test_LQR.py
@@ -6,7 +6,7 @@ import cvxpy as cp
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def LQR_cp(C, c, F, f, x_init, T, n_state, n_ctrl): 
+def LQR_cp(Q, p, F, f, x_init, T, n_state, n_ctrl): 
 
     tau = cp.Variable((n_state+n_ctrl, T))
 
@@ -17,7 +17,7 @@ def LQR_cp(C, c, F, f, x_init, T, n_state, n_ctrl):
     for t in range(T):
         xt = tau[:n_state,t]
         ut = tau[n_state:,t]
-        objs.append(0.5*cp.quad_form(tau[:,t], C[t]) + cp.sum(cp.multiply(c[t], tau[:,t])))
+        objs.append(0.5*cp.quad_form(tau[:,t], Q[t]) + cp.sum(cp.multiply(p[t], tau[:,t])))
         if t+1 < T:
             xtp1 = tau[:n_state, t+1]
             cons.append(xtp1 == F[t]@tau[:,t]+f[t])
@@ -28,67 +28,44 @@ def LQR_cp(C, c, F, f, x_init, T, n_state, n_ctrl):
 
 
 def test_LQR_linear():
+
     torch.manual_seed(0)
-
-    n_batch = 2
-    n_state, n_ctrl = 3, 4
+    n_batch, n_state, n_ctrl, T = 2, 4, 3, 5
     n_sc = n_state + n_ctrl
-    T = 5
-    C = torch.randn(T, n_batch, n_sc, n_sc)
-    C = torch.matmul(C.mT, C)
-    c = torch.randn(T, n_batch, n_sc)
+    Q = torch.randn(T, n_batch, n_sc, n_sc)
+    Q = torch.matmul(Q.mT, Q)
+    p = torch.randn(T, n_batch, n_sc)
     alpha = 0.2
-    R = torch.tile(torch.eye(n_state) + alpha * torch.randn(n_state, n_state), (T, n_batch, 1, 1))
-    S = torch.tile(torch.randn(n_state, n_ctrl), (T, n_batch, 1, 1))
-    F = torch.cat((R, S), axis=3)
-    f = torch.tile(torch.randn(n_state), (T, n_batch, 1))
+    A = torch.tile(torch.eye(n_state) + alpha * torch.randn(n_state, n_state), (n_batch, 1, 1))
+    B = torch.tile(torch.randn(n_state, n_ctrl), (n_batch, 1, 1))
+    C = torch.tile(torch.eye(n_state), (n_batch, 1, 1))
+    D = torch.tile(torch.zeros(n_state, n_ctrl), (n_batch, 1, 1))
+    c1 = torch.tile(torch.randn(n_state), (n_batch, 1))
+    c2 = torch.tile(torch.zeros(n_state), (n_batch, 1))
     x_init = torch.randn(n_batch, n_state)
+    u_lqr = torch.tile(torch.zeros(n_ctrl), (n_batch, 1))
+    F = torch.cat((A, B), axis=2)
+    F = torch.tile(F, (T, 1, 1, 1))
+    f = torch.tile(c1, (T, 1, 1))
 
-    tau_cp, objs_cp = LQR_cp(C[:,0], c[:,0], F[:,0], f[:,0], x_init[0], T, n_state, n_ctrl)
+    tau_cp, objs_cp = LQR_cp(Q[:,0], p[:,0], F[:,0], f[:,0], x_init[0], T, n_state, n_ctrl)
     tau_cp = tau_cp.T
     x_cp = tau_cp[:,:n_state]
     u_cp = tau_cp[:,n_state:]
 
-    C, c, R, S, F, f, x_init = [torch.Tensor(x).double() if x is not None else None
-        for x in [C, c, R, S, F, f, x_init]]
+    Q, p, A, B, C, D, c1, c2, x_init, u_lqr = [torch.Tensor(x).double() if x is not None else None
+        for x in [Q, p, A, B, C, D, c1, c2, x_init, u_lqr]]
 
-    u_lqr = None
-    LQR_DP  = pp.module.DP_LQR(n_state, n_ctrl, T)
-    Ks, ks = LQR_DP.DP_LQR_backward(C, c, F, f)
-    x_lqr, u_lqr, objs_lqr, tau = LQR_DP.DP_LQR_forward(x_init, C, c, F, f, Ks, ks)
+    lti = pp.module.LTI(A, B, C, D, c1, c2)
+    
+    LQR_DP  = pp.module.DP_LQR(n_state, n_ctrl, T, lti)
+    x_lqr, u_lqr, objs_lqr, tau = LQR_DP(x_init, Q, p)
     tau_lqr = torch.cat((x_lqr, u_lqr), 2)
     assert torch.allclose(tau_cp, tau_lqr[:,0]) 
-
-    lambda_dual = LQR_DP.DP_LQR_costates(tau, C, c, F)
-    
-    lambda_dual_test = []
-    
-    for t in range(T-1, -1, -1):
-        t_rev_new = T-1-t
-        tau_t = tau[t_rev_new]
-        Ct = C[t]
-        ct = c[t]
-        Ft = F[t]
-        Ct_x = Ct[:, :n_state, :]
-        ct_x = ct[:, :n_state]
-
-        if t == T-1:
-            lambda_final_test = lambda_tp1_test = Ct_x.bmm(tau_t.unsqueeze(2)).squeeze(2) + ct_x
-        else: 
-            Ft_T = Ft.transpose(1,2)
-            Ft_x_T = Ft_T[:, :n_state, :]
-            lambda_t_test = Ft_x_T .bmm(lambda_tp1_test.unsqueeze(2)).squeeze(2) + Ct_x.bmm(tau_t.unsqueeze(2)).squeeze(2) + ct_x
-            lambda_tp1_test = lambda_t_test
-            lambda_dual_test.append(lambda_t_test)
-            
-    lambda_dual_test.reverse()
-    lambda_dual_test.append(lambda_final_test)
-    lambda_dual_test= torch.stack(lambda_dual_test)
-
-    assert torch.allclose(lambda_dual, lambda_dual_test)
 
     print("Done")
 
 
 if __name__ == '__main__':
     test_LQR_linear()
+
