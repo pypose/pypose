5e46f55f4b916d76bac36f95d1de448ba44410f5
Chen Wang <chenwang@dr.com>
2022-12-11 23:47:43 -0500

    update ekf example

--- examples/module/ekf/tank_robot.py
+++ examples/module/ekf/tank_robot.py
@@ -1,115 +1,69 @@
-import math
-import pypose as pp
-import torch as torch
-import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import argparse
+from pypose.module import EKF
+import math, matplotlib.pyplot as plt
+import torch, pypose as pp, numpy as np
 
 
 class TankRobot(pp.module.System):
-    def __init__(self, dt, Q, R):
+    def __init__(self, Q, R):
         super(TankRobot, self).__init__()
-        self.state = torch.tensor([0.0, 0.0, 0.0])
-        self.input = torch.tensor([0.0, 0.0])
-        self.dt = dt
-        self.register_buffer("Q", torch.eye(self.A.shape) if Q is None else Q)
-        self.register_buffer("R", torch.eye(self.C.shape[0]) if R is None else R)
-        self.set_refpoint()
+        self.register_buffer("Q", Q)
+        self.register_buffer("R", R)
 
     def state_transition(self, state, input, t=None):
-        # input is torch.tensor([v, w]) where v is forward velo and w is angular velo
-        input_dt = torch.mul(input, self.dt)
-        theta = state[2] + input_dt[1]
-        v_x = input_dt[0] * torch.cos(theta)
-        v_y = input_dt[0] * torch.sin(theta)
-        w = torch.randn(state.shape) @ self.Q  # process noise
-        state = torch.tensor([state[0] + v_x, state[1] + v_y, theta]) + w
-        return state
+        theta = state[2] + input[1]
+        vx = input[0] * theta.cos()
+        vy = input[0] * theta.sin()
+        state = torch.stack([state[0] + vx, state[1] + vy, theta])
+        return state + self.noise(self.Q)
 
     def observation(self, state, input, t=None):
-        y = state  # observation is just state here
-        v = torch.randn(state.shape) @ self.R  # measurement noise
-        return y + v
+        return state + self.noise(self.R)
+
+    def noise(self, W):
+        r'''
+        Randomly generated batched noises.
+        '''
+        n = torch.randn(W.shape[:-1], device=W.device, dtype=W.dtype)
+        return pp.bmv(W, n)
 
 
-def createTimePlot(x, y, figname="Un-named plot", title=None, xlabel=None, ylabel=None):
-    f = plt.figure(figname)
-    plt.plot(x, y)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-    return f
-
-
-def color_fade(c1, c2, mix=0):  # lerp from color c1 (at mix=0) to c2 (mix=1)
-    return [c1[i] * mix + c2[i] * (1 - mix) for i in range(4)]
+def creatPlot(state, est):
+    N = state.shape[0]
+    state = state.cpu().numpy()
+    est = est.cpu().numpy()
+    w = torch.arange(0, N, dtype=torch.float).view(-1, 1) / N
+    c = torch.tensor([[1, 0, 0, 1]]).repeat(N, 1) * w + \
+        torch.tensor([[0, 0, 1, 1]]).repeat(N, 1) * (1 - w)
+    plt.quiver(state[:-1,0], state[:-1,1],
+               state[1:,0]-state[:-1,0], state[1:,1]-state[:-1,1],
+               scale_units="xy", angles="xy", scale=1, color=c.tolist())
+    plt.plot(est[:, 0], est[:, 1], '.-')
+    plt.legend(['True State', 'Estimated State'])
+    plt.show()
 
 
 if __name__ == "__main__":
 
-    x = 0.0  # starting x position
-    y = 0.0  # starting y position
-    theta = 0.0  # starting heading
-    state = torch.tensor([x, y, theta])
-    N = 100  # number of timesteps
-    dt = 1
-    time = torch.arange(0, N + 1) * dt
+    parser = argparse.ArgumentParser(description='IMU Preintegration')
+    parser.add_argument("--device", type=str, default='cpu', help="cuda or cpu")
+    args = parser.parse_args()
 
-    est_state = torch.tensor([10.0, 0.0, 0.0])  # initial state estimate
-    P = torch.eye(3)  # initial state covariance
+    device = args.device
+    N, q, r = 100, 0.2, 0.2  # steps, covariance of transition and observation noise
+    input  = torch.randn(N, 2, device=device) * 0.1 + torch.tensor([1, 0], device=device)
+    state  = torch.zeros(N, 3, device=device)               # true states
+    est    = torch.randn(N, 3, device=device) + 10          # estimation
+    obs    = torch.zeros(N, 3, device=device)               # observation
+    cov    = torch.eye(3, 3, device=device).repeat(N, 1, 1) # estimation covariance
+    Q      = torch.eye(3, device=device) * q                # covariance of transition
+    R      = torch.eye(3, device=device) * r                # covariance of observation
 
-    Q = 0.2 * torch.eye(3) + 0.1 * torch.randn(3, 3)
-    R = 0.2 * torch.eye(3) + 0.1 * torch.randn(3, 3)
-    model = TankRobot(dt, Q, R)  # system model
-    filter = pp.module.EKF(model)  # Kalman Filter to estimate the model
+    robot = TankRobot(Q, R).to(device)
+    ekf = EKF(robot, Q, R).to(device)
 
-    # keep history of following values
-    inputs = []
-    states = []
-    estimates = []
-    covariances = []
+    for i in range(N - 1):
+        state[i+1], obs[i] = robot(state[i], input[i])  # model measurement
+        est[i+1], cov[i+1] = ekf(est[i], obs[i], input[i], cov[i])
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    for i in time:
-        print("timestep {}".format(i))
-        u = torch.tensor([1, 0]) + 0.1 * torch.randn(2)  # random input
-
-        inputs.append(u)
-        states.append(state)  # real state
-        estimates.append(est_state)  # estimated state
-        covariances.append(P)
-
-        x = state[0]
-        y = state[1]
-        theta = state[2]
-
-        state, observation = model(state, u)  # model measurement
-        model.set_refpoint(state=state, input=u)
-        est_state, P = filter(est_state, observation, P, u)
-
-        color_path = tuple(color_fade([1, 0, 0, 1], [0, 0, 1, 1], mix=(i / N).item()))
-
-        arrow = plt.quiver(
-            x,
-            y,
-            state[0] - x,
-            state[1] - y,
-            scale_units="xy",
-            angles="xy",
-            scale=1,
-            color=color_path,
-        )
-        ax.add_artist(arrow)
-
-        dot = plt.Circle(
-            (est_state[0], est_state[1]),
-            math.hypot(P[0][0], P[1][1]),
-            fill=False,
-            color=color_path,
-        )
-        ax.add_artist(dot)
-
-    plt.show()
+    creatPlot(state, est)
--- pypose/module/kalman.py
+++ pypose/module/kalman.py
@@ -1,115 +1,159 @@
-import torch
 from torch import nn
-import pypose as pp
+import torch, pypose as pp
+from torch.linalg import pinv
 
 
 class EKF(nn.Module):
-    r"""
-    Linearized Extended Kalman Filter. 
-    
-    Args:
-        model (:obj:`System`): The noisy system model to estimate using this filter. 
-    """
+    r'''
+    Performs Batched Extended Kalman Filter (EKF).
 
-    def __init__(self, model):
+    Args:
+        model (:obj:`System`): The system model to be estimated, a subclass of
+            :obj:`pypose.module.System`.
+        Q (:obj:`Tensor`, optional): The covariance matrices of system transition noise.
+            Ignored if provided during each iteration. Default: ``None``
+        R (:obj:`Tensor`, optional): The covariance matrices of system observation noise.
+            Ignored if provided during each iteration. Default: ``None``
+
+    A non-linear system can be described as
+
+    .. math::
+        \begin{aligned}
+            \mathbf{x}_{k+1} &= \mathbf{f}(\mathbf{x}_k, \mathbf{u}_k, t_k) + \mathbf{w}_k,
+            \quad \mathbf{w}_k \sim \mathcal{N}(\mathbf{0}, \mathbf{Q})  \\
+            \mathbf{y}_{k} &= \mathbf{g}(\mathbf{x}_k, \mathbf{u}_k, t_k) + \mathbf{v}_k,
+            \quad \mathbf{v}_k \sim \mathcal{N}(\mathbf{0}, \mathbf{R})
+        \end{aligned}
+
+    It will be linearized automatically:
+
+    .. math::
+        \begin{align*}
+            \mathbf{z}_{k+1} = \mathbf{A}_{k}\mathbf{x}_{k} + \mathbf{B}_{k}\mathbf{u}_{k}
+                             + \mathbf{c}_{k}^1 + \mathbf{w}_k\\
+            \mathbf{y}_{k} = \mathbf{C}_{k}\mathbf{x}_{k} + \mathbf{D}_{k}\mathbf{u}_{k}
+                           + \mathbf{c}_{k}^2 + \mathbf{v}_k\\
+        \end{align*}
+
+    EKF can be described as the following five equations, where the subscript :math:`\cdot_{k}`
+    is omited for simplicity.
+
+    1. Priori State Estimation.
+
+        .. math::
+            \mathbf{x}^{-} = \mathbf{A}\mathbf{x} + \mathbf{B}\mathbf{u}_k + \mathbf{c}_1
+
+    2. Priori Covariance Propagation.
+
+        .. math::
+            \mathbf{P}^{-} = \mathbf{A}\mathbf{P}\mathbf{A}^{T} + \mathbf{Q}
+
+    3. Update Kalman Gain
+
+        .. math::
+            \mathbf{K} = \mathbf{P}\mathbf{C}^{T}
+                        (\mathbf{C}\mathbf{P} \mathbf{C}^{T} + \mathbf{R})^{-1}
+
+    4. Posteriori State Estimation
+
+        .. math::
+            \mathbf{x}^{+} = \mathbf{x}^{-} + \mathbf{K} (\mathbf{y} -
+                        \mathbf{C}\mathbf{x}^{-} - \mathbf{D}\mathbf{u} - \mathbf{c}_2)
+
+    5. Posteriori Covariance Estimation
+
+        .. math::
+            \mathbf{P} = (\mathbf{I} - \mathbf{K}\mathbf{C}) \mathbf{P}^{-}
+
+    where superscript :math:`\cdot^{-}` and :math:`\cdot^{+}` denote the priori and
+    posteriori estimation, respectively.
+
+    Example:
+        1. Initialize model and filter
+
+        >>> import torch, pypose as pp
+        >>> model = pp.module.System()  # System model
+        >>> filter = pp.module.EKF(model)
+
+        2. Calculate filter for one iteration
+
+        >>> # u is system input
+        >>> state, observation = model(state, u)  # model measurement
+        >>> est_state, P = filter(est_state, observation, P, u)
+
+    Note:
+        Implementation is based on Section 5.1 of this book
+
+        * Dan Simon, `Optimal State Estimation: Kalman, H∞, and Nonlinear Approaches
+          <https://onlinelibrary.wiley.com/doi/epdf/10.1002/0470045345.fmatter>`_,
+          Cleveland State University, 2006
+    '''
+    def __init__(self, model, Q=None, R=None):
         super().__init__()
+        self.set_uncertainty(Q=Q, R=R)
         self.model = model
 
-    def forward(self, x, y, P, U):
-        r"""
-        Calculates filter one timestep.
+    def forward(self, x, y, u, P, Q=None, R=None):
+        r'''
+        Performs one step estimation.
 
         Args:
-            x (:obj:`Tensor`): estimated system state of previous timestep
-            y (:obj:`Tensor`): system observation at current timestep (measurement)
-            P (:obj:`Tensor`): state covariance of previous timestep
-            U (:obj:`Tensor`): system input at current timestep
-        Returns:
-            x (:obj:`Tensor`): estimated system state at current timestep (posteriori state estimate)
-            P (:obj:`Tensor`): estimated state covariance at current timestep (posteriori covariance)
+            x (:obj:`Tensor`): estimated system state of previous step
+            y (:obj:`Tensor`): system observation at current step (measurement)
+            u (:obj:`Tensor`): system input at current step
+            P (:obj:`Tensor`): state estimation covariance of previous step
+            Q (:obj:`Tensor`, optional): covariance of system transition model
+            R (:obj:`Tensor`, optional): covariance of system observation model
 
-        Uses a Linearized System Model, thus it is a Linearized Extended Kalman Filter. 
-        If not defined, :obj:`System.A`, :obj:`System.B`, :obj:`System.C` are computed each call using torch.autograd.functional.jacobian(), see :obj:`System`
-        
-        Note:
-            To use this filter, your System subclass must define Q (process noise covariance) and R (measurement noise covariance). 
-            Your System should add noise to the returned state and observation by the following equations:
+        Return:
+            list of :obj:`Tensor`: posteriori state and covariance estimation
+        '''
+        # Upper cases are matrices, lower cases are vectors
+        self.model.set_refpoint(state=x, input=u)
+        I = torch.eye(P.shape[-1], device=P.device, dtype=P.dtype)
+        A, B = self.model.A, self.model.B
+        C, D = self.model.C, self.model.D
+        c1, c2 = self.model.c1, self.model.c2
+        Q = Q if Q is not None else self.Q
+        R = R if R is not None else self.R
 
-            .. math::
-                w_k \sim (0, Q) \\
-                x_k = x_k + w_k
-
-            .. math::
-                v_k \sim (0, R) \\
-                y_k = y_k + v_k
-
-        
-        For each :obj:`forward()` call, the Kalman Filter Equations are executed as follows:
-
-        Note:
-            Since System.x is a row-vector, before doing these calculations, x is transposed into a column vector. 
-
-        1. Predict State (Priori State Estimate) using State Transition Equation.
-        
-        .. math::
-            \mathbf{x} = f(x_{k-1}, U_k)
-        
-        For a Linear System, that is:
-
-        .. math::
-            \mathbf{x} = \mathbf{A}\mathbf{x_{k-1}} + \mathbf{B}\mathbf{U}
-        
-        2. Predict State Covariance.
-
-        .. math::
-            \mathbf{P} = \mathbf{A}\mathbf{P}\mathbf{A}^{T} + \mathbf{Q}
-        
-        3. Update Kalman Gain
-
-        .. math::
-            \mathbf{K} = \mathbf{P}\mathbf{C}^{T} (\mathbf{C}\mathbf{P}\mathbf{C}^{T} + \mathbf{R})^{-1}
-        
-        4. Correction Step (Posteriori State Estimate)
-
-        .. math::
-            \mathbf{x} = \mathbf{x} + \mathbf{K} (\mathbf{y} - \mathbf{C}\mathbf{x}) \\
-            \mathbf{P} = (\mathbf{I} - \mathbf{K}\mathbf{C}) \mathbf{P}
-        
-        Example:
-
-            1. Initialize model and filter
-
-            >>> import torch
-            >>> import pypose as pp
-            >>> model = pp.module.System()  # System model
-            >>> filter = pp.module.EKF(model)
-
-            2. Calculate filter over one timestep
-
-            >>> # u is system input
-            >>> state, observation = model(state, u)  # model measurement
-            >>> model.set_refpoint(state=state, input=u)
-            >>> est_state, P = filter(est_state, observation, P, u)
-
-        Note:
-            Implementation is based on section 5.1 of this book
-
-            * Dan Simon, `Optimal State Estimation: Kalman, H∞, and Nonlinear Approaches <https://onlinelibrary.wiley.com/doi/epdf/10.1002/0470045345.fmatter>`_, Cleveland State University, 2006
-        
-
-        """
-
-        x_tposed = x.T
-
-        # Predict
-        x_tposed = self.model.state_transition(x_tposed, U)
-        P = self.model.A.matmul(P).matmul(self.model.A.T) + self.model.Q
-        # Update
-        S = self.model.C.matmul(P).matmul(self.model.C.T) + self.model.R
-        K = P.matmul(self.model.C.T).matmul(torch.linalg.inv(S))
-        x_tposed = x_tposed + K.matmul(y - self.model.C.matmul(x_tposed))
-
-        sh = K.matmul(self.model.C)  # to get the shape
-        P = (torch.eye(sh.shape[0]) - K.matmul(self.model.C)).matmul(P)
-        x = x_tposed.T
+        x = pp.bmv(A, x) + pp.bmv(B, u) + c1      # 1. System transition
+        P = A @ P @ A.mT + Q                      # 2. Covariance predict
+        K = P @ C.mT @ pinv(C @ P @ C.mT + R)     # 3. Kalman gain
+        e = y - pp.bmv(C, x) - pp.bmv(D, u) - c2  #    predicted observation error
+        x = x + pp.bmv(K, e)                      # 4. Posteriori state
+        P = (I - K @ C) @ P                       # 5. Posteriori covariance
         return x, P
+
+    @property
+    def Q(self):
+        r'''
+        The covariance of system transition noise.
+        '''
+        if not hasattr(self, '_Q'):
+            raise NotImplementedError('Call set_uncertainty() to defines\
+                                        transition covariance Q.')
+        return self._Q
+
+    @property
+    def R(self):
+        r'''
+        The covariance of system observation noise.
+        '''
+        if not hasattr(self, '_R'):
+            raise NotImplementedError('Call set_uncertainty() to defines\
+                                        transition covariance R.')
+        return self._R
+
+    def set_uncertainty(self, Q=None, R=None):
+        r'''
+        Set the covariance matrices of transition noise and observation noise.
+
+        Args:
+            Q (:obj:`Tensor`): batched square covariance matrices of transition noise.
+            R (:obj:`Tensor`): batched square covariance matrices of observation noise.
+        '''
+        if Q is not None:
+            self.register_buffer("_Q", Q)
+        if R is not None:
+            self.register_buffer("_R", R)
