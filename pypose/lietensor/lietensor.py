2c3465fac1059d2c4c4f07b33bbb4113f4286ffe
Chen Wang <chenwang@dr.com>
2022-12-14 13:37:52 -0500

    fix comment

--- pypose/lietensor/lietensor.py
+++ pypose/lietensor/lietensor.py
@@ -1105,7 +1105,7 @@ class LieTensor(torch.Tensor):
 
         roll = torch.atan2(t0, t1)
         pitch = torch.asin(t2.clamp(-1, 1))
-        # sigularity when pitch angle = +/- 90
+        # sigularity when pitch angle ~ +/-pi/2
         flag = -0.998 < t2 < 0.998
         yaw1 = torch.atan2(t3, t4)
         yaw2 = -2 * torch.sign(t2) * torch.atan2(x, w)
