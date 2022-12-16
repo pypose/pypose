ae3878a27567af62b30e2133942f7451007b35b5
hakunanh <hannahh6069@gmail.com>
2022-12-16 08:10:57 -0500

    change order of T and n_batch

--- test/module/test_LQR.py
+++ test/module/test_LQR.py
@@ -6,7 +6,7 @@ import cvxpy as cp
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def LQR_cp(Q, p, F, f, x_init, T, n_state, n_ctrl): 
+def lqr_cp(Q, p, F, f, x_init, T, n_state, n_ctrl): 
 
     tau = cp.Variable((n_state+n_ctrl, T))
 
@@ -25,30 +25,30 @@ def LQR_cp(Q, p, F, f, x_init, T, n_state, n_ctrl):
     prob.solve()
     assert 'optimal' in prob.status
     return torch.tensor(tau.value), torch.tensor([obj_t.value for obj_t in objs])
+ 
 
-
-def test_LQR_linear():
+def test_lqr_linear():
 
     torch.manual_seed(0)
-    n_batch, n_state, n_ctrl, T = 2, 4, 3, 5
+    n_batch, n_state, n_ctrl, T = 2, 4, 2, 5
     n_sc = n_state + n_ctrl
-    Q = torch.randn(T, n_batch, n_sc, n_sc)
-    Q = torch.matmul(Q.mT, Q)
-    p = torch.randn(T, n_batch, n_sc)
+    Q = torch.randn(n_batch, T, n_sc, n_sc)
+    Q = Q.mT @ Q
+    p = torch.randn(n_batch, T, n_sc)
     alpha = 0.2
     A = torch.tile(torch.eye(n_state) + alpha * torch.randn(n_state, n_state), (n_batch, 1, 1))
-    B = torch.tile(torch.randn(n_state, n_ctrl), (n_batch, 1, 1))
+    B = torch.randn(n_batch, n_state, n_ctrl)
     C = torch.tile(torch.eye(n_state), (n_batch, 1, 1))
     D = torch.tile(torch.zeros(n_state, n_ctrl), (n_batch, 1, 1))
     c1 = torch.tile(torch.randn(n_state), (n_batch, 1))
     c2 = torch.tile(torch.zeros(n_state), (n_batch, 1))
     x_init = torch.randn(n_batch, n_state)
     u_lqr = torch.tile(torch.zeros(n_ctrl), (n_batch, 1))
-    F = torch.cat((A, B), axis=2)
-    F = torch.tile(F, (T, 1, 1, 1))
+    F = torch.cat((A[0,:,:], B[0,:,:]), dim = 1)
+    F = torch.tile(F, (T, 1, 1))
     f = torch.tile(c1, (T, 1, 1))
 
-    tau_cp, objs_cp = LQR_cp(Q[:,0], p[:,0], F[:,0], f[:,0], x_init[0], T, n_state, n_ctrl)
+    tau_cp, objs_cp = lqr_cp(Q[0,:,:,:], p[0,:,:], F, f[:,0], x_init[0], T, n_state, n_ctrl)
     tau_cp = tau_cp.T
     x_cp = tau_cp[:,:n_state]
     u_cp = tau_cp[:,n_state:]
@@ -58,29 +58,29 @@ def test_LQR_linear():
 
     lti = pp.module.LTI(A, B, C, D, c1, c2)
     
-    LQR_DP  = pp.module.DP_LQR(n_state, n_ctrl, T, lti)
-    x_lqr, u_lqr, objs_lqr, tau = LQR_DP(x_init, Q, p)
+    LQRDP  = pp.module.DPLQR(lti)
+    x_lqr, u_lqr, objs_lqr, tau = LQRDP(x_init, Q, p)
     tau_lqr = torch.cat((x_lqr, u_lqr), 2)
     assert torch.allclose(tau_cp, tau_lqr[:,0]) 
 
     print("Done")
 
 
-def test_LQR_ltv_random():
+def test_lqr_ltv_random():
 
     torch.manual_seed(1)
     n_batch, n_state, n_ctrl, T = 2, 4, 3, 5
     n_sc = n_state + n_ctrl
 
-    Q = torch.randn(T, n_batch, n_sc, n_sc)
-    Q = torch.matmul(Q.mT, Q)
-    p = torch.randn(T, n_batch, n_sc)
-    A = torch.randn(T, n_batch, n_state, n_state)
-    B = torch.randn(T, n_batch, n_state, n_ctrl)
-    C = torch.tile(torch.eye(n_state), (T, n_batch, 1, 1))
-    D = torch.tile(torch.zeros(n_state, n_ctrl), (T, n_batch, 1, 1))
-    c1 = torch.randn(T, n_batch, n_state)
-    c2 = torch.tile(torch.zeros(n_state), (T, n_batch, 1))
+    Q = torch.randn(n_batch, T, n_sc, n_sc)
+    Q = Q.mT @ Q
+    p = torch.randn(n_batch, T, n_sc)
+    A = torch.randn(n_batch, T, n_state, n_state)
+    B = torch.randn(n_batch, T, n_state, n_ctrl)
+    C = torch.tile(torch.eye(n_state), (n_batch, T, 1, 1))
+    D = torch.tile(torch.zeros(n_state, n_ctrl), (n_batch, T, 1, 1))
+    c1 = torch.randn(n_batch, T, n_state)
+    c2 = torch.tile(torch.zeros(n_state), (n_batch, T, 1))
     x_init = torch.randn(n_batch, n_state)
     
     class LTV(pp.module.System):
@@ -97,17 +97,17 @@ def test_LQR_ltv_random():
 
         def state_transition(self, state, input, t=None):
             for t in range(self.T):
-                A = self.A[t]
-                B = self.B[t]
-                c1 = self.c1[t]
+                A = self.A[:,t,:,:]
+                B = self.B[:,t,:,:]
+                c1 = self.c1[:,t,:]
 
             return torch.einsum('...ik,...k->...i', [A, state]) + torch.einsum('...ik,...k->...i', [B, input]) + c1
 
         def observation(self, state, input, t=None):
             for t in range(self.T):
-                C = self.C[t]
-                D = self.D[t]
-                c2 = self.c2[t]
+                C = self.C[:,t,:,:]
+                D = self.D[:,t,:,:]
+                c2 = self.c2[:,t,:]
 
             return torch.einsum('...ik,...k->...i', [C, state]) + torch.einsum('...ik,...k->...i', [D, input]) + c2
 
@@ -160,13 +160,13 @@ def test_LQR_ltv_random():
             self._c2 = c2
 
     ltv = LTV(T, A, B, C, D, c1, c2)
-    LQR_DP  = pp.module.DP_LQR(n_state, n_ctrl, T, ltv)
-    x_lqr, u_lqr, objs_lqr, tau = LQR_DP(x_init, Q, p)
+    LQRDP  = pp.module.DPLQR(ltv)
+    x_lqr, u_lqr, objs_lqr, tau = LQRDP(x_init, Q, p)
 
     print("Done")
 
 
 if __name__ == '__main__':
-    test_LQR_linear()
-    test_LQR_ltv_random()
+    test_lqr_linear()
+    test_lqr_ltv_random()
 
