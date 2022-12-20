1. Fix Matrix square root 2. Modified covariance calculation method

pypose/module/ukf.py
@@ -1,9 +1,42 @@
 import torch
 from torch import nn
 from ..basics import bmv
-from torch.linalg import pinv
+from torch.linalg import pinv,matrix_power
+from torch.autograd import Function
+import numpy as np
+import scipy.linalg
 
 
+class MatrixSquareRoot(Function):
+
+    """Square root of a positive definite matrix.
+    https://github.com/steveli/pytorch-sqrtm/blob/master/sqrtm.py
+    NOTE: matrix square root is not differentiable for matrices with
+          zero eigenvalues.
+    """
+    @staticmethod
+    def forward(ctx, input):
+        m = input.detach().cpu().numpy().astype(np.float_)
+        sqrtm = torch.from_numpy(scipy.linalg.sqrtm(m).real).to(input)
+        ctx.save_for_backward(sqrtm)
+        return sqrtm
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = None
+        if ctx.needs_input_grad[0]:
+            sqrtm, = ctx.saved_tensors
+            sqrtm = sqrtm.data.cpu().numpy().astype(np.float_)
+            gm = grad_output.data.cpu().numpy().astype(np.float_)
+
+            # Given a positive semi-definite matrix X,
+            # since X = X^{1/2}X^{1/2}, we can compute the gradient of the
+            # matrix square root dX^{1/2} by solving the Sylvester equation:
+            # dX = (d(X^{1/2})X^{1/2} + X^{1/2}(dX^{1/2}).
+            grad_sqrtm = scipy.linalg.solve_sylvester(sqrtm, sqrtm, gm)
+
+            grad_input = torch.from_numpy(grad_sqrtm).to(grad_output)
+        return grad_input
 class UKF(nn.Module):
     r'''
     Performs Batched Unscented Kalman Filter (UKF).
@@ -23,12 +56,17 @@ class UKF(nn.Module):
         '''
         x_sigma = []
         y_sigma = []
+        sqrtm = MatrixSquareRoot.apply
+
         for loop in range(1, self.loop_range):
 
             if loop <= self.dim:
-                x_ = x + torch.sqrt_(self.weight * P).mT[loop-1]
+
+                x_ = x +  sqrtm(self.dim * P).mT[loop-1]
             else:
-                x_ = x - torch.sqrt_(self.weight * P).mT[loop - self.dim-1]
+
+                x_ = x - sqrtm(self.dim * P).mT[loop - self.dim-1]
+
             y_ = bmv(C, x_) + bmv(D, u) + c2    # compute Observation
             x_sigma.append(x_)
             y_sigma.append(y_)
@@ -40,11 +78,11 @@ class UKF(nn.Module):
         compute mix covariance
         '''
         p_estimate = torch.zeros((P.shape), device=P.device, dtype=P.dtype)
-        e_x = torch.sub(x_sigma,x_estimate).unsqueeze(1)
-        e_y = torch.sub(y_sigma,y_estimate).unsqueeze(1)
-        p_estimate+= torch.sum(torch.bmm(e_x,e_y.permute(0, 2, 1)))
+        e_x = torch.sub(x_sigma,x_estimate).unsqueeze(2)
+        e_y = torch.sub(y_sigma,y_estimate).unsqueeze(2)
+        p_estimate+= torch.sum(torch.bmm(e_x,e_y.permute(0, 2, 1)),dim = 0)
 
-        p_estimate = torch.sum(torch.diag_embed(p_estimate),dim = 0)
+        # p_estimate =torch.diag_embed(p_estimate)[0]
         return self.weight * p_estimate
 
     def compute_conv(self, P, estimate, sigma, noise=0):
@@ -52,9 +90,9 @@ class UKF(nn.Module):
         compute covariance
         '''
         p_estimate = torch.zeros((P.shape), device=P.device, dtype=P.dtype)
-        e = torch.sub(sigma,estimate).unsqueeze(1)
-        p_estimate += torch.sum(torch.bmm(e, e.permute(0, 2, 1)))
-        p_estimate = torch.sum(torch.diag_embed(p_estimate),dim = 0)
+        e = torch.sub(sigma,estimate).unsqueeze(2)
+        p_estimate += torch.sum(torch.bmm(e, e.permute(0, 2, 1)),dim =0 )
+        # p_estimate =torch.diag_embed(p_estimate)[0]
 
 
         return self.weight*p_estimate + noise