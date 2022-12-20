1. File standardization(ekf,ukf) 2. Bug fixes 3. Using torch replace loop 4. Header standardization 5. Remove Matrix covariance approximation

pypose/module/ukf.py
@@ -18,51 +18,43 @@ class UKF(nn.Module):
         return 1 / (2 * self.dim)
 
     def compute_sigma(self, x, u, P, C, D, c2):  # compute sigma point
-
+        r'''
+        compute sigma point
+        '''
         x_sigma = []
         y_sigma = []
         for loop in range(1, self.loop_range):
 
             if loop <= self.dim:
-                x_ = x + torch.sqrt_(self.weight * P).mT
+                x_ = x + torch.sqrt_(self.weight * P).mT[loop-1]
             else:
-                x_ = x - torch.sqrt_(self.weight * P).mT
-
-            y_ = bmv(C, x_) + bmv(D, u) + c2  # compute Observation
+                x_ = x - torch.sqrt_(self.weight * P).mT[loop - self.dim-1]
+            y_ = bmv(C, x_) + bmv(D, u) + c2    # compute Observation
             x_sigma.append(x_)
             y_sigma.append(y_)
 
-        return torch.cat(x_sigma, dim=0), torch.cat(y_sigma, dim=0)
+        return torch.cat(x_sigma, dim=0).reshape(-1,self.dim), torch.cat(y_sigma, dim=0).reshape(-1,self.dim)
 
-    def compute_conv_mix(self, P, x_estimate, x_sigma, y_estimate, y_sigma):  # compute mix covariance
+    def compute_conv_mix(self, P, x_estimate, x_sigma, y_estimate, y_sigma):
+        r'''
+        compute mix covariance
+        '''
+        p_estimate = torch.zeros((P.shape), device=P.device, dtype=P.dtype)
+        e_x = torch.sub(x_sigma,x_estimate).unsqueeze(1)
+        e_y = torch.sub(y_sigma,y_estimate).unsqueeze(1)
+        p_estimate+= torch.sum(torch.bmm(e_x,e_y.permute(0, 2, 1)))
 
+        return self.weight * p_estimate
+
+    def compute_conv(self, P, estimate, sigma, noise=0):
+        r'''
+        compute covariance
+        '''
         P_estimate = torch.zeros((P.shape), device=P.device, dtype=P.dtype)
-        for loop in range(1, self.loop_range):
-            e_x = x_sigma[loop - 1] - x_estimate
-            e_y = y_sigma[loop - 1] - y_estimate
+        e = torch.sub(sigma,estimate).unsqueeze(1)
+        P_estimate += torch.sum(torch.bmm(e, e.permute(0, 2, 1)))
 
-            P_estimate += torch.matmul(e_x, e_y.t()) * self.weight
-
-        return P_estimate
-
-    def compute_conv(self, P, estimate, sigma, noise=0):  # compute covariance
-
-        P_estimate = torch.zeros((P.shape), device=P.device, dtype=P.dtype)
-
-        for loop in range(1, self.loop_range):
-            e = sigma[loop - 1] - estimate
-            P_estimate += self.weight * torch.matmul(e, e.t())
-
-        return P_estimate + noise
-
-    def matrix_conv(self, P, estimate, sigma,
-                    noise):  # Optimal State Estimation: Kalman, H∞, and Nonlinear Approaches 14.49 Covariance matrix approximation， Is this correct ?
-
-        P_estimate = torch.zeros((P.shape), device=P.device, dtype=P.dtype)
-        for loop in range(1, self.loop_range):
-            print(estimate)
-            P_estimate += estimate @ P @ estimate.t() * self.weight
-        return P_estimate + noise
+        return self.weight*P_estimate + noise
 
     def forward(self, x, y, u, P, Q=None, R=None):
         r'''

test/module/test_ekf.py
@@ -3,9 +3,9 @@ import pypose as pp
 import torch as torch
 
 
-class TestKalman:
+class TestEKF:
 
-    def test_kalman(self):
+    def test_ekf(self):
 
         class NTI(pp.module.System):
             def __init__(self):
@@ -37,9 +37,10 @@ class TestKalman:
             states[i+1], observ[i] = model(states[i] + w, inputs[i])
             estim[i+1], P[i+1] = ekf(estim[i], observ[i] + v, inputs[i], P[i], Q, R)
         error =  (states - estim).norm(dim=-1)
+
         assert torch.all(error[0] - error[-1] > 0), "Filter error last step too large."
 
 
 if __name__ == '__main__':
-    test = TestKalman()
-    test.test_kalman()
+    test = TestEKF()
+    test.test_ekf()

test/module/test_ukf.py
@@ -19,7 +19,7 @@ class TestUKF:
         model = NTI().to(device)
         ukf = pp.module.UKF(model).to(device)
 
-        T, N = 20, 2  # steps, state dim
+        T, N = 5, 2  # steps, state dim
         states = torch.zeros(T, N, device=device)
         inputs = torch.randn(T, N, device=device)
         observ = torch.zeros(T, N, device=device)
@@ -31,13 +31,12 @@ class TestUKF:
         estim = torch.randn(T, N, device=device) * p
 
         for i in range(T - 1):
-            print('%d is start' % (i + 1))
             w = q * torch.randn(N, device=device)  # transition noise
             v = r * torch.randn(N, device=device)  # observation noise
             states[i + 1], observ[i] = model(states[i] + w, inputs[i])
             estim[i + 1], P[i + 1] = ukf(estim[i], observ[i] + v, inputs[i], P[i], Q, R)
         error = (states - estim).norm(dim=-1)
-
+        print(error)
         assert torch.all(error[0] - error[-1] > 0), "Filter error last step too large."
 
 