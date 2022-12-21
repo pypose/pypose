23623b710ea76b9834db177aa0325117872ea396
xiaohaitao <756745172@qq.com>
2022-12-21 10:52:58 +0800

    1. Add Differentiable Matrix Square Root

--- pypose/module/ukf.py
+++ pypose/module/ukf.py
@@ -1,42 +1,10 @@
 import torch
 from torch import nn
 from ..basics import bmv
-from torch.linalg import pinv,matrix_power
-from torch.autograd import Function
-import numpy as np
-import scipy.linalg
+from torch.linalg import pinv
+from pypose.basics.utils import *
 
 
-class MatrixSquareRoot(Function):
-
-    """Square root of a positive definite matrix.
-    https://github.com/steveli/pytorch-sqrtm/blob/master/sqrtm.py
-    NOTE: matrix square root is not differentiable for matrices with
-          zero eigenvalues.
-    """
-    @staticmethod
-    def forward(ctx, input):
-        m = input.detach().cpu().numpy().astype(np.float_)
-        sqrtm = torch.from_numpy(scipy.linalg.sqrtm(m).real).to(input)
-        ctx.save_for_backward(sqrtm)
-        return sqrtm
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        grad_input = None
-        if ctx.needs_input_grad[0]:
-            sqrtm, = ctx.saved_tensors
-            sqrtm = sqrtm.data.cpu().numpy().astype(np.float_)
-            gm = grad_output.data.cpu().numpy().astype(np.float_)
-
-            # Given a positive semi-definite matrix X,
-            # since X = X^{1/2}X^{1/2}, we can compute the gradient of the
-            # matrix square root dX^{1/2} by solving the Sylvester equation:
-            # dX = (d(X^{1/2})X^{1/2} + X^{1/2}(dX^{1/2}).
-            grad_sqrtm = scipy.linalg.solve_sylvester(sqrtm, sqrtm, gm)
-
-            grad_input = torch.from_numpy(grad_sqrtm).to(grad_output)
-        return grad_input
 class UKF(nn.Module):
     r'''
     Performs Batched Unscented Kalman Filter (UKF).
@@ -56,16 +24,16 @@ class UKF(nn.Module):
         '''
         x_sigma = []
         y_sigma = []
-        sqrtm = MatrixSquareRoot.apply
+        matrix_sqrt = MPA_Lya.apply
 
         for loop in range(1, self.loop_range):
-
+            nP = (self.dim * P).unsqueeze(0)
             if loop <= self.dim:
 
-                x_ = x +  sqrtm(self.dim * P).mT[loop-1]
+                x_ = x + matrix_sqrt(nP)[0].mT[loop-1]
             else:
 
-                x_ = x - sqrtm(self.dim * P).mT[loop - self.dim-1]
+                x_ = x - matrix_sqrt(nP)[0].mT[loop - self.dim-1]
 
             y_ = bmv(C, x_) + bmv(D, u) + c2    # compute Observation
             x_sigma.append(x_)
@@ -73,29 +41,26 @@ class UKF(nn.Module):
 
         return torch.cat(x_sigma, dim=0).reshape(-1,self.dim), torch.cat(y_sigma, dim=0).reshape(-1,self.dim)
 
-    def compute_conv_mix(self, P, x_estimate, x_sigma, y_estimate, y_sigma):
+    def compute_conv_mix(self, x_estimate, x_sigma, y_estimate, y_sigma):
         r'''
         compute mix covariance
         '''
-        # p_estimate = torch.zeros((P.shape), device=P.device, dtype=P.dtype)
+
         e_x = torch.sub(x_sigma,x_estimate).unsqueeze(2)
         e_y = torch.sub(y_sigma,y_estimate).unsqueeze(2)
         p_estimate = torch.sum(torch.bmm(e_x,e_y.permute(0, 2, 1)),dim = 0)
 
-        # p_estimate =torch.diag_embed(p_estimate)[0]
         return self.weight * p_estimate
 
-    def compute_conv(self, P, estimate, sigma, noise=0):
+    def compute_conv(self, estimate, sigma, noise=0):
         r'''
         compute covariance
         '''
 
         e = torch.sub(sigma,estimate).unsqueeze(2)
         p_estimate = torch.sum(torch.bmm(e, e.permute(0, 2, 1)),dim =0 )
-        # p_estimate =torch.diag_embed(p_estimate)[0]
 
-
-        return self.weight*p_estimate + noise
+        return self.weight * p_estimate + noise
 
     def forward(self, x, y, u, P, Q=None, R=None):
         r'''
@@ -129,11 +94,11 @@ class UKF(nn.Module):
         # compute sigma point,mean,covariance
         x_sigma, y_sigma = self.compute_sigma(x, u, P, C, D, c2)
         x_estimate = self.weight * torch.sum(x_sigma, dim=0)
-        Pk = self.compute_conv(P, x_estimate, x_sigma, Q)
+        Pk = self.compute_conv(x_estimate, x_sigma, Q)
         x_sigma, y_sigma = self.compute_sigma(x_estimate, u, Pk, C, D, c2)
         y_estimate = self.weight * torch.sum(y_sigma, dim=0)
-        Py = self.compute_conv(P, y_estimate, y_sigma, R)
-        Pxy = self.compute_conv_mix(Py, x_estimate, x_sigma, y_estimate, y_sigma)
+        Py = self.compute_conv(y_estimate, y_sigma, R)
+        Pxy = self.compute_conv_mix(x_estimate, x_sigma, y_estimate, y_sigma)
 
         # Equation
         K = Pxy @ pinv(Py)
