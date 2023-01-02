10b75d698d52b84f56271450455df77f5b120514
xiao haitao <48181529+HongLouyemeng@users.noreply.github.com>
2023-01-03 02:26:54 +0800

    synchronization bmv

--- pypose/basics/ops.py
+++ pypose/basics/ops.py
@@ -6,21 +6,18 @@ import mpmath as mpm
 def bmv(input, vec, *, out=None):
     r'''
     Performs batched matrix-vector product.
-
     Args:
         input (:obj:`Tensor`): matrices to be multiplied.
         vec (:obj:`Tensor`): vectors to be multiplied.
-    
+
     Return:
         out (:obj:`Tensor`): the output tensor.
-
     Note:
         The ``input`` has to be a (:math:`\cdots\times n \times m`) tensor,
         the ``vec`` has to be a (:math:`\cdots\times m`) tensor,
         and ``out`` will be a (:math:`\cdots\times n`) tensor.
         Different from ``torch.mv``, which is not broadcast, this function
         is broadcast and supports batched product.
-
     Example:
         >>> matrix = torch.randn(2, 1, 3, 2)
         >>> vec = torch.randn(1, 2, 2)
@@ -30,6 +27,8 @@ def bmv(input, vec, *, out=None):
     '''
     assert input.ndim >= 2 and vec.ndim >= 1, 'Input arguments invalid'
     assert input.shape[-1] == vec.shape[-1], 'matrix-vector shape invalid'
+    input = input.tensor() if isinstance(input, LieTensor) else input
+    vec = vec.tensor() if isinstance(vec, LieTensor) else vec
     return torch.matmul(input, vec.unsqueeze(-1), out=out).squeeze(-1)
 
 
