8439893e9605ed7359b2a459347997ef25dd9f2c
Haitao Xiao <48181529+HongLouyemeng@users.noreply.github.com>
2023-01-04 22:57:14 +0800

    Update bicycle.py
    fix bicycle url

--- examples/module/filter/bicycle.py
+++ examples/module/filter/bicycle.py
@@ -35,7 +35,7 @@ def model_factory(robot, model_name, device, Q, R):
 class Bicycle(pp.module.System):
     '''
     This is an implementation of the 2D Bicycle kinematic model,
-    see: https://dingyan89.medium.com/simple-understanding-of-kinematic-bicycle-model
+    see: https://dingyan89.medium.com/simple-understanding-of-kinematic-bicycle-model-81cac6420357
     The robot is given a rotational and forward velocity, and traverses the 2D plane accordingly.
     This model is Nonlinear Time Invariant (NTI) and can be filtered with the ``pp.module.EKF``
     and  ``pp.module.UKF``.
