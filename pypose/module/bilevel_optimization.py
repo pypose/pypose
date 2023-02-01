6e36762b4f6522642724005350550fb4e158b412
afp5571 <afp5571@psu.edu>
2023-02-01 20:01:27 +0530

    Pseudo-code for bi-level optimization

---
+++ pypose/module/bilevel_optimization.py
@@ -0,0 +1,42 @@
+import pypose as pp
+import torch
+
+class someModel(torch.nn.Module):
+    # Define model here
+
+class LowOptimization(torch.nn.Module):
+    def __init__(self, model, constraints, solver, etc.):
+        super.__init__()
+
+    def func(self):
+        def forward(self) -> tuple(tau, mu):
+            result = self.solver.solve(model, constraints)
+            self.constrains = result.constraints
+            return result.optimized_parameters
+        
+        def backward():
+    
+
+class HighOptimization(torch.nn.Module):
+    def __init__(self, low_optimizer, torch.optim.optimizer, etc):
+        super.__init__()
+
+    def forward(self):
+        tau, mu = low_optimizer.forward()
+        # some loss calculation
+        return loss
+    
+    def backward():
+    
+    def solve():
+        while(not_optimized):
+            loss=forward
+            loss.backward
+            optimizer
+        return self.low_optimizer.model.parameters()
+
+if __name__ == "__main__":
+    model = someModel
+    low_opt = LowOptimization(model, etc)
+    high_opt = HighOptimization()
+    optimized_parameters = high_opt.solve()
