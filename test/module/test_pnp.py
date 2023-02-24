0379da05b94948d6d4b1b3bbc7f36e39bd99a378
Jingnan Shi <jnshi@mit.edu>
2023-02-24 12:39:44 -0500

    more epnp test stub

--- test/module/test_pnp.py
+++ test/module/test_pnp.py
@@ -2,14 +2,22 @@ import pypose as pp
 import torch
 
 
-def test_epnp_5pts():
-    # TODO: Implement this
-    return
+class TestEPnP:
+    def test_epnp_5pts(self):
+        # TODO: Implement this
+        return
 
-def test_epnp_10pts():
-    # TODO: Implement this
-    return
+    def test_epnp_10pts(self):
+        # TODO: Implement this
+        return
 
-def test_epnp_random():
-    # TODO: Implement this
-    return
+    def test_epnp_random(self):
+        # TODO: Implement this
+        return
+
+
+if __name__ == "__main__":
+    epnp_fixture = TestEPnP()
+    epnp_fixture.test_epnp_5pts()
+    epnp_fixture.test_epnp_10pts()
+    epnp_fixture.test_epnp_random()
