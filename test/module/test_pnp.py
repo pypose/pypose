6af34fad67ebf3a0301873eb6727216cfd8d5ec7
Jingnan Shi <jnshi@mit.edu>
2023-02-24 12:38:05 -0500

    add test stub

---
+++ test/module/test_pnp.py
@@ -0,0 +1,15 @@
+import pypose as pp
+import torch
+
+
+def test_epnp_5pts():
+    # TODO: Implement this
+    return
+
+def test_epnp_10pts():
+    # TODO: Implement this
+    return
+
+def test_epnp_random():
+    # TODO: Implement this
+    return
