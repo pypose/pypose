178e600a0b2058b02d17031a8c4e54d3d9a4f4b3
Zitong Zhan <ztzhan1108@gmail.com>
2023-03-26 04:46:46 +0000

    remaining fixes

--- test/optim/test_jacobian.py
+++ test/optim/test_jacobian.py
@@ -1,10 +1,28 @@
+import copy
 import torch
 import pypose as pp
 from torch import nn
-import functorch, functools
+import functorch
 from torch.autograd.functional import jacobian
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+def make_functional(mod, disable_autograd_tracking=False):
+    # relies on the code snippet from 
+    # https://pytorch.org/docs/stable/func.migrating.html?highlight=functorch#functorch-make-functional
+    params_dict = dict(mod.named_parameters())
+    params_names = params_dict.keys()
+    params_values = tuple(params_dict.values())
+    
+    stateless_mod = copy.deepcopy(mod)
+    stateless_mod.to('meta')
+
+    def fmodel(new_params_values, *args, **kwargs):
+        new_params_dict = {name: value for name, value in zip(params_names, new_params_values)}
+        return torch.func.functional_call(stateless_mod, new_params_dict, args, kwargs)
+  
+    if disable_autograd_tracking:
+        params_values = torch.utils._pytree.tree_map(torch.Tensor.detach, params_values)
+    return fmodel, params_values
 
 class TestJacobian:
 
@@ -25,13 +43,13 @@ class TestJacobian:
 
         model = Model()
         input = torch.randn(2, 2, 2)
-        func, params = functorch.make_functional(model)
+        func, params = make_functional(model)
         J = jacobian(lambda *param: func(param, input), params)
         self.verify_jacobian(J)
 
         model = nn.Conv2d(2, 2, 2)
         input = torch.randn(2, 2, 2)
-        func, params = functorch.make_functional(model)
+        func, params = make_functional(model)
         J = jacobian(lambda *param: func(param, input), params)
         self.verify_jacobian(J)
 
@@ -51,8 +69,8 @@ class TestJacobian:
         J1 = pp.optim.functional.modjac(model, input=None, flatten=True)
         self.verify_jacobian(J1)
 
-        func, params = functorch.make_functional(model)
-        jacrev = functorch.jacrev(lambda *param: func(param, input), params)
+        func, params = make_functional(model)
+        jacrev = torch.func.jacrev(lambda *param: func(param, input), params)
 
         model, input = PoseTransform().to(device), pp.randn_SO3(device=device)
         huber = pp.optim.kernel.Huber(delta=0.01)
