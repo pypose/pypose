41056de4afe79d45f04db65aa7bdba4c05d33b4e
hakunanh <hannahh6069@gmail.com>
2023-03-30 17:46:11 -0400

    add new lqr and mpc modules

--- pypose/module/__init__.py
+++ pypose/module/__init__.py
@@ -3,5 +3,7 @@ from .lqr import LQR
 from .ekf import EKF
 from .ukf import UKF
 from .pf import PF
+from .nlqr import NLQR
+from .nlmpc import NLMPC
 from .dynamics import System, LTI, LTV, NLS
 from .imu_preintegrator import IMUPreintegrator
