85e5b87a3e0d934c520de7e6378d9c86712207eb
hakunanh <hannahh6069@gmail.com>
2023-03-30 17:46:32 -0400

    add mpc in nl case

---
+++ pypose/module/nlmpc.py
@@ -0,0 +1,52 @@
+import torch
+import pypose as pp
+import torch.nn as nn
+
+
+class NLMPC(nn.Module):
+
+    def __init__(self, system, T, step=10, eps=1e-4):
+        super().__init__()
+        self.system = system
+        self.T = T
+        self.step = step
+        self.eps = eps
+
+    def forward(self, x_init, current_x, current_u, Q, p, time):
+
+        best = None
+
+        for i in range(self.step):
+
+            current_u.requires_grad = False
+            if not current_u.requires_grad:
+                current_u = current_u 
+            else:
+                current_u = current_u.detach() 
+
+            nlqr = pp.module.NLQR(self.system, Q, p, self.T)
+            x, u, cost = nlqr(x_init, current_x, current_u, time)
+            assert x.ndim == u.ndim == 3
+
+            if best is None: 
+                best = {
+                    'x': x,
+                    'u': u,
+                    'cost': cost,}
+            else:
+                if cost <= best['cost']+ self.eps:
+                    best['x'] = x
+                    best['u']= u
+                    best['cost'] = cost
+
+            current_x = x
+            current_u = u
+
+
+        current_x = best['x']
+        current_u = best['u']
+
+        _nlqr = pp.module.NLQR(self.system, Q, p, self.T)
+        x, u, cost= _nlqr(x_init, current_x, current_u, time)
+
+        return x, u, cost
