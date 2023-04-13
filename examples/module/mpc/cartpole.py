29ed01f13376d0c1c8f71435c080299d344178d4
hakunanh <hannahh6069@gmail.com>
2023-04-13 15:38:58 -0400

    add examples for mpc

---
+++ examples/module/mpc/cartpole.py
@@ -0,0 +1,153 @@
+import os
+import torch
+import shutil
+import argparse
+import setproctitle
+import pypose as pp
+import pickle as pkl
+import torch.optim as optim
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_batch', type=int, default=1)
+    parser.add_argument('--n_state', type=int, default=4)
+    parser.add_argument('--n_ctrl', type=int, default=1)
+    parser.add_argument('--T', type=int, default=5)
+    parser.add_argument('--save', type=str)
+    parser.add_argument('--work2', type=str, default='work2')
+    parser.add_argument('--no-cuda', action='store_true')
+    parser.add_argument('--seed', type=int, default=0)
+    args = parser.parse_args()
+
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    t = '.'.join(["{}={}".format(x, getattr(args, x))
+                  for x in ['n_batch', 'n_state', 'n_ctrl', 'T']])
+    setproctitle.setproctitle('bamos.lqr.'+t+'.{}'.format(args.seed))
+    if args.save is None:
+        args.save = os.path.join(args.work2, t, str(args.seed))
+    if os.path.exists(args.save):
+        shutil.rmtree(args.save)
+    os.makedirs(args.save, exist_ok=True)
+
+    device = torch.device("cuda" if args.cuda else "cpu")
+    n_batch, n_state, n_ctrl, T= args.n_batch, args.n_state, args.n_ctrl, args.T
+    n_sc = n_state + n_ctrl
+
+    class CartPole(pp.module.NLS):
+        def __init__(self, dt, length, cartmass, polemass, gravity):
+            super().__init__()
+            self.tau = dt
+            self.length = length
+            self.cartmass = cartmass
+            self.polemass = polemass
+            self.gravity = gravity
+            self.polemassLength = self.polemass * self.length
+            self.totalMass = self.cartmass + self.polemass
+
+        def state_transition(self, state, input, t=None):
+            x, xDot, theta, thetaDot = state.squeeze()
+            force = input.squeeze()
+            costheta = torch.cos(theta)
+            sintheta = torch.sin(theta)
+
+            temp = (
+                force + self.polemassLength * thetaDot**2 * sintheta
+            ) / self.totalMass
+            thetaAcc = (self.gravity * sintheta - costheta * temp) / (
+                self.length * (4.0 / 3.0 - self.polemass * costheta**2 / self.totalMass)
+            )
+            xAcc = temp - self.polemassLength * thetaAcc * costheta / self.totalMass
+
+            _dstate = torch.stack((xDot, xAcc, thetaDot, thetaAcc))
+
+            return (state.squeeze() + torch.mul(_dstate, self.tau)).unsqueeze(0)
+
+        def observation(self, state, input, t=None):
+            return state
+
+    expert_seed = 7
+    assert expert_seed != args.seed
+    torch.manual_seed(expert_seed)
+
+    Q = torch.tile(torch.eye(n_state + n_ctrl, device=device), (n_batch, T, 1, 1))
+    p = torch.randn(n_batch, T, n_sc)
+    dt = 0.01
+    g = 9.81
+    time  = torch.arange(0, T, device=device) * dt
+
+    expert = dict(
+        Q = torch.tile(torch.eye(n_state + n_ctrl, device=device), (n_batch, T, 1, 1)),
+        p = torch.randn(n_batch, T, n_sc).to(device),
+        len = torch.tensor(1.5).to(device),
+        m_cart = torch.tensor(20).to(device),
+        m_pole = torch.tensor(10).to(device),
+    )
+    fname = os.path.join(args.save, 'expert.pkl')
+    with open(fname, 'wb') as fi:
+        pkl.dump(expert, fi)
+
+    current_x = torch.zeros(1, T, n_state, device=device)
+    current_u = torch.sin(time).unsqueeze(1).unsqueeze(0)
+    x_init = torch.tensor([[0, 0, torch.pi*0.5, 0]], device=device)
+    current_x[...,0,:] = x_init
+    expert_cartPoleSolver = CartPole(dt, expert['len'], expert['m_cart'], expert['m_pole'], g).to(device)
+    for i in range(T-1):
+        current_x[...,i+1,:], _ = expert_cartPoleSolver(current_x[...,i,:], current_u[...,i,:])
+
+    torch.manual_seed(args.seed)
+    len = torch.tensor(1.55).to(device).requires_grad_()
+    m_cart = torch.tensor(20.3).to(device).requires_grad_()
+    m_pole = torch.tensor(10.4).to(device).requires_grad_()
+
+    fname = os.path.join(args.save, 'cartpole losses.csv')
+    loss_f = open(fname, 'w')
+    loss_f.write('im_loss,mse\n')
+    loss_f.flush()
+
+    mpc_expert = pp.module.MPC(expert_cartPoleSolver, T, step=15).to(device)
+    x_true, u_true, cost_true = mpc_expert(expert['Q'], expert['p'], x_init, current_x, current_u, time)
+
+    def get_loss(_len, _m_cart, _m_pole):
+        current_x = torch.zeros(1, T, n_state, device=device)
+        current_u = torch.sin(time).unsqueeze(1).unsqueeze(0)
+        x_init = torch.tensor([[0, 0, torch.pi*0.5, 0]], device=device)
+        current_x[...,0,:] = x_init
+        agent_cartPoleSolver = CartPole(dt, _len, _m_cart, _m_pole, g).to(device)
+        for i in range(T-1):
+            current_x[...,i+1,:], _ = agent_cartPoleSolver(current_x[...,i,:], current_u[...,i,:])
+
+        mpc_agent = pp.module.MPC(agent_cartPoleSolver, T, step=15).to(device)
+        x_pred, u_pred, cost_pred = mpc_agent(expert['Q'], expert['p'], x_init, current_x, current_u, time)
+
+        traj_loss = torch.mean((u_true - u_pred)**2) \
+            + torch.mean((x_true - x_pred)**2)
+
+        return traj_loss
+
+    opt = optim.RMSprop((len, m_cart, m_pole), lr=1e-2)
+
+    for i in range(10):
+        traj_loss = get_loss(len, m_cart, m_pole)
+        opt.zero_grad()
+        traj_loss.backward()
+        opt.step()
+
+        model_loss = torch.mean((len - expert['len'])**2) + \
+                     torch.mean((m_cart - expert['m_cart'])**2) + \
+                     torch.mean((m_pole - expert['m_pole'])**2)
+
+        loss_f.write('{},{}\n'.format(traj_loss.item(), model_loss.item()))
+        loss_f.flush()
+
+        plot_interval = 1
+        if i % plot_interval == 0:
+            os.system('./plot.py "{}" &'.format(args.save))
+            print(len, expert['len'])
+            print(m_cart, expert['m_cart'])
+            print(m_pole, expert['m_pole'])
+            print('{:04d}: traj_loss: {:.8f} model_loss: {:.8f}'.format(
+            i, traj_loss.item(), model_loss.item()))
+
+
+if __name__=='__main__':
+    main()
---
+++ examples/module/mpc/linear.py
@@ -0,0 +1,126 @@
+import os
+import time
+import torch
+import shutil
+import argparse
+import setproctitle
+import pypose as pp
+import pickle as pkl
+import torch.optim as optim
+
+class TrainMPC:
+    def main(self, device='cpu'):
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--n_batch', type=int, default=16)
+        parser.add_argument('--n_state', type=int, default=5)
+        parser.add_argument('--n_ctrl', type=int, default=5)
+        parser.add_argument('--T', type=int, default=5)
+        parser.add_argument('--save', type=str)
+        parser.add_argument('--work', type=str, default='work')
+        parser.add_argument('--no-cuda', action='store_true')
+        parser.add_argument('--seed', type=int, default=0)
+        args = parser.parse_args()
+
+        args.cuda = not args.no_cuda and torch.cuda.is_available()
+        t = '.'.join(["{}={}".format(x, getattr(args, x))
+                    for x in ['n_batch', 'n_state', 'n_ctrl', 'T']])
+        setproctitle.setproctitle('bamos.lqr.'+t+'.{}'.format(args.seed))
+        if args.save is None:
+            args.save = os.path.join(args.work, t, str(args.seed))
+        if os.path.exists(args.save):
+            shutil.rmtree(args.save)
+        os.makedirs(args.save, exist_ok=True)
+
+        device = torch.device("cuda" if args.cuda else "cpu")
+        n_batch, n_state, n_ctrl, T= args.n_batch, args.n_state, args.n_ctrl, args.T
+        n_sc = n_state + n_ctrl
+
+        expert_seed = 26
+        assert expert_seed != args.seed
+        torch.manual_seed(expert_seed)
+
+        Q = torch.tile(torch.eye(n_sc, device=device), (n_batch, T, 1, 1))
+        p = torch.tile(torch.randn(n_sc, device=device), (n_batch, T, 1))
+        C = torch.eye(n_state, device=device)
+        D = torch.zeros(n_state, n_ctrl, device=device)
+        c1 = torch.zeros(n_state, device=device)
+        c2 = torch.zeros(n_state, device=device)
+
+        expert = dict(
+            Q = torch.tile(torch.eye(n_sc, device=device), (n_batch, T, 1, 1)),
+            p = torch.tile(torch.randn(n_sc, device=device), (n_batch, T, 1)),
+            A = torch.eye(n_state, device=device) \
+                + 0.2 * torch.randn(n_state, n_state, device=device),
+            B = torch.randn(n_state, n_ctrl, device=device),
+        )
+        fname = os.path.join(args.save, 'expert.pkl')
+        with open(fname, 'wb') as fi:
+            pkl.dump(expert, fi)
+
+        x_init = torch.randn(n_batch, n_state, device=device)
+        lti = pp.module.LTI(expert['A'], expert['B'], C, D, c1, c2)
+        mpc_expert = pp.module.MPC(lti, T, step=1)
+        x_true, u_true, cost_true = mpc_expert.forward(expert['Q'], expert['p'], x_init)
+
+        torch.manual_seed(args.seed)
+        A = (torch.eye(n_state, device=device) + 0.2 * torch.randn(n_state, n_state, device=device))\
+            .requires_grad_()
+        B = torch.randn(n_state, n_ctrl, device=device).requires_grad_()
+
+        fname = os.path.join(args.save, 'pypose losses.csv')
+        loss_f = open(fname, 'w')
+        loss_f.write('im_loss,mse\n')
+        loss_f.flush()
+        fname = os.path.join(args.save, 'pypose time.csv')
+        time_d = open(fname, 'w')
+        time_d.write('backward_time,overall_time\n')
+        time_d.flush()
+
+        def get_loss(x_init, _A, _B):
+            lti_ = pp.module.LTI(_A, _B, C, D, c1, c2)
+            mpc_agent = pp.module.MPC(lti_, T, step=1)
+            x_pred, u_pred, cost_pred = mpc_agent.forward(expert['Q'], expert['p'], x_init)
+
+            traj_loss = torch.mean((u_true - u_pred)**2) \
+                + torch.mean((x_true - x_pred)**2)
+
+            return traj_loss
+
+        opt = optim.RMSprop((A, B), lr=1e-2)
+
+        for i in range(150):
+            t1 = time.time()
+            traj_loss = get_loss(x_init, A, B)
+            opt.zero_grad()
+            t2 = time.time()
+            traj_loss.backward()
+            t3 = time.time()
+            backward_time = t3 - t2
+            opt.step()
+            t4 = time.time()
+            overall_time = t4 - t1
+
+            model_loss = torch.mean((A - expert['A'])**2) + \
+                        torch.mean((B - expert['B'])**2)
+
+            loss_f.write('{},{}\n'.format(traj_loss.item(), model_loss.item()))
+            loss_f.flush()
+            time_d.write('{},{}\n'.format(backward_time, overall_time))
+            time_d.flush()
+
+            plot_interval = 100
+            if i % plot_interval == 0:
+                print(A, expert['A'])
+                print(B, expert['B'])
+
+            print('{:04d}: traj_loss: {:.4f} model_loss: {:.4f}'.format(
+                i, traj_loss.item(), model_loss.item()))
+            print("backward_time = ", backward_time)
+            print("overall_time = ", overall_time)
+
+
+if __name__=='__main__':
+    train = TrainMPC()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train.main(device)
