ab47b22e9f706d1dd07e88dd6d0a32f2726e9b37
Murphy Xu <48173064+Murphy41@users.noreply.github.com>
2023-04-14 15:15:46 +1000

    Use SE3 and LieTensor operation instead of @
    Signed-off-by: Murphy Xu <48173064+Murphy41@users.noreply.github.com>

--- test/module/test_icp.py
+++ test/module/test_icp.py
@@ -4,22 +4,9 @@ import torch as torch
 # A temp test file
 if __name__=="__main__":
     input_pc = torch.randn([10, 20, 3])
-    transT = 0.1 * torch.randn([10, 1, 3])
-    print("The ori transT is", transT)
-    transR = pp.randn_SO3(10)
-    print("The ori transR is", transR)
-    transR = transR.matrix()
-
-    output_pc = (input_pc @ transR) + transT
-
+    tf = pp.randn_SE3(10)
+    print("The true tf is", tf)
+    output_pc = tf.unsqueeze(-2).Act(input_pc)
     icpsvd = pp.module.ICP(matched=False)
     result = icpsvd(input_pc, output_pc)
     print("The output is", result)
-
-    # output_transT = result[:,0:3]
-    # output_transR = result[:,3:7]
-    # output_transR = output_transR.matrix()
-    # print("The ICP result is", output_transT.shape)
-    # print("The ICP result is", output_transR)
-    # # if torch.eq(output_transR, transR) :
-    # #     print("pass the test")
