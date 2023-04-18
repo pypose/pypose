0ac88f6d36a3e4b3c046f76e205ce5d9e8ad77f9
Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>
2023-04-18 16:31:32 +0800

    add init_transform
    Signed-off-by: Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>

--- pypose/module/icp.py
+++ pypose/module/icp.py
@@ -10,46 +10,26 @@ class ICP(torch.nn.Module):
         matched: whether the input points set and the target points set have been matched
     '''
 
-    def __init__(self, steplim=200, tol=0.0001, matched=False):
+    def __init__(self, steplim=200, tol=0.0001, init_transform =None):
         super().__init__()
+        assert type(init_transform) == pp.LieTensor
         self.steplim = steplim
         self.tol = tol
-        self.matched = matched
-
-    def forward(self, p1, p2):
-        temppc = p1.clone()
-        iter = 0
-        err = 0
-        if (not self.matched):
-            while iter <= self.steplim:
-                iter += 1
-                nn = self._k_nearest_neighbor(temppc, p2)
-                errnew = sum(sum(nn.values) / len(nn.values))
-                T = EPnP._points_transform(temppc, p2[:, nn.indices[-1],:].squeeze(-2))
-                temppc = T.unsqueeze(-2).Act(temppc)
-                if (abs(err - errnew) < self.tol):
-                    break
-                err = errnew
-            T = EPnP._points_transform(p1, temppc)
-            return T
-        else:
-            T = EPnP._points_transform(p1, p2)
-            return T
-
-    @staticmethod
-    def _k_nearest_neighbor(pc1, pc2, k = 1, norm = 2, sort: bool = True):
+        self.init_transform = init_transform
+        
+    def _k_nearest_neighbor(self, pc1, pc2, k = 1, norm = 2, sort: bool = True):
+        
         r'''
         Select the k nearest neighbor point of pc1 from pc2
         Args:
             pc1: the source points set
             pc2: the target points set
             tolerance: the threshold of min distance
-
         Returns:
-            distances: the min distance between point in p1 and its nearest neighbor
-            indices: the index of the nearest neighbor point in p2
+            nn.values: the min distance between point in p1 and its nearest neighbor
+            nn.indices: the index of the nearest neighbor point in p2
         '''
-
+        
         diff = pc1.unsqueeze(-2) - pc2.unsqueeze(-3)
         if norm == 1:
             distance = torch.sum(torch.abs(diff), dim=-1)
@@ -60,3 +40,21 @@ class ICP(torch.nn.Module):
         # Will add sorted function later.
 
         return nn
+
+    def forward(self, p1, p2):
+        temppc = p1.clone()
+        iter = 0
+        err = 0
+        if (self.init_transform != None):
+            temppc = self.init_transform.Act(temppc)
+        while iter <= self.steplim:
+            iter += 1
+            nn = self._k_nearest_neighbor(temppc, p2)
+            errnew = sum(sum(sum(nn.values) / len(nn.values)))
+            T = EPnP._points_transform(temppc, p2[:, nn.indices[-1],:].squeeze(-2))
+            temppc = T.unsqueeze(-2).Act(temppc)
+            if (abs(err - errnew) < self.tol):
+                break
+            err = errnew
+        T = EPnP._points_transform(p1, temppc)
+        return T
