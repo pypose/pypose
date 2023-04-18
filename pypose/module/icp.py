fb6fa5cd99ecc2af8fa65cdb0a5894a326dafb7c
Murphy41 <yangmengfei.xu@gmail.com>
2023-04-18 19:49:57 +1000

    resolve conflict

--- pypose/module/icp.py
+++ pypose/module/icp.py
@@ -12,22 +12,47 @@ class ICP(torch.nn.Module):
 
     def __init__(self, steplim=200, tol=0.0001, init_transform =None):
         super().__init__()
-        assert type(init_transform) == pp.LieTensor
+        #assert type(init_transform) == pp.LieTensor
         self.steplim = steplim
         self.tol = tol
         self.init_transform = init_transform
 
-    def _k_nearest_neighbor(self, pc1, pc2, k = 1, norm = 2, sort: bool = True):
+    def forward(self, p1, p2):
+        temppc = p1.clone()
+        iter = 0
+        err = None
+        if (self.init_transform != None):
+            temppc = self.init_transform.Act(temppc)
+        while iter <= self.steplim:
+            iter += 1
+            neighbors = self._k_nearest_neighbor(temppc, p2)
+            errnew = torch.mean(neighbors.values, dim=-1)
+            T = EPnP._points_transform(temppc, p2[:, neighbors.indices[-1],:].squeeze(-2))
+            temppc = T.unsqueeze(-2).Act(temppc)
+            if err is None:
+                err = errnew
+            else:
+                if torch.all(torch.abs((errnew - err) / err) < self.tol):
+                    break
+            err = errnew
+        T = EPnP._points_transform(p1, temppc)
+        return T
 
+    @staticmethod
+    def _k_nearest_neighbor(pc1, pc2, k = 1, norm = 2, sort: bool = False):
         r'''
         Select the k nearest neighbor point of pc1 from pc2
         Args:
             pc1: the source points set
             pc2: the target points set
-            tolerance: the threshold of min distance
+            k: the number of nearest neighbors to find
+            norm: the norm to use for distance calculation (1 or 2)
+            sort: whether to sort the k nearest neighbors by distance
+
         Returns:
-            nn.values: the min distance between point in p1 and its nearest neighbor
-            nn.indices: the index of the nearest neighbor point in p2
+            distances: the distance between each point in pc1 and
+                        its k nearest neighbors in pc2
+            indices: the index of the k nearest neighbor points in pc2
         '''
 
         diff = pc1.unsqueeze(-2) - pc2.unsqueeze(-3)
@@ -35,25 +60,15 @@ class ICP(torch.nn.Module):
             distance = torch.sum(torch.abs(diff), dim=-1)
         elif norm == 2:
             distance = torch.sqrt(torch.sum(diff ** 2, dim=-1))
-        nn = distance.topk(k, largest=False)
+        else:
+            raise ValueError("Invalid norm. Only 1-norm and 2-norm are supported.")
 
-        # Will add sorted function later.
+        knn = distance.topk(k, largest=False)
 
-        return nn
+        if k > 1 and sort:
+            dist, rank= knn.values.sort(dim=-1)
+            idx = torch.gather(knn.indices, -1, rank)
+            TopK = torch.namedtuple('TopK', ['values', 'indices'])
+            knn = TopK(values=dist, indices=idx)
 
-    def forward(self, p1, p2):
-        temppc = p1.clone()
-        iter = 0
-        err = 0
-        if (self.init_transform != None):
-            temppc = self.init_transform.Act(temppc)
-        while iter <= self.steplim:
-            iter += 1
-            nn = self._k_nearest_neighbor(temppc, p2)
-            errnew = sum(sum(sum(nn.values) / len(nn.values)))
-            T = EPnP._points_transform(temppc, p2[:, nn.indices[-1],:].squeeze(-2))
-            temppc = T.unsqueeze(-2).Act(temppc)
-            if (abs(err - errnew) < self.tol):
-                break
-            err = errnew
-        T = EPnP._points_transform(p1, temppc)
+        return knn
