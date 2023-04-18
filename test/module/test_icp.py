96443c817197c481eef6f0a35e80b50f6c6bfca4
Murphy41 <yangmengfei.xu@gmail.com>
2023-04-18 19:52:17 +1000

    modify test_icp file

--- test/module/test_icp.py
+++ test/module/test_icp.py
@@ -7,6 +7,6 @@ if __name__=="__main__":
     tf = pp.randn_SE3(10)
     print("The true tf is", tf)
     output_pc = tf.unsqueeze(-2).Act(input_pc)
-    icpsvd = pp.module.ICP(matched=False)
+    icpsvd = pp.module.ICP()
     result = icpsvd(input_pc, output_pc)
     print("The output is", result)
