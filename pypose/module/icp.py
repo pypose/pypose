d8ba9d8487166d58254240fd894e37ccce5e3566
Murphy41 <yangmengfei.xu@gmail.com>
2023-04-18 15:32:31 +1000

    Replace @ with Act to perform transformation :)

--- pypose/module/icp.py
+++ pypose/module/icp.py
@@ -15,7 +15,7 @@ class ICP(torch.nn.Module):
         self.steplim = steplim
         self.tol = tol
         self.matched = matched
-        
+
     def nearest_neighbor(self, p1, p2, k=1):
         r'''
         Select the nearest neighbor point of p1 from p2
@@ -34,7 +34,7 @@ class ICP(torch.nn.Module):
         nn = dist.topk(k, largest=False)
         return nn
 
-    
+
     def forward(self, p1, p2):
         temppc = p1.clone()
         iter = 0
@@ -45,7 +45,7 @@ class ICP(torch.nn.Module):
                 nn = self.nearest_neighbor(temppc, p2)
                 errnew = sum(sum(nn.values) / len(nn.values))
                 T = EPnP._points_transform(temppc, p2[:, nn.indices[-1],:].squeeze(-2))
-                temppc = T.rotation().unsqueeze(-2).Act(temppc)+T.translation().unsqueeze(-2)
+                temppc = T.unsqueeze(-2).Act(temppc)
                 if (abs(err - errnew) < self.tol):
                     break
                 err = errnew
@@ -54,4 +54,3 @@ class ICP(torch.nn.Module):
         else:
             T = EPnP._points_transform(p1, p2)
             return T
-        
