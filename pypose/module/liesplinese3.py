011a642cc0471e303c0790e4aa2bfd5845656f78
Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>
2023-04-19 11:22:52 +0800

    Update liesplinese3.py
    Signed-off-by: Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>

--- pypose/module/liesplinese3.py
+++ pypose/module/liesplinese3.py
@@ -5,20 +5,21 @@ class lieSpline(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.B = torch.tensor([[5, 3, -3, 1],
-                               [1, 3, 3, -2],
-                               [0, 0, 0, 1]]) / 6
-
+        
     def compute_weights(self, time):
-        alpha = torch.arange(4)
+        alpha = torch.arange(4, dtype=time.dtype, device=time.device)
         tt = time[:, None, :] ** alpha[None, :, None]
-        return self.B @ tt
+        B = torch.tensor([[5, 3, -3, 1],
+                               [1, 3, 3, -2],
+                               [0, 0, 0, 1]], dtype=time.dtype, device=time.device) / 6
+        return B @ tt
 
     def interpolateSE3(self, input_poses, time, inter_begin_and_end = True):
         """ B spline interpolation in SE3
         Args:
             input_poses: input n(n>=4) poses with [batch_size, n, 4, 4] shape
             time: k interpolation time point with [1, 1, k] shape
+            inter_begin_and_end: the flag of whether interpolate the poses between input_poses[0] and input_poses[1] and input_poses[-2] and input_poses[-1]
         Returns:
             output_poses: output (n-3)*k poses with [batch_size, (n-3)*k, 4, 4] shape
         """
@@ -29,7 +30,6 @@ class lieSpline(nn.Module):
         timeSize = time.shape[-1]
         batchSize = input_poses.shape[0]
         posesSize = input_poses.shape[1]
-        print(timeSize)
         w = self.compute_weights(time)
         w0 = w[0, :, 0, :].T
         w1 = w[0, :, 1, :].T
@@ -43,9 +43,8 @@ class lieSpline(nn.Module):
         A0 = ((posesTensor[:, :, [0], :].Inv()*(posesTensor[:, :, [1], :])).Log() * w0).Exp()
         A1 = ((posesTensor[:, :, [1], :].Inv()*(posesTensor[:, :, [2], :])).Log() * w1).Exp()
         A2 = ((posesTensor[:, :, [2], :].Inv()*(posesTensor[:, :, [3], :])).Log() * w2).Exp()
-        waypoints = (T_delta * A0 * A1* A2).reshape((batchSize, timeSize * (posesSize - 3),7))
+        waypoints = T_delta * A0 * A1* A2
         return waypoints
-
 
 
 
