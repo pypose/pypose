f95807559ce19b0b5584dc0676747efea805b24e
Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>
2023-04-21 12:03:55 +0800

    modify the example of liespline
    Signed-off-by: Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>

--- examples/module/liespline/liesplinese3.py
+++ examples/module/liespline/liesplinese3.py
@@ -1,43 +1,60 @@
 import torch
+import os.path
+import argparse
 import pypose as pp
 import matplotlib.pyplot as plt
 from pytransform3d.rotations import plot_basis
-from pypose.module.liesplinese3 import lieSpline
+from pypose.module.liesplinese3 import LieSpline
 
-angle1 = pp.euler2SO3(torch.Tensor([0., 0., 0.]))
-angle2 = pp.euler2SO3(torch.Tensor([torch.pi / 4., torch.pi / 3., torch.pi / 2.]))
-time = torch.arange(0, 1, 0.25).reshape(1, 1, -1)
-input_poses = pp.LieTensor([[[0., 4., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [0., 3., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [0., 2., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [0., 1., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [1., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]],
-                             [2., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]],
-                             [3., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]],
-                             [4., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]]],
-                            [[2., 4., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [3., 3., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [4., 2., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [5., 1., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
-                             [1., 0., 2., angle2[0], angle2[1], angle2[2], angle2[3]],
-                             [2., 0., 3., angle2[0], angle2[1], angle2[2], angle2[3]],
-                             [2., 0., 4., angle2[0], angle2[1], angle2[2], angle2[3]],
-                             [3., 0., 5., angle2[0], angle2[1], angle2[2], angle2[3]]]], ltype=pp.SE3_type)
+def plot_result(wayposes, x_range, y_range, z_range, k = 0, save=None, show=None):
+    assert k < wayposes.shape[0]
+    wayposes = wayposes.matrix()
+    ax = None
+    for pose in wayposes[k, :, :, :]:
+        R = pose[0:3, 0:3]
+        p = pose[0:3, 3]
+        ax = plot_basis(ax=ax, s=0.15, R=R, p=p)
+    ax.set_xlim(x_range)
+    ax.set_ylim(y_range)
+    ax.set_zlim(z_range)
+    if save is not None:
+        file_path = os.path.join(save, 'liespline.png')
+        plt.savefig(file_path)
+        print("Save to", file_path)
+    if show:
+        plt.show()
 
-LS = lieSpline()
-waypoints = LS.interpolateSE3(input_poses, time)
-print(waypoints.shape)
-wayposes = waypoints.matrix()
-print(wayposes.shape)
-ax = None
-for pose in wayposes[0, :, :, :]:
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='LieSpline Example')
+    parser.add_argument("--device", type=str, default='cpu', help="cuda or cpu")
+    parser.add_argument("--save", type=str, default='./examples/module/liespline/save/',
+                        help="location of png files to save")
+    parser.add_argument('--show', dest='show', action='store_true',
+                        help="show plot, default: False")
+    parser.set_defaults(show=False)
+    args = parser.parse_args()
+    os.makedirs(os.path.join(args.save), exist_ok=True)
+    print(args)
+    angle1 = pp.euler2SO3(torch.Tensor([0., 0., 0.]))
+    angle2 = pp.euler2SO3(torch.Tensor([torch.pi / 4., torch.pi / 3., torch.pi / 2.]))
+    time = torch.arange(0, 1, 0.25).reshape(1, 1, -1)
+    input_poses = pp.LieTensor([[[0., 4., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [0., 3., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [0., 2., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [0., 1., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [1., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]],
+                                 [2., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]],
+                                 [3., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]],
+                                 [4., 0., 1., angle2[0], angle2[1], angle2[2], angle2[3]]],
+                                [[2., 4., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [3., 3., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [4., 2., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [5., 1., 0., angle1[0], angle1[1], angle1[2], angle1[3]],
+                                 [1., 0., 2., angle2[0], angle2[1], angle2[2], angle2[3]],
+                                 [2., 0., 3., angle2[0], angle2[1], angle2[2], angle2[3]],
+                                 [2., 0., 4., angle2[0], angle2[1], angle2[2], angle2[3]],
+                                 [3., 0., 5., angle2[0], angle2[1], angle2[2], angle2[3]]]], ltype=pp.SE3_type)
 
-    R = pose[0:3, 0:3]
-    p = pose[0:3, 3]
-    ax = plot_basis(ax=ax, s=0.15, R=R, p=p)
-ax.set_xlim([0,5.])
-ax.set_ylim([0,5.])
-ax.set_zlim([0,1.2])
-
-plt.savefig("liespline.png")
-plt.show()
+    ls = LieSpline()
+    wayposes = ls(input_poses, time)
+    plot_result(wayposes, [0, 5.], [0, 5.], [0, 1.2], save=args.save, show=args.show)
