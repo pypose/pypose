eadfbb57fc74ef2e29aed9f896dfe1120aa388fb
Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>
2023-04-23 16:41:46 +0800

    add doc
    Signed-off-by: Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>

--- pypose/module/icp.py
+++ pypose/module/icp.py
@@ -4,10 +4,36 @@ from .. import knn, points_transform
 class ICP(torch.nn.Module):
     r'''
     Iterative Closest Point (ICP) using Singular Value Decomposition (SVD).
+    
     Args:
         steplim: the max step number
         tol: the tolerance of error for early stopping
         tf: initial transformation
+
+    ICP algorithm aims to calculate the optimal transformation :math:`T`
+    between two point clouds, source point cloud and target point cloud.
+
+    .. math::
+        \begin{align*}
+            argmin \sum \|p_i^t - Tp_i^s\|,
+        \end{align*}
+
+    where :math:`p^t` is the point in target point cloud, and :math:`p^s` is
+    the point in the source point cloud.
+
+    1. For each point in pc1, nearest neighbor algorithm is used to select its
+    closest point in pc2 to the matched point pairs.
+
+    2. Singular value decomposition (SVD) algorithm is used to get the rotation
+    and translation matrices from the matched point pairs.
+
+    3. The source point cloud is updated using the obtained rotation and
+    translation matrices. The distance between the updated source point cloud and
+    the target point cloud is calculated.
+
+    4. Iterate these steps until the change of obtained distance is lower than the
+    given tolerance.
+    
     '''
 
     def __init__(self, steplim = 200, tol = 0.0001, tf = None):
@@ -17,6 +43,16 @@ class ICP(torch.nn.Module):
         self.tf = tf
 
     def forward(self, p1, p2):
+        r'''
+
+        Args:
+            p1(``torch.Tensor``): the source point cloud with [..., points_num, 3] shape.
+            p2(``torch.Tensor``): the target point cloud with [..., points_num, 3] shape.
+
+        Returns:
+            ``LieTensor``: the estimated transformation from p1 to p2.
+
+        '''
         temppc = p1.clone()
         iter = 0
         err = 0
