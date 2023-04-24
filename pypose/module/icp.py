410615efe38e355e413b47ffe7da6378b4405da2
Murphy41 <yangmengfei.xu@gmail.com>
2023-04-24 10:04:47 +1000

    Update ICP documentation

--- pypose/module/icp.py
+++ pypose/module/icp.py
@@ -3,61 +3,73 @@ from .. import knn, points_transform
 
 class ICP(torch.nn.Module):
     r'''
-    Iterative Closest Point (ICP) using Singular Value Decomposition (SVD).
-    
-    Args:
-        steplim: the max step number
-        tol: the tolerance of error for early stopping
-        tf: initial transformation
+    This class implements the batched Iterative Closest Point (ICP) algorithm to find a
+    similarity transformation (:math:`T`) between two differently-sized sets of
+    3-dimensional points using Singular Value Decomposition (SVD). It's important to note
+    that the solution found is only a local optimum.
 
-    ICP algorithm aims to calculate the optimal transformation :math:`T`
-    between two point clouds, source point cloud and target point cloud.
+    Args:
+        steplim (``int``, optional): The maximum number of ICP iteration steps.
+            Default: 200.
+        tol (``double``, optional): The tolerance of the relative error used to terminate
+            the algorithm. Default: 1e-4.
+        tf (``LieTensor``, optional): The initial transformation :math:`T_{init}` in
+            ``SE3type``. Default: None.
+
+    The algorithm takes two input point clouds: source point cloud (psrc) and target
+    point cloud (ptgt). The objective is to find the optimal similarity transformation
+    (:math:`T`) to minimize the error between the transformed source point cloud and the
+    target point cloud as shown in the equation:
 
     .. math::
         \begin{align*}
-            argmin \sum \|p_i^t - Tp_i^s\|,
+            \operatorname{arg\,min}_T \sum_i \|ptgt_i - T \cdot psrc_j\|,
         \end{align*}
 
-    where :math:`p^t` is the point in target point cloud, and :math:`p^s` is
-    the point in the source point cloud.
+    where :math:`ptgt_i` is the ith point in the target point cloud, and :math:`psrc_j` is
+    the jth point in the source point cloud. The algorithm consists of the following steps:
 
-    1. For each point in pc1, nearest neighbor algorithm is used to select its
-    closest point in pc2 to the matched point pairs.
+    1. For each point in psrc, the nearest neighbor algorithm is used to select its
+    closest point in ptgt to form the matched point pairs.
 
-    2. Singular value decomposition (SVD) algorithm is used to get the rotation
+    2. Singular value decomposition (SVD) algorithm is used to compute the rotation
     and translation matrices from the matched point pairs.
 
     3. The source point cloud is updated using the obtained rotation and
     translation matrices. The distance between the updated source point cloud and
     the target point cloud is calculated.
 
-    4. Iterate these steps until the change of obtained distance is lower than the
-    given tolerance.
-    
+    4. The algorithm iterates through these steps until the change in the obtained
+    distance is lower than the given tolerance.
+
     '''
 
-    def __init__(self, steplim = 200, tol = 0.0001, tf = None):
+    def __init__(self, steplim = 200, tol = 1e-4, tf = None):
         super().__init__()
         self.steplim = steplim
         self.tol = tol
         self.tf = tf
 
-    def forward(self, p1, p2):
+    def forward(self, psrc, ptgt):
         r'''
 
         Args:
-            p1(``torch.Tensor``): the source point cloud with [..., points_num, 3] shape.
-            p2(``torch.Tensor``): the target point cloud with [..., points_num, 3] shape.
+            psrc(``torch.Tensor``): The source point cloud tensor with
+                [..., points_num, 3] shape.
+            ptgt(``torch.Tensor``): The target point cloud tensor with
+                [..., points_num, 3] shape.
 
         Returns:
-            ``LieTensor``: the estimated transformation from p1 to p2.
+            ``LieTensor``: The estimated transformation (``SE3type``) from psrc to ptgt.
 
         '''
-        temppc = p1.clone()
+
+
+        temppc = psrc.clone()
         iter = 0
         err = 0
         if self.tf != None:
-            if p1.shape[:-2] == self.tf.shape[:-1] and \
+            if psrc.shape[:-2] == self.tf.shape[:-1] and \
                 self.tf.shape[-1] == 7:
                 temppc = self.tf.unsqueeze(-2).Act(temppc)
             else:
@@ -66,7 +78,7 @@ class ICP(torch.nn.Module):
                                  "input pointcloud.")
         while iter <= self.steplim:
             iter += 1
-            neighbors = knn(temppc, p2)
+            neighbors = knn(temppc, ptgt)
             knndist = neighbors.values.squeeze(-1)
             knnidx = neighbors.indices
             errnew = torch.mean(knndist, dim=-1)
@@ -75,7 +87,7 @@ class ICP(torch.nn.Module):
             err = errnew
             batch = torch.arange(temppc.shape[0],dtype=knnidx.dtype, device=knnidx.device).repeat(knnidx.shape[1],1).T.unsqueeze(-1)
             batch_knnidx = torch.cat((batch, knnidx),dim =-1)
-            T = points_transform(temppc, p2[batch_knnidx[:,:,0],batch_knnidx[:,:,1],:])
+            T = points_transform(temppc, ptgt[batch_knnidx[:,:,0],batch_knnidx[:,:,1],:])
             temppc = T.unsqueeze(-2).Act(temppc)
-        T = points_transform(p1, temppc)
+        T = points_transform(psrc, temppc)
         return T
