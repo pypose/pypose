92dda986d8abab4246dc4966305d2982c0fc5fdc
Murphy41 <yangmengfei.xu@gmail.com>
2023-05-03 10:41:44 +1000

    Add stepper args to ICP examples

--- examples/module/pcr/icp.py
+++ examples/module/pcr/icp.py
@@ -5,6 +5,15 @@ from pointcloud import Pointcloud, pointcloud_plot
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ICP Example')
     parser.add_argument("--device", type=str, default='cpu', help="cuda or cpu")
+    parser.add_argument("--steps", type=int, default=200,
+                        help="maximum number of ICP will step, default: 200")
+    parser.add_argument("--patience", type=int, default=5,
+                        help="number of steps with no loss 'decreasing' is seen, default: 5")
+    parser.add_argument("--decreasing", type=float, default=1e-3,
+                        help="relative loss decreasing used to count the number of patience steps, default: 1e-3")
+    parser.add_argument("--verbose", dest='verbose', action='store_true',
+                        help="print a message for each step, default: False")
+    parser.set_defaults(verbose=False)
     parser.add_argument("--save", type=str, default='./examples/module/pcr/save/',
                         help="location of png files to save")
     parser.add_argument('--show', dest='show', action='store_true',
@@ -17,7 +26,9 @@ if __name__ == "__main__":
     source = source.to(args.device)
     tf = pp.SE3([-0.0500, -0.0200,  0.0000, 0, 0, 0.4794255, 0.8775826]).to(args.device)
     target = tf.Act(target.to(args.device))
-    icp = pp.module.ICP().to(args.device)
+    stepper = pp.utils.ReduceToBason(steps=args.steps, patience=args.patience,
+                                     decreasing=args.decreasing, verbose=args.verbose)
+    icp = pp.module.ICP(stepper=stepper).to(args.device)
     result = icp(source, target)
     reg = result.Act(source)
 
--- examples/module/pcr/readme.md
+++ examples/module/pcr/readme.md
@@ -13,12 +13,21 @@ Iterative Closest Point (ICP)
 python examples/module/pcr/icp.py
 ```
 
+Or
+```bash
+python examples/module/pcr/icp.py --device cpu --steps 200 --patience 5 --decreasing 1e-5 --verbose --show
+```
+
 * Other supported arguments:
 
     ICP
 
         optional arguments:
-        -h, --help       show this help message and exit
-        --device DEVICE  cuda or cpu
-        --save SAVE      location of png files to save
-        --show           show plot, default: False
+        -h, --help                  show this help message and exit
+        --device DEVICE             cuda or cpu
+        --steps STEPS               maximum number of ICP will step, default: 200
+        --patience PATIENCE         number of steps with no loss 'decreasing' is seen, default: 5
+        --decreasing DECREASING     relative loss decreasing used to count the number of patience steps, default: 1e-3
+        --verbose                   print a message for each step, default: False
+        --save SAVE                 location of png files to save
+        --show                      show plot, default: False
