e62cebd8292de4429ddda431621317a52142b5f6
ntu-caokun <kun001@e.ntu.edu.sg>
2023-05-13 20:00:41 +0800

    update docs from LQR

--- pypose/module/ipddp.py
+++ pypose/module/ipddp.py
@@ -5,14 +5,145 @@ from ..basics import bmv, bvmv, btdot
 
 class ddpOptimizer(nn.Module):
     r'''
-    The class of ipddp optimizer
-    iterates between forwardpass and backwardpass to get a final trajectory 
+    Interior-point differential dynamic programming.
+
+    Args:
+        sys (:obj:`instance`): System dynamics of the optimal control problem.
+        stage_cost (:obj:`instance`): Stage cost of the optimal control problem.
+        terminal_cost (:obj:`instance`): Terminal cost of the optimal control problem. 
+        cons (:obj:`instance`): Constraints of the optimal control problem. 
+        n_cons (:obj:`int`): Dimension of constraints.
+        init_traj (:obj:`Dict`): Initial system trajectory.
+    
+    A discrete-time system can be described as:
+
+    .. math::
+        \begin{align*}
+            \mathbf{x}_{t+1} &= \mathbf{A}_t\mathbf{x}_t + \mathbf{B}_t\mathbf{u}_t 
+                                                         + \mathbf{c}_{1t}          \\
+            \mathbf{y}_t &= \mathbf{C}_t\mathbf{x}_t + \mathbf{D}_t\mathbf{u}_t
+                                                     + \mathbf{c}_{2t}              \\
+        \end{align*}
+
+    where :math:`\mathbf{x}`, :math:`\mathbf{u}` are the state and input of the linear system; 
+    :math:`\mathbf{y}` is the observation of the linear system; :math:`\mathbf{A}`,
+    :math:`\mathbf{B}` are the state matrix and input matrix of the linear system;
+    :math:`\mathbf{C}`, :math:`\mathbf{D}` are the output matrix and observation matrix of the
+    linear system; :math:`\mathbf{c}_{1}`, :math:`\mathbf{c}_{2}` are the constant input and
+    constant output of the linear system. The subscript :math:`\cdot_{t}` denotes the time step.
+
+    LQR finds the optimal nominal trajectory :math:`\mathbf{\tau}_{1:T}^*` = 
+    :math:`\begin{Bmatrix} \mathbf{x}_t, \mathbf{u}_t \end{Bmatrix}_{1:T}` 
+    for the linear system of the optimization problem:
+
+    .. math::
+        \begin{align*}
+          \mathbf{\tau}_{1:T}^* = \mathop{\arg\min}\limits_{\tau_{1:T}} \sum\limits_t\frac{1}{2}
+          \mathbf{\tau}_t^\top\mathbf{Q}_t\mathbf{\tau}_t + \mathbf{p}_t^\top\mathbf{\tau}_t \\
+          \mathrm{s.t.} \quad \mathbf{x}_1 = \mathbf{x}_{\text{init}}, \\
+          \mathbf{x}_{t+1} = \mathbf{F}_t\mathbf{\tau}_t + \mathbf{c}_{1t} \\
+        \end{align*}
+
+    where :math:`\mathbf{\tau}_t` = :math:`\begin{bmatrix} \mathbf{x}_t \\ \mathbf{u}_t
+    \end{bmatrix}`, :math:`\mathbf{F}_t` = :math:`\begin{bmatrix} \mathbf{A}_t & \mathbf{B}_t
+    \end{bmatrix}`.
+
+    The LQR process can be summarised as a backward and a forward recursion.
+
+    - The backward recursion.
+        
+      For :math:`t` = :math:`T` to 1:
+
+        .. math::
+            \begin{align*}
+                \mathbf{Q}_t &= \mathbf{Q}_t + \mathbf{F}_t^\top\mathbf{V}_{t+1}\mathbf{F}_t \\
+                \mathbf{q}_t &= \mathbf{q}_t + \mathbf{F}_t^\top\mathbf{V}_{t+1}
+                                        \mathbf{c}_{1t} + \mathbf{F}_t^\top\mathbf{v}_{t+1}  \\
+                \mathbf{K}_t &= -\mathbf{Q}_{\mathbf{u}_t, \mathbf{u}_t}^{-1} 
+                                                     \mathbf{Q}_{\mathbf{u}_t, \mathbf{x}_t} \\
+                \mathbf{k}_t &= -\mathbf{Q}_{\mathbf{u}_t, \mathbf{u}_t}^{-1} 
+                                                           \mathbf{q}_{\mathbf{u}_t}         \\
+                \mathbf{V}_t &= \mathbf{Q}_{\mathbf{x}_t, \mathbf{x}_t} 
+                    + \mathbf{Q}_{\mathbf{x}_t, \mathbf{u}_t}\mathbf{K}_t 
+                    + \mathbf{K}_t^\top\mathbf{Q}_{\mathbf{u}_t, \mathbf{x}_t} 
+                    + \mathbf{K}_t^\top\mathbf{Q}_{\mathbf{u}_t, \mathbf{u}_t}\mathbf{K}_t   \\
+                \mathbf{v}_t &= \mathbf{q}_{\mathbf{x}_t} 
+                    + \mathbf{Q}_{\mathbf{x}_t, \mathbf{u}_t}\mathbf{k}_t 
+                    + \mathbf{K}_t^\top\mathbf{q}_{\mathbf{u}_t} 
+                    + \mathbf{K}_t^\top\mathbf{Q}_{\mathbf{u}_t, \mathbf{u}_t}\mathbf{k}_t   \\
+            \end{align*}
+
+    - The forward recursion.
+
+      For :math:`t` = 1 to :math:`T`:
+
+        .. math::
+            \begin{align*}
+                \mathbf{u}_t &= \mathbf{K}_t\mathbf{x}_t + \mathbf{k}_t \\
+                \mathbf{x}_{t+1} &= \mathbf{A}_t\mathbf{x}_t + \mathbf{B}_t\mathbf{u}_t 
+                                                             + \mathbf{c}_{1t} \\
+            \end{align*}
+
+    Then quadratic costs of the system over the time horizon:
+
+        .. math::
+            \mathbf{c} \left( \mathbf{\tau}_t \right) = \frac{1}{2}
+            \mathbf{\tau}_t^\top\mathbf{Q}_t\mathbf{\tau}_t + \mathbf{p}_t^\top\mathbf{\tau}_t
+
+    Note:
+        The discrete-time system to be solved by LQR could be both either linear time-invariant
+        (:meth:`LTI`) system or linear time-varying (:meth:`LTV`) system.
+
+    From the learning perspective, this can be interpreted as a module with unknown parameters
+    :math:`\begin{Bmatrix} \mathbf{Q}, \mathbf{p}, \mathbf{F}, \mathbf{f} \end{Bmatrix}`, 
+    which can be integrated into a larger end-to-end learning system.
+
+    Note:
+        The implementation is based on page 24-32 of `Optimal Control and Planning 
+        <http://rll.berkeley.edu/deeprlcourse/f17docs/lecture_8_model_based_planning.pdf>`_.
+
+    Example:
+        >>> n_batch, T = 2, 5
+        >>> n_state, n_ctrl = 4, 3
+        >>> n_sc = n_state + n_ctrl
+        >>> Q = torch.randn(n_batch, T, n_sc, n_sc)
+        >>> Q = torch.matmul(Q.mT, Q)
+        >>> p = torch.randn(n_batch, T, n_sc)
+        >>> r = 0.2 * torch.randn(n_state, n_state)
+        >>> A = torch.tile(torch.eye(n_state) + r, (n_batch, 1, 1))
+        >>> B = torch.randn(n_batch, n_state, n_ctrl)
+        >>> C = torch.tile(torch.eye(n_state), (n_batch, 1, 1))
+        >>> D = torch.tile(torch.zeros(n_state, n_ctrl), (n_batch, 1, 1))
+        >>> c1 = torch.tile(torch.randn(n_state), (n_batch, 1))
+        >>> c2 = torch.tile(torch.zeros(n_state), (n_batch, 1))
+        >>> x_init = torch.randn(n_batch, n_state)
+        >>> 
+        >>> lti = pp.module.LTI(A, B, C, D, c1, c2)
+        >>> LQR = pp.module.LQR(lti, Q, p, T)
+        >>> x, u, cost = LQR(x_init)
+        >>> print("u = ", u)
+        x =  tensor([[[-0.2633, -0.3466,  2.3803, -0.0423],
+                      [ 0.1849, -1.3884,  1.0898, -1.6229],
+                      [ 1.2138, -0.7161,  0.2954, -0.6819],
+                      [ 1.4840, -1.1249, -1.0302,  0.9805],
+                      [-0.3477, -1.7063,  4.6494,  2.6780]],
+                     [[-0.9744,  0.4976,  0.0603, -0.5258],
+                      [-0.6356,  0.0539,  0.7264, -0.5048],
+                      [-0.2275, -0.1649,  0.3872, -0.4614],
+                      [ 0.2697, -0.3576,  0.0999, -0.4594],
+                      [ 0.3916, -2.0832,  0.0701, -0.5407]]])
+        u =  tensor([[[ 1.0405,  0.1586, -0.1282],
+                      [-1.4845, -0.5745,  0.2523],
+                      [-0.6322, -0.3281, -0.3620],
+                      [-1.6768,  2.4054, -0.1047],
+                      [-1.7948,  3.5269,  9.0703]],
+                     [[-0.1795,  0.9153,  1.7066],
+                      [ 0.0814,  0.4004,  0.7114],
+                      [ 0.0435,  0.5782,  1.0127],
+                      [-0.3017, -0.2897,  0.7251],
+                      [-0.0728,  0.7290, -0.3117]]])
     '''
-    # def __init__(self, system, constraint, Q, p, T):
     def __init__(self, sys=None, stage_cost=None, terminal_cost=None, cons=None, n_cons=0, init_traj=None):
-        r'''
-        Initialize three key classes
-        '''
         super().__init__()
         self.f_fn = sys
         self.p_fn = terminal_cost
@@ -364,6 +495,17 @@ class ddpOptimizer(nn.Module):
         return self
 
     def forward(self, fp_list):
+        r'''
+        Performs LQR for the linear system.
+
+        Args:
+            x_init (:obj:`Tensor`): The initial state of the system.
+
+        Returns:
+            List of :obj:`Tensor`: A list of tensors including the solved state sequence
+            :math:`\mathbf{x}`, the solved input sequence :math:`\mathbf{u}`, and the associated
+            quadratic costs :math:`\mathbf{c}` over the time horizon.
+        '''
         with torch.autograd.set_detect_anomaly(True): # for debug
             self.prepare(fp_list)
             self.infeas = False
