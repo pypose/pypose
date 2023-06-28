8a66d1dde1fb463051916e72c4789bcc4b0cf3dd
Huan Xu <hxu296@wisc.edu>
2023-06-28 04:34:20 +0000

    feat: address most comment

--- examples/sparse/bal_loader.py
+++ examples/sparse/bal_loader.py
@@ -1,58 +1,55 @@
-import os
+import torch, os, warnings
+import numpy as np
+import pypose as pp
 from functools import partial
 from operator import itemgetter, methodcaller
-from pathlib import Path
-from typing import Union
-
-import numpy as np
+from bs4 import BeautifulSoup, MarkupResemblesLocatorWarning
+from torchvision.transforms import Compose
 from scipy.spatial.transform import Rotation
 from torchdata.datapipes.iter import HttpReader, IterableWrapper, OnlineReader
-from torchvision.transforms import Compose
-import pypose as pp
-import torch
 
-from bs4 import BeautifulSoup, MarkupResemblesLocatorWarning
-import warnings
 # ignore bs4 warning
 warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
 
+# only export __all__
 __ALL__ = ['build_pipeline', 'read_bal_data', 'DATA_URL', 'ALL_DATASETS']
 
+# base url for the BAL dataset, used to download the problem files
 DATA_URL = 'https://grail.cs.washington.edu/projects/bal/'
+
+# all dataset names in the BAL dataset, used to check if the dataset name is valid
 ALL_DATASETS = ['ladybug', 'trafalgar', 'dubrovnik', 'venice', 'final']
 
-with_base_url = partial(os.path.join, DATA_URL)
+# helper for torchdata, add base url to the file name
+_with_base_url = partial(os.path.join, DATA_URL)
 
-def concat(a, b):
-    return a + b
-
-def endswith(s, b):
+# helper for torchdata, check if s ends with b
+def _endswith(s, b):
     return s.endswith(b)
 
-def not_none(s):
+# helper for torchdata, check if s is not None
+def _not_none(s):
     return s is not None
 
-def debug(s):
-    print(s)
-    return s
-
-def problem_lister(*problem_url):
-    # extract problem file urls from the problem url
+# extract problem file urls from the problem url
+def _problem_lister(*problem_url):
     return OnlineReader(IterableWrapper(problem_url)
     # parse HTML <a> tag's href attributes using bs4
     ).readlines(return_path=False
     ).map(partial(BeautifulSoup, features="html.parser")).map(methodcaller('find', 'a')
     # must end with .bz2
-    ).filter(not_none).map(methodcaller('get', 'href')).filter(partial(endswith, b='.bz2')
+    ).filter(_not_none).map(methodcaller('get', 'href')).filter(partial(_endswith, b='.bz2')
     # add base url
-    ).map(with_base_url)
+    ).map(_with_base_url)
 
-def download_pipe(cache_dir: Union[str, Path], url_dp, suffix: str):
+# download and decompress the problem files
+def _download_pipe(cache_dir, url_dp, suffix: str):
+    # cache compressed files
     cache_compressed = url_dp.on_disk_cache(
         filepath_fn=Compose([os.path.basename, partial(os.path.join, cache_dir)]) ,
     )
     cache_compressed = HttpReader(cache_compressed).end_caching(same_filepath_fn=True)
-    # decompress
+    # cache decompressed files
     cache_decompressed = cache_compressed.on_disk_cache(
         filepath_fn=Compose([partial(str.split, sep=suffix), itemgetter(0)]),
     )
@@ -64,7 +61,23 @@ def download_pipe(cache_dir: Union[str, Path], url_dp, suffix: str):
 def read_bal_data(file_name: str) -> dict:
     """
     Read a Bundle Adjustment in the Large dataset.
-    Ref: https://scipy-cookbook.readthedocs.io/items/bundle_adjustment.html
+
+    Referenced Scipy's BAL loader: https://scipy-cookbook.readthedocs.io/items/bundle_adjustment.html
+
+    According to BAL official documentation, each problem is provided as a text file in the following format:
+
+    <num_cameras> <num_points> <num_observations>
+    <camera_index_1> <point_index_1> <x_1> <y_1>
+    ...
+    <camera_index_num_observations> <point_index_num_observations> <x_num_observations> <y_num_observations>
+    <camera_1>
+    ...
+    <camera_num_cameras>
+    <point_1>
+    ...
+    <point_num_points>
+
+    Where, there camera and point indices start from 0. Each camera is a set of 9 parameters - R,t,f,k1 and k2. The rotation R is specified as a Rodrigues' vector.
 
     Parameters
     ----------
@@ -86,9 +99,9 @@ def read_bal_data(file_name: str) -> dict:
             contains initial estimates of point coordinates in the world frame.
         - points_2d: torch.Tensor (n_observations, 2)
             contains measured 2-D coordinates of points projected on images in each observations.
-        - camera_indices: torch.Tensor (n_observations,)
+        - camera_index_of_observations: torch.Tensor (n_observations,)
             contains indices of cameras (from 0 to n_cameras - 1) involved in each observation.
-        - point_indices: torch.Tensor (n_observations,)
+        - point_index_of_observations: torch.Tensor (n_observations,)
             contains indices of points (from 0 to n_points - 1) involved in each observation.
     """
     with open(file_name, "r") as file:
@@ -141,19 +154,53 @@ def read_bal_data(file_name: str) -> dict:
     camera_intrinsics[:, 2, 2] = 1
     camera_intrinsics = torch.from_numpy(camera_intrinsics)
 
-    return {'problem_name': os.path.basename(file_name).split('.')[0], # str
+    return {'problem_name': os.path.splitext(os.path.basename(file_name))[0], # str
             'camera_extrinsics': camera_extrinsics, # pp.LieTensor (n_cameras, 7)
             'camera_intrinsics': camera_intrinsics, # torch.Tensor (n_cameras, 3, 3)
             'points_3d': points_3d, # torch.Tensor (n_points, 3)
             'points_2d': points_2d, # torch.Tensor (n_observations, 2)
-            'camera_indices': camera_indices, # torch.Tensor (n_observations,)
-            'point_indices': point_indices, # torch.Tensor (n_observations,)
+            'camera_index_of_observations': camera_indices, # torch.Tensor (n_observations,)
+            'point_index_of_observations': point_indices, # torch.Tensor (n_observations,)
             }
 
 def build_pipeline(dataset='ladybug', cache_dir='bal_data'):
+    """
+    Build a pipeline for the Bundle Adjustment in the Large dataset.
+
+    Parameters
+    ----------
+    dataset : str, optional
+        The name of the dataset, by default 'ladybug'.
+        Must be one of ['ladybug', 'trafalgar', 'dubrovnik', 'venice', 'final'].
+    cache_dir : str, optional
+        The directory to cache the downloaded files, by default 'bal_data'.
+
+    Returns
+    -------
+    dp : torchdata.datapipes.IterableWrapper
+        The pipeline for the dataset.
+        In each iteration, return a dictionary containing the following fields:
+        - problem_name: str
+            The name of the problem.
+        - camera_extrinsics: pp.LieTensor (n_cameras, 7)
+            The camera extrinsics.
+            First three columns are translation, last four columns is unit quaternion.
+        - camera_intrinsics: torch.Tensor (n_cameras, 3, 3)
+            The camera intrinsics. Each camera is represented as a 3x3 K matrix.
+        - points_3d: torch.Tensor (n_points, 3)
+            contains initial estimates of point coordinates in the world frame.
+        - points_2d: torch.Tensor (n_observations, 2)
+            contains measured 2-D coordinates of points projected on images in each observations.
+        - camera_index_of_observations: torch.Tensor (n_observations,)
+            contains indices of cameras (from 0 to n_cameras - 1) involved in each observation.
+        - point_index_of_observations: torch.Tensor (n_observations,)
+            contains indices of points (from 0 to n_points - 1) involved in each observation.
+    """
+    global ALL_DATASETS
     assert dataset in ALL_DATASETS, f"dataset_name must be one of {ALL_DATASETS}"
-    url_dp = problem_lister(with_base_url(dataset + '.html'))
-    download_dp = download_pipe(cache_dir=cache_dir, url_dp=url_dp, suffix='.bz2')
+    print(f"Building pipeline for {dataset}...")
+    url_dp = _problem_lister(_with_base_url(dataset + '.html'))
+    download_dp = _download_pipe(cache_dir=cache_dir, url_dp=url_dp, suffix='.bz2')
     bal_data_dp = download_dp.map(read_bal_data)
     return bal_data_dp
 
