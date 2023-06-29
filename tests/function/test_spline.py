4de5545c75dd6c60d2cc4b3720154949f5b2ed5a
Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>
2023-06-29 14:14:04 +0800

    Update test_spline.py
    Signed-off-by: Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>

--- tests/function/test_spline.py
+++ tests/function/test_spline.py
@@ -6,23 +6,22 @@ class TestSpline:
     def test_bsplilne(self):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # test two poses
-        timeline = torch.arange(0, 1, 0.1, device=device)
         data = pp.randn_SE3(2,device=device)
-        poses = pp.bspline(data, timeline)
+        poses = pp.bspline(data, 10)
         torch.testing.assert_close(poses[...,[0,-1],:].translation(),
                                    data[...,[0,-1],:].translation(),atol=1e-4, rtol=1e-2)
         torch.testing.assert_close(poses[...,[0,-1],:].rotation(),
                                    data[...,[0,-1],:].rotation(),atol=1e-4, rtol=1e-2)
         # test for multi batch
         data = pp.randn_SE3(2,3, device=device)
-        poses = pp.bspline(data, timeline)
+        poses = pp.bspline(data, 10)
         torch.testing.assert_close(poses[...,[0,-1],:].translation(),
                                    data[...,[0,-1],:].translation(),atol=1e-4, rtol=1e-2)
         torch.testing.assert_close(poses[...,[0,-1],:].rotation(),
                                    data[...,[0,-1],:].rotation(),atol=1e-4, rtol=1e-2)
         # test for high dimension
         data = pp.randn_SE3(2,3,4, device=device)
-        poses = pp.bspline(data, timeline)
+        poses = pp.bspline(data, 10)
         torch.testing.assert_close(poses[...,[0,-1],:].translation(),
                                    data[...,[0,-1],:].translation(),atol=1e-4, rtol=1e-2)
         torch.testing.assert_close(poses[...,[0,-1],:].rotation(),
@@ -31,22 +30,22 @@ class TestSpline:
     def test_chspline(self):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # test for different point dimension
-        points = 10*torch.randn(1,2,3, device=device)
-        interpoints = pp.chspline(points, interval=0.1)
+        points = torch.randn(1,2,3, device=device)
+        interpoints = pp.chspline(points, 10)
         num = points.shape[-2]
         index = torch.arange(0, int(num/0.1), 1/0.1, device=device, dtype=torch.int64)
         po = interpoints.index_select(-2, index)
         assert (points - po).sum()<1e-5
         # test multi points
         points = torch.randn(20, 3, device=device)
-        interpoints = pp.chspline(points, interval=0.2)
+        interpoints = pp.chspline(points, 5)
         num = points.shape[-2]
         index = torch.arange(0, int(num/0.2), 1/0.2, device=device, dtype=torch.int64)
         po = interpoints.index_select(-2, index)
         assert (points - po).sum()<1e-5
         # test multi batches
         points = torch.randn(3,20,3, device=device)
-        interpoints = pp.chspline(points, interval=0.25)
+        interpoints = pp.chspline(points, 4)
         num = points.shape[-2]
         index = torch.arange(0, int(num/0.25), 1/0.25, device=device, dtype=torch.int64)
         po = interpoints.index_select(-2, index)
