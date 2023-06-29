151e12811ab592252cbfd8831f88124c644d6d04
Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>
2023-06-29 14:12:28 +0800

    Update spline.py
    use the number of points as input parameter
    
    Signed-off-by: Xiangyu Chen <89004488+XiangyuChen0212@users.noreply.github.com>

--- pypose/function/spline.py
+++ pypose/function/spline.py
@@ -2,7 +2,7 @@ import torch
 from . import is_SE3
 
 
-def chspline(points, interval=0.1):
+def chspline(points, num=10):
     r"""
     Cubic Hermite Spline, a piecewise-cubic interpolator matching values and first
     derivatives.
@@ -10,15 +10,21 @@ def chspline(points, interval=0.1):
     Args:
         points (:obj:`Tensor`): the sequence of points for interpolation with shape
             [..., point_num, dim].
-        interval (:obj:`Float`): the interval between adjacant interpolation points.
+        num (:obj:`int`): the number of interpolated points between adjcent two poses.
 
     Returns:
-       ``Tensor``: the interpolated points with [..., inter_points_num, dim] shape.
+       ``Tensor``: the interpolated points.
 
-    On the unit interval [0, 1], given a starting point :math:`p_0` at :math:`t = 0` and
-    an ending point :math:`p_1` at :math:`t = 1` with starting tangent :math:`m_0` at
-    :math:`t = 0` and ending tagent :math:`m_1` at :math:`t=1`, the polynomial can be
-    defined by
+    The interpolated points are evenly distributed between a start point and a end point
+    according to the number of interpolated points. In this function, the interpolation
+    time is aslo evenly distributed between the corresponding time of start point and
+    end point. For example, if 10 points are interpolated between the points at
+    :math:`t_0` and :math:`t_0+1`. The interpolation time is
+    :math:`t\in[t_0, t_0+0.1,...,t_0+0.9]`.
+
+    Denote the starting point :math:`p_0` with the starting tangent :math:`m_0` and the
+    ending point :math:`p_1` with the ending tagent :math:`m_1`. The interpolated point at
+    time :math:`t` can be defined as:
 
     .. math::
         \begin{aligned}
@@ -59,22 +65,24 @@ def chspline(points, interval=0.1):
         ...                         [5., 1., 0.2],
         ...                         [4., 0.75, 0.2],
         ...                         [5., 0., 0.]]])
-        >>> waypoints = pp.chspline(points)
+        >>> waypoints = pp.chspline(points, 10)
 
     .. figure:: /_static/img/module/liespline/CsplineR3.png
         :width: 600
 
         Fig. 1. Result of Cubic Spline Interpolation in 3D space.
     """
-    assert points.dim() >= 2, 'Dimension of points should be [..., N, C]'
-    assert 1/interval%1 == 0.0, '1 should be divisible by interval between points'
+    assert points.dim() >= 2, "Dimension of points should be [..., N, C]"
+    assert type(num)==int, "The type of interpolated point number should be int."
+    assert num!=0, "The number of interpolated point number is 0."
     batch, N = points.shape[:-2], points.shape[-2]
     dargs = {'device': points.device, 'dtype': points.dtype}
-    xs = torch.arange(0, N-1+interval, interval, **dargs)
+    interval = 1.0/num
+    timeline = interval*torch.arange(0, num*(N-1)+1, **dargs)
     x = torch.arange(N, **dargs)
-    idxs = torch.searchsorted(x[...,1:], xs[..., :])
+    idxs = torch.searchsorted(x[...,1:], timeline[..., :])
     x = x.expand(batch+(-1,))
-    xs = xs.expand(batch+(-1,))
+    xs = timeline.expand(batch+(-1,))
     m = (points[..., 1:, :] - points[..., :-1, :])
     m /= (x[..., 1:] - x[..., :-1])[..., None]
     m = torch.cat([m[...,[0],:], (m[..., 1:,:] + m[..., :-1,:]) / 2, m[...,[-1],:]], -2)
@@ -94,23 +102,23 @@ def chspline(points, interval=0.1):
     return interpoints
 
 
-def bspline(data, timeline):
+def bspline(data, num:int=10):
     r'''
     B-spline interpolation, which currently only support SE3 LieTensor.
 
     Args:
         data (:obj:`LieTensor`): the input sparse poses with
-            [batch_size, point_num, dim] shape.
-        timeline (:obj:`Tensor`): the time line to interpolate.
+            [batch_size, poses_num, dim] shape.
+        num (``int``): the number of interpolated poses between adjcent two poses.
 
     Returns:
-        ``LieTensor``: the interpolated SE3 LieTensor.
+        :obj:`LieTensor`: the interpolated SE3 LieTensor.
 
     A poses query at any time :math:`t \in [t_i, t_{i+1})` (i.e. a segment of the spline)
-    only relies on the poses located at time steps :math:`\{t_{i-1},t_i,t_{i+1},t_{i+2}\}`.
+    only relies on the poses at four time steps :math:`\{t_{i-1},t_i,t_{i+1},t_{i+2}\}`.
     It means that the interpolation between adjacent poses needs four consecutive poses.
-    Thus, the B-spline interpolation could estimate the pose between :math:`[t_1, t_{n-1}]`
-    by the input poses at :math:`\{t_0, ...,t_{n}\}`.
+    In this function, the interpolation time :math:`t` is evenly distributed between a
+    poses query according to the number of interpolated poses.
 
     The absolute pose of the spline :math:`T_{s}^w(t)`, where :math:`w` denotes the world
     and :math:`s` is the spline coordinate frame, can be calculated:
@@ -166,7 +174,6 @@ def bspline(data, timeline):
         >>> import torch, pypose as pp
         >>> a1 = pp.euler2SO3(torch.Tensor([0., 0., 0.]))
         >>> a2 = pp.euler2SO3(torch.Tensor([torch.pi / 4., torch.pi / 3., torch.pi / 2.]))
-        >>> timeline = torch.arange(0, 1, 0.25)
         >>> poses = pp.SE3([[[0., 4., 0., a1[0], a1[1], a1[2], a1[3]],
         ...                  [0., 3., 0., a1[0], a1[1], a1[2], a1[3]],
         ...                  [0., 2., 0., a1[0], a1[1], a1[2], a1[3]],
@@ -175,20 +182,23 @@ def bspline(data, timeline):
         ...                  [2., 0., 1., a2[0], a2[1], a2[2], a2[3]],
         ...                  [3., 0., 1., a2[0], a2[1], a2[2], a2[3]],
         ...                  [4., 0., 1., a2[0], a2[1], a2[2], a2[3]]]])
-        >>> wayposes = pp.bspline(poses, timeline)
+        >>> wayposes = pp.bspline(poses, 10)
 
-    .. figure:: /_static/img/module/liespline/BsplineSE3.png
+    - .. figure:: /_static/img/module/liespline/BsplineSE3.png
 
         Fig. 1. Result of B Spline Interpolation in SE3.
     '''
     assert is_SE3(data), "The input poses are not SE3Type."
-    assert data.dim()>=2, 'Dimension of data should be [..., N, C]'
+    assert data.dim()>=2, "Dimension of data should be [..., N, C]."
+    assert type(num)==int, "The type of interpolated pose number should be int."
+    assert num!=0, "The number of interpolated pose number is 0."
     batch = data.shape[:-2]
     data = torch.cat((data[..., :1, :].expand(batch+(2,-1)),
                       data,
                       data[..., -1:, :].expand(batch+(2,-1))), dim=-2)
-    Bth, N, D, K = data.shape[:-2], data.shape[-2], data.shape[-1], timeline.shape[-1]
-    dargs = {'dtype': timeline.dtype, 'device': timeline.device}
+    Bth, N, D = data.shape[:-2], data.shape[-2], data.shape[-1]
+    dargs = {'dtype': data.dtype, 'device': data.device}
+    timeline = torch.arange(0, 1, 1.0/num, **dargs)
     tt = timeline ** torch.arange(4, **dargs).view(-1, 1)
     B = torch.tensor([[5, 3,-3, 1],
                       [1, 3, 3,-2],
@@ -196,7 +206,7 @@ def bspline(data, timeline):
     dP = data[..., 0:-3, :].unsqueeze(-2)
     w = (B @ tt).unsqueeze(-1)
     index = (torch.arange(0, N-3).unsqueeze(-1) + torch.arange(0, 4)).view(-1)
-    P = data[..., index, :].view(Bth + (N-3, 4, 1, D))
-    A = ((P[..., :3,:, :].Inv() * P[..., 1:,:,:]).Log() * w).Exp()
+    P = data[..., index, :].view(Bth + (N-3,4,1,D))
+    A = ((P[..., :3,:, :].Inv() * P[...,1:,:,:]).Log() * w).Exp()
     A = A[...,0,:,:] * A[...,1,:,:] * A[...,2,:,:]
-    return (dP * A).view(Bth + (K * (N - 3), D))
+    return (dP * A).view(Bth + (num * (N - 3), D))
