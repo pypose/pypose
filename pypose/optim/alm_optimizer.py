6beab616efda2a6f0df7cb6357316a059d383a75
Ha1O7 <76957722+Ha1O7@users.noreply.github.com>
2023-07-27 01:32:02 +0800

    v3_added 3 tests and generalized algorithm

--- pypose/optim/alm_optimizer.py
+++ pypose/optim/alm_optimizer.py
@@ -1,33 +1,40 @@
-
 import torch
-import pypose
+import pypose as pp
+import math
 import numpy as np
 from torch import nn
-from .optimizer import _Optimizer
+from .optimizer import _Optimizer, RobustModel
+from .optimizer import GaussNewton as GN
 from .optimizer import LevenbergMarquardt as LM
+from .strategy import TrustRegion
 
-
+	### Updating largragian related parameters
 class _Unconstrained_Model(nn.Module):
-    def __init__(self, model, constraints, en_pf=None, kernel=None, enable_penalty=False):
-        super().__init__()  # Call the __init__() method of the parent class
+    def __init__(self, model, constraints, penalty_factor):
+        super().__init__()
         self.model = model
         self.constraints = constraints
-        self.pf = en_pf if en_pf else 0 
-        
+        self.pf = penalty_factor
+        self.lmd = self.lmd if hasattr(self, 'lmd') \
+                            else torch.zeros((self.constraints(input).shape[0], ))
+      
     def update_lambda(self, error):
         self.lmd += error * self.pf
         return self.lmd
         
-    def update_penalty_factor(self, pnf_update_step):
-        self.pf += pnf_update_step
-            
-    def forward(self, input, target=None):
-        R = self.model(input)
-        C = self.constraints(input)
-        self.lmd = self.lmd if hasattr(self, 'lmd') \
-                                    else torch.zeros_like(C) 
-        return R + self.lmd * C + self.pf * torch.norm(C) * torch.norm(C)
-
+    def update_penalty_factor(self, pnf_update_step, safe_guard):
+        pf = self.pf * pnf_update_step
+        self.pf = pf if pf < safe_guard else safe_guard
+        
+    def forward(self, input=None, target=None):
+        R = self.model(input).to(torch.float32)
+        trainable_params = list(self.model.parameters())
+        if pp.is_lietensor(trainable_params[0]):
+            R = torch.norm(R)
+        C = self.constraints(input).to(torch.float32)
+        penalty_term = torch.square(torch.norm(C))
+        L = R + (self.lmd @ C) + self.pf * penalty_term / 2
+        return L
 
 ############
     # Update Needed Parameters:
@@ -37,71 +44,98 @@ class _Unconstrained_Model(nn.Module):
     #	-> self.update_parameter(pg['params'], D)
     #   3. penalty factor(Optional): update_para * penalty factor
 class Augmented_Lagrangian_Algorithm(_Optimizer):
-    def __init__(self, model, constraints, unconstrained_optimizer=None, enable_penalty=False, penalty_safeguard=1e3, \
-                       convergence_tolerance_constrains=100, convergence_tolerance_model= 1e-3, min=1e-6, max=1e32,\
-                       update_tolerance=2, scheduler=None, penalty_factor=5, lm_rate=0.01, num_iter=10, kernel=None \
+    def __init__(self, model, constraints, unconstrained_optimizer=None, penalty_factor=1e-3, penalty_safeguard=1e9, \
+                       min=1e-6, max=1e32, scheduler=None,  num_iter=200, learning_rate=1e-3, clip_value=None,
                 ):        
         defaults = {**{'min':min, 'max':max}}
         super().__init__(model.parameters(), defaults=defaults)
         #### choose your own optimizer for unconstrained opt.
         self.unconstrained_optimizer =  unconstrained_optimizer if unconstrained_optimizer \
-        								else LM
+        								else Generalized_SGD 
         
+        ### Shared Augments
         self.model = model
         self.constraints = constraints
-        self.lm_rate = lm_rate
         self.num_iter = num_iter
-        self.enable_penalty = enable_penalty
-        self.convergence_tolerance_constrains = convergence_tolerance_constrains
-        self.convergence_tolerance_model = convergence_tolerance_model
-        self.update_tolerance = update_tolerance
-        self.penalty_safeguard = penalty_safeguard
+        self.lr = learning_rate
         self.penalty_factor = penalty_factor
-        self.aga_model = _Unconstrained_Model(self.model, self.constraints, en_pf=penalty_factor) 
+        self.alm_model = _Unconstrained_Model(self.model, self.constraints, penalty_factor=self.penalty_factor) 
+        
+        # algorithm implemented 
+        self.last_object_value = 1e2#self.alm_model.model(input=None)
+        self.object_value_decrease_tolerance = 1e-5
+        self.violation_tolerance = 1e-5
+        self.decrease_rate = 0.8
+        self.pf_rate = 2.0
+        self.pf_safeguard = 1e9
+        
+        # EXTRA, to avoid explosure
+        self.clip_value = clip_value
+        self.loss_bank_max = 100
+        self.loss_bank_tolerance = 1e-9
+        
     
     #### f(x) - y = loss_0, f(x) + C(x) - 0 - y 
-    @torch.no_grad()
-    def step(self, input, target=None, weight=None, strategy=None):
-            # convert to unconstraine problem
-            optim = self.unconstrained_optimizer(self.aga_model, strategy=strategy)
-            self.last = self.loss = self.loss if hasattr(self, 'loss') \
-                                    else optim.step(input)       
-            #  e.g. LM, it calculates the J of [f(x) - y] to update \theta
-            #  for AGA, it calculates the J of [f(x) - y + C - 0 + C_pf] to update \theta
-            # update model parameter \theta
-            for _ in range(self.num_iter):
-                self.loss = optim.step(input)
-            model_eror, error= self.model(input), self.constraints(input)
-            print(torch.norm(error), torch.norm(model_eror))
-            self.aga_model.update_lambda(error)
-            if torch.norm(error) < self.convergence_tolerance_constrains: 
-                pnf_update_step = min(self.penalty_safeguard, self.penalty_factor * self.update_tolerance)
-                self.aga_model.update_penalty_factor(pnf_update_step)
-            return self.loss
+    def step(self, input, target=None, weight=None, strategy=None):     
+        self.best_violation = self.best_violation if hasattr(self, 'best_violation') \
+        else torch.norm(self.alm_model.constraints(input=input))
         
-           
+        optim = self.unconstrained_optimizer(self.alm_model, learning_rate=self.lr, clip_value=self.clip_value)
+        self.last = self.loss = self.loss if hasattr(self, 'loss') \
+                                    else self.alm_model(input)  
+        self.loss_bank = []
+        for _ in range(self.num_iter):
+            self.loss = optim.step(input)
+            
+        violation = self.constraints(input)
+        with torch.no_grad():
+            self.log_generation(alm_model=self.alm_model, violation=violation,\
+             inputs=input, last_object_value=self.last_object_value)
+            
+        
+            if torch.norm(violation) <= torch.norm(self.best_violation) * self.decrease_rate:
+                if torch.norm(self.last_object_value-self.alm_model.model(input=input)) <= self.object_value_decrease_tolerance \
+                    and torch.norm(violation) <= self.violation_tolerance:
+                    print("found optimal")
+                    return self.loss 
+                    
+                self.alm_model.update_lambda(violation)
+                self.best_violation = violation
+            # if violation is not well satisfied, add further pubnishment
+            else:
+                self.alm_model.update_penalty_factor(self.pf_rate, self.pf_safeguard)
+            
+            self.last_object_value = self.alm_model.model(input)
+
+        return self.loss
+    
+    def log_generation(self, alm_model, violation, inputs, last_object_value):
+        print('--------------------NEW-ALM-EPOCH-------------------')
+        print('current_lambda: ', alm_model.lmd)
+        print('parameters: ', alm_model.model.parameters())
+        print('object_loss:', self.alm_model.model(inputs))
+        print('absolute violation:', torch.norm(violation))
+        print("object_loss_decrease", torch.norm(last_object_value-alm_model.model(inputs)))
+            
 class Generalized_SGD(_Optimizer):
-    def __init__(self, model, solver=None, strategy=None, kernel=None, corrector=None, \
-                       learning_rate=0.1, momentum=0.9, min=1e-6, max=1e32, vectorize=True):
-        assert min > 0, ValueError("min value has to be positive: {}".format(min))
-        assert max > 0, ValueError("max value has to be positive: {}".format(max))
+    def __init__(self, model, solver=None, strategy=None, kernel=None, corrector=None, clip_value=None, \
+                       learning_rate=1e-3, momentum=0.9, min=1e-6, max=1e32, vectorize=True):
         self.strategy = TrustRegion() if strategy is None else strategy
         defaults = {**{'min':min, 'max':max}, **self.strategy.defaults}
         super().__init__(model.parameters(), defaults=defaults) 
         self.momentum = momentum
         self.lr = learning_rate
-        self.model = RobustModel(model, kernel, auto=False)
+        self.model = model
+        self.clip_value = clip_value
+        print(self.lr)
+        self.sgd = torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.9)
 
     @torch.enable_grad()
-    def step(self, input, target=None, weight=None):        
-        for pg in self.param_groups:
-            self.last = self.loss = self.loss if hasattr(self, 'loss') \
-                                    else self.model.loss(input, target)
-            grad = torch.autograd.grad(self.loss, pg['params'])[0]
-            D = -self.lr * grad.reshape(-1,1)
-            self.last = self.loss if hasattr(self, 'loss') \
-                        else self.model.loss(input, target)
-            with torch.no_grad():
-                self.update_parameter(params=pg['params'], step=D)
-            self.loss = self.model.loss(input, target)
+    def step(self, input, target=None, weight=None):       
+        self.sgd.zero_grad()
+        self.loss = self.model(input)
+        self.loss.backward()
+        if self.clip_value:
+            torch.nn.utils.clip_grad_norm_(self.alm_model.parameters(), clip_value)
+        self.sgd.step()
         return self.loss
