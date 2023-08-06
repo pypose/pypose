fbefaba4548649900391562b48a6c49a001c0742
Huan Xu <hxu296@wisc.edu>
2023-08-06 19:17:58 +0000

    cleanup import and comment

--- pypose/func/__init__.py
+++ pypose/func/__init__.py
@@ -1,17 +1,12 @@
-from typing import Callable, Union, Tuple, List, Any, Optional
 import torch
-from functools import partial, wraps
-from torch._functorch.vmap import vmap, doesnt_support_saved_tensors_hooks, get_chunk_sizes
-from torch._functorch.eager_transforms import error_if_complex, _slice_argnums, \
-    _chunked_standard_basis_for_, _safe_zero_index, _vjp_with_argnums
-from torch.utils._pytree import tree_flatten, tree_unflatten, tree_map, tree_map_only
+from typing import Callable, Union, Tuple, Optional
 
 from pypose.lietensor.lietensor import retain_ltype
 
 
 def jacrev(func: Callable, argnums: Union[int, Tuple[int]] = 0, *, has_aux=False,
            chunk_size: Optional[int] = None,
-           _preallocate_and_copy=False):
+           _preallocate_and_copy=False) -> Callable:
     """
     This function provides the exact same functionality as `torch.func.jacrev`, except
     that it allows LieTensor to be used as input.
