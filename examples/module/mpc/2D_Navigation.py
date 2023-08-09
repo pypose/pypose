ef0db84e8be8938f15f0402855ed803fff35b20a
hakunanh <hannahh6069@gmail.com>
2023-08-09 19:55:02 -0400

    update weight mareix Q and visualization

--- examples/module/mpc/2D_Navigation.py
+++ examples/module/mpc/2D_Navigation.py
@@ -3,9 +3,6 @@ import matplotlib.pyplot as plt
 import pypose as pp
 
 class Simple2DNav(pp.module.NLS):
-    """
-    A simple 2D navigation model for testing MPPI on non-linear system
-    """
 
     def __init__(self,dt,length=1.0):
         super().__init__()
@@ -13,9 +10,7 @@ class Simple2DNav(pp.module.NLS):
         self._length=length
 
     def state_transition(self, state, input, t=None):
-        """
-        The simple 2D nav has state: (x, y, theta) and input: (v, omega)
-        """
+
         def dynamics(state, input):
             x, y, theta = state.squeeze().moveaxis(-1, 0)
             v, omega = input.squeeze().moveaxis(-1, 0)
@@ -36,32 +31,18 @@ class Simple2DNav(pp.module.NLS):
         return (state.squeeze() + torch.mul(f1 + 2 * f2 + 2 * f3 + f4, self._tau/6.0)).unsqueeze(0)
 
     def observation(self, state, input, t=None):
-        """
-        Returns:
-            [N x 3] Tensor of state (as the system is fully observable)
-        """
+
         return state
 
-def visualize(system, traj, controls, costs):
-    """
-    pyplot visualization of Simple2DNav for debugging
-    Args:
-        system: The Simple2DNav system
-        traj: [T x 3] Tensor of states to plot
-        controls: [T x 2] Tensor of controls to plot
-        costs: [T] List of costs to plot
-    Returns:
-        None
-    """
+def visualize(system, traj):
 
-    fig, axs = plt.subplots(1, 3, figsize=(18, 6))
+    """ fig, axs = plt.subplots(1, 2, figsize=(18, 6))
     plt.show(block=False)
 
     x = traj[:, 0]
     y = traj[:, 1]
     th = traj[:, 2]
 
-    #plot position
     for i in range(traj.shape[0]):
         for ax in axs:
             ax.cla()
@@ -69,23 +50,40 @@ def visualize(system, traj, controls, costs):
         axs[0].set_xlim(min(x)-1., max(x)+1.)
         axs[0].set_ylim(min(y)-1., max(y)+1.)
 
-        axs[0].plot(x[:i], y[:i], alpha=0.3, c='b')
-        axs[0].scatter(x[i], y[i], c='r', label='Current Position')
+        axs[0].set_xlabel('x(m)')
+        axs[0].set_ylabel('y(m)')
 
-        #plot states
+        axs[0].plot(x[:i], y[:i], alpha=0.3, c='b')
+        axs[0].scatter(1.5, 1.5, c='r', label='Goal Position')
+
+
         for j, (label, color) in enumerate(zip(['x', 'y', 'th'], ['r', 'g', 'b'])):
             axs[1].plot(traj[:i, j], label=label, c=color)
             axs[1].scatter(i, traj[i, j], c=color, marker='.')
 
-        # Plot cost
-        axs[2].plot(costs[:i], label='Cost', c='r')
-        #axs[2].scatter(i, costs[i], c='r', marker='.')
-
-
         for ax in axs:
             ax.legend()
 
-        plt.pause(system._tau)
+        plt.pause(system._tau) """
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    x = traj[:, 0]
+    y = traj[:, 1]
+
+    ax.plot(x, y, alpha=0.3, c='b')
+
+    ax.set_xlim(min(x)-1., max(x)+1.)
+    ax.set_ylim(min(y)-1., max(y)+1.)
+
+    ax.set_xlabel('x(m)')
+    ax.set_ylabel('y(m)')
+
+    ax.scatter(1.5, 1.5, color='red', label='Goal Position')
+    
+    ax.legend()
+
+    plt.show(block=False)
 
 
 
@@ -99,7 +97,7 @@ if __name__ == '__main__':
     n_state, n_ctrl = 3, 2
 
     Q = torch.tile(torch.eye(n_state + n_ctrl), (n_batch, T, 1, 1))
-    Q[...,0,0], Q[...,1,1], Q[...,2,2], Q[...,3,3], Q[...,4,4] = 1, 5, 0.1, 0.5, 0.05
+    Q[...,0,0], Q[...,1,1], Q[...,2,2], Q[...,3,3], Q[...,4,4] = 1, 6.55, 0.1, 0.5, 0.05
     #p = torch.tile(torch.ones(n_state + n_ctrl), (n_batch, T, 1))
     p = torch.tile(torch.zeros(n_state + n_ctrl), (n_batch, T, 1))
     dynamics=Simple2DNav(dt)
@@ -140,5 +138,7 @@ if __name__ == '__main__':
         X.append(xt.squeeze())
         U.append(ut_mpc.squeeze())
 
-    visualize(Simple2DNav(dt), torch.stack(X), torch.stack(U), costs)
+    print(xt)
+
+    visualize(Simple2DNav(dt), torch.stack(X))
     plt.show(block=True)
