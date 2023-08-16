708bdd69a845ad091561c21acc0be7f59155fcff
Zhaozhpe <zhaozhp2001@gmail.com>
2023-08-16 00:38:06 -0400

    version1

--- pypose/optim/__init__.py
+++ pypose/optim/__init__.py
@@ -3,6 +3,6 @@ from .optimizer import GaussNewton
 from .optimizer import GaussNewton as GN
 from .optimizer import LevenbergMarquardt
 from .optimizer import LevenbergMarquardt as LM
-from .alm_optimizer import AugmentedLagrangianMethod
-from .alm_optimizer import AugmentedLagrangianMethod as ALM
-from .alm_optimizer_SLO import AugmentedLagrangianMethod as ALM_SLO
+# from .alm_optimizer import AugmentedLagrangianMethod
+# from .alm_optimizer import AugmentedLagrangianMethod as ALM
+from .optimizer_alm import AugmentedLagrangianMethod as ALM
