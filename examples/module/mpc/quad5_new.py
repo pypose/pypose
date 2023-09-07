22ebb9f9cde99c049291993d0ee01a0d8b00fb59
hakunanh <hannahh6069@gmail.com>
2023-09-06 23:48:23 -0400

    update set up

--- examples/module/mpc/quad5_new.py
+++ examples/module/mpc/quad5_new.py
@@ -6,59 +6,83 @@ import numpy as np
 
 class Quad(pp.module.NLS):
 
-    def __init__(self, dt, J, mass=1.0, gravity=9.81):
+    def __init__(self, dt,
+                 J = torch.tensor([[0.1673, 0., 0.],
+                                   [0., 0.1673, 0.],
+                                   [0., 0., 0.2879]]), #inertia matrix
+                  mass=7.4270, gravity=9.81):
         super().__init__()
         self._tau = dt
         self._J = J
         self._m = mass
         self._g = gravity
 
+    def dstate_transition(self, state, input):
+        roll, pitch, yaw, p, q, r, u, v, w, x, y, z = state.squeeze().moveaxis(-1, 0)
+        #roll, pitch, yaw, p, q, r, vx, vy, vz, x, y, z = state.squeeze().moveaxis(-1, 0)
+        '''
+        [roll;pitch;yaw]: Euler angles, [ph;th;ps]
+        [p;q;r]: angular velocity in the body-fixed frame
+        [u;v;w]: velocity in the body frame
+        [vx;vy;vz]: velocity in the world frame
+        [x;y;x]: position in the world frame
+        Setting the Z-axis downward as the positive direction
+        '''
+        thrust, mx, my, mz = input.squeeze().moveaxis(-1, 0)
+        '''
+        thrust: total thrust of the quadrotor
+        mx, my, mz: torque in the world frame
+        '''
+        Ix, Iy, Iz = self._J[0,0], self._J[1,1], self._J[2,2]
+
+        s_ph = torch.sin(roll)
+        s_th = torch.sin(pitch)
+        s_ps = torch.sin(yaw)
+        c_ph = torch.cos(roll)
+        c_th = torch.cos(pitch)
+        c_ps = torch.cos(yaw)
+        t_th = torch.tan(pitch)
+
+        ph_Dot = p + r * (c_ph * t_th) + q * (s_ph * t_th)
+        th_Dot = q * c_ph - r * s_ph
+        ps_Dot = r * (c_ph/c_th) + q * (s_ph/c_th)
+        p_Dot = (Iy - Iz)/Ix * r * q + mx/Ix
+        q_Dot = (Iz - Ix)/Iy * p * r + my/Iy
+        r_Dot = (Ix - Iy)/Iz * p * q + mz/Iz
+        u_Dot = r * v - q * w - self._g * s_th
+        v_Dot = p * w - r * u + self._g * (s_ph * c_th)
+        w_Dot = q * u - p * v + self._g * (c_th * c_ph) - thrust/self._m
+        x_Dot = w * (s_ph * s_ps + c_ph * c_ps * s_th) - v * (c_ph * s_ps - c_ps * s_ph * s_th) + u * (c_ps * c_th)
+        y_Dot = v * (c_ph * c_ps + s_ph * s_ps * s_th) - w * (s_ph * c_ps - c_ph * s_ps * s_th) + u * (s_ps * c_th)
+        z_Dot = w * (c_ph * c_th) - u * s_th + v * (c_th * s_ph)
+        """ vx_Dot = - (thrust * (c_ph * s_th * c_ps + s_ph * s_ps)) / self._m
+        vy_Dot = (thrust * (s_ph * c_ps - c_ph * s_th * s_ps)) / self._m
+        vz_Dot = self._g - (thrust * (c_ph * c_th)) / self._m
+        x_Dot = vx
+        y_Dot = vy
+        z_Dot = vz """
+
+        _dstate = torch.stack((ph_Dot, th_Dot, ps_Dot, p_Dot, q_Dot, r_Dot, u_Dot, v_Dot, w_Dot, x_Dot, y_Dot, z_Dot), dim=-1)
+        return _dstate
+
+
     def state_transition(self, state, input, t=None):
 
-        def dynamics(state, input):
-            roll, pitch, yaw, wx, wy, wz, vx, vy, vz, x, y, z = state.squeeze().moveaxis(-1, 0)
-            thrust, mx, my, mz = input.squeeze().moveaxis(-1, 0)
-            Ix, Iy, Iz = self._J[0,0], self._J[1,1], self._J[2,2]
+        # _dstate = self.dstate_transition(state, input)
+        f1 = self.dstate_transition(state, input)
+        f2 = self.dstate_transition(state + 0.5 * self._tau * f1, input)
+        f3 = self.dstate_transition(state + 0.5 * self._tau * f2, input)
+        f4 = self.dstate_transition(state + self._tau * f3, input)
 
-            s_ph = torch.sin(roll)
-            s_th = torch.sin(pitch)
-            s_ps = torch.sin(yaw)
-            c_ph = torch.cos(roll)
-            c_th = torch.cos(pitch)
-            c_ps = torch.cos(yaw)
-            t_th = torch.tan(pitch)
+        _dstate = torch.mul(f1 + 2 * f2 + 2 * f3 + f4, 1/6.0)
 
-            r_Dot = wx + wz * (c_ph * t_th) + wy * (s_ph * t_th)
-            p_Dot = wy * c_ph - wz * s_ph
-            y_Dot = wz * (c_ph/c_th) + wy * (s_ph/c_th)
-            wx_Dot = (Iy - Iz)/Ix * wz * wy + mx/Ix
-            wy_Dot = (Iz - Ix)/Iy * wx * wz + my/Iy
-            wz_Dot = (Ix - Iy)/Iz * wx * wy + mz/Iz
-            vx_Dot = wz * vy - wy * vz - self._g * s_th
-            vy_Dot = wx * vz - wz * vx + self._g * (s_ph * c_th)
-            vz_Dot = wy * vx - wx * vy + self._g * (c_th * c_ph) - thrust/self._m
-            xDot = vz * (s_ph * s_ps + c_ph * c_ps * s_th) - vy * (c_ph * s_ps - c_ps * s_ph * s_th) + vx * (c_ps * c_th)
-            yDot = vy * (c_ph * c_ps + s_ph * s_ps * s_th) - vz * (s_ph * c_ps - c_ph * s_ps * s_th) + vx * (s_ps * c_th)
-            zDot = vz * (c_ph * c_th) - vx * s_th + vy * (c_th * s_ph)
-
-            #_dstate = torch.stack((xDot, yDot, zDot, r_Dot, p_Dot, y_Dot, vx_Dot, vy_Dot, vz_Dot, wx_Dot, wy_Dot, wz_Dot), dim=-1)
-            _dstate = torch.stack((r_Dot, p_Dot, y_Dot, wx_Dot, wy_Dot, wz_Dot, vx_Dot, vy_Dot, vz_Dot, xDot, yDot, zDot), dim=-1)
-
-            return _dstate
-
-        f1 = dynamics(state, input)
-        f2 = dynamics(state + 0.5 * self._tau * f1, input)
-        f3 = dynamics(state + 0.5 * self._tau * f2, input)
-        f4 = dynamics(state + self._tau * f3, input)
-
-        return (state.squeeze() + torch.mul(f1 + 2 * f2 + 2 * f3 + f4, self._tau/6.0)).unsqueeze(0)
+        return (state.squeeze() + torch.mul(_dstate, self._tau)).unsqueeze(0)
 
     def observation(self, state, input, t=None):
         return state
 
 
 def visualize(system, traj, controls):
-
 
     fig, axs = plt.subplots(4, 3, figsize=(18, 6))
 
@@ -130,7 +154,7 @@ def visualize(system, traj, controls):
 
     plt.tight_layout()
 
-    """ fig.suptitle('states', fontsize=16)
+    fig.suptitle('states', fontsize=16)
 
     fig, axs = plt.subplots(2, 2, figsize=(18, 6))
 
@@ -144,7 +168,7 @@ def visualize(system, traj, controls):
     axs[1, 0].set_title('my')
     axs[1, 1].set_title('mz')
 
-    fig.suptitle('inputs', fontsize=16) """
+    fig.suptitle('inputs', fontsize=16)
 
     """ fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
@@ -171,7 +195,7 @@ def visualize(system, traj, controls):
 
 if __name__ == '__main__':
 
-    dt = 0.001
+    dt = 0.02
     T = 30
     n_batch = 1
     n_state, n_ctrl = 12, 4
@@ -180,18 +204,14 @@ if __name__ == '__main__':
     x_goal = torch.tensor([[0., 0., 0., 0., 0., 0.,
                             0., 0., 0., 0, 0., 0.]], requires_grad=False)
 
-    u_init = torch.tile(torch.tensor([9.81, 0.0, 0.0, 0.0]), (n_batch, T, 1))
-
-    J = torch.tensor([[0.01466, 0., 0.],
-                      [0., 0.01466, 0.],
-                      [0., 0., 0.02848]])
+    u_init = torch.tile(torch.tensor([9.81*7.4270, 0.0, 0.0, 0.0]), (n_batch, T, 1))
 
     Q = torch.tile(torch.eye(n_state + n_ctrl), (n_batch, T, 1, 1))
     Q[...,0,0], Q[...,1,1], Q[...,2,2] = 10000000, 1000000, 10000000
     Q[...,12,12], Q[...,13,13], Q[...,14,14], Q[...,15,15] = 0.01, 0.05, 0.05, 0.05
     p = torch.tile(torch.zeros(n_state + n_ctrl), (n_batch, T, 1))
-    dynamics=Quad(dt, J)
-    stepper = pp.utils.ReduceToBason(steps=3, verbose=False)
+    dynamics=Quad(dt)
+    stepper = pp.utils.ReduceToBason(steps=5, verbose=False)
     MPC = pp.module.MPC(dynamics, Q, p, T, stepper=stepper)
 
     N = 20
@@ -208,10 +228,14 @@ if __name__ == '__main__':
         ut_mpc = u_mpc[...,0,:]
         #print(ut_mpc)
         xt = dynamics.forward(xt, ut_mpc)[0]
+        print(xt)
+        u_new = u_mpc[...,1:,:]
+        u_last = u_mpc[...,-1,:]
+        u_init = torch.cat((u_new, u_last.unsqueeze(0)), dim=1) #u shift, not necessary
         X.append(xt.squeeze())
         U.append(ut_mpc.squeeze())
 
-    print(xt)
+    #print(xt)
 
     visualize(dynamics, torch.stack(X), torch.stack(U))
     plt.show(block=True)
