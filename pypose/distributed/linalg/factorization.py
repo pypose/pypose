abf123e7ec60b911927869e135972d0084b92e53
xiaohaitao <756745172@qq.com>
2023-11-02 18:45:47 +0800

    1. init calu factorization

--- pypose/distributed/linalg/factorization.py
+++ pypose/distributed/linalg/factorization.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+from datetime import timedelta
+import torch.distributed._functional_collectives as funcol
 import torch.distributed as dist
 from torch.distributed._tensor import Shard,  distribute_tensor
 
@@ -8,7 +10,71 @@ def get_neighbor(i, s):
         return i + 2 ** s
     else:
         return i - 2 ** s
+def get_null_id(mesh_array, find_ranks): #获取空通信ranks
+  result = []
+  for idx in mesh_array.flatten():
+    if idx not in find_ranks:
+      result.append(idx)
+  return result
 
+def get_panel_group(mesh_array,current_rank, method = 0): #计算panel的进程
+  row_ranks, col_ranks, null_ranks = [], [], []
+  row_grp, col_grp, null_grp = None, None, None
+  mesh_shape = mesh_array.shape
+
+  if method == 0:
+    for row_loop in range(current_rank + 1, mesh_shape[0]):
+      row_ranks.append(mesh_array[current_rank, row_loop])
+      col_ranks.append(mesh_array[row_loop, current_rank])
+
+  else:
+    for row_loop in range(current_rank + 1):
+      row_ranks.append(mesh_array[current_rank, row_loop])
+      col_ranks.append(mesh_array[row_loop, current_rank])
+
+  null_ranks = get_null_id(mesh_array, row_ranks + col_ranks)
+
+  return row_ranks, col_ranks, null_ranks
+
+def get_panel_broad_rank(mesh_array, rank,method = 0): #获取braodcast ranks
+
+  result,null_result = [], []
+  x, y = np.argwhere(mesh_array == rank)[0]
+  if method == 0:
+    for i in range(x + 1,mesh_array.ndim):
+      result.append(mesh_array[i,y])
+    if len(result) == 0:
+      result = [mesh_array[0,y]]
+  else:
+
+    for i in range(y + 1,len(mesh_array)):
+      result.append(mesh_array[x,i])
+
+    if len(result) == 0:
+        result = [mesh_array[x,0]]
+
+  null_ranks = get_null_id(mesh_array, result)
+  return result, null_ranks
+
+
+def update_row_panel(l, local_data):
+
+  return torch.linalg.solve(l, local_data) #行块更新
+
+def update_col_panel(u, local_data):
+
+  return torch.linalg.solve(u, local_data, left = False) #列块更新
+
+def update_diagblock(local_data, list_l, list_u):
+  for i in range(len(list_l)):
+    local_data -= list_l[i] @ list_u[i] #对角块更新
+  return local_data
+def find_index(grp_ranks, input):
+  list1 = []
+  for item in input[:len(input) - 1]:
+    list1.append(np.argwhere(grp_ranks == item)[0])
+
+  return list1
 
 def block_diag(m):  # 需要异构块支持奇数个设备
     """
@@ -90,7 +156,8 @@ class Dfactori:
                 return list_L, U, list_s
 
         return list_L, U, list_s
-
+    def calu(self):
+        return
     def factorization(self, input, device_mesh, method=tslu):
         r'''
         Returns multiplication for tensor.
