244960801a9182db85ba79ced57449f5cf1fbcb5
zitongzhan <zzhan22@wisc.edu>
2023-12-05 00:08:10 -0500

    rename

--- pypose/lietensor/basics.py
+++ pypose/lietensor/basics.py
@@ -156,7 +156,7 @@ def bsr_bsc_matmul(bsr:torch.Tensor, bsc:torch.Tensor):
     assert dense_p * sparse_p == p
 
     result_step: int = 0
-    result_indices: List[int] = list()
+    coo_indices: List[int] = list()
     index: List[int] = list()
     source: List[int] = list()
     for i in range(sparse_m):
@@ -173,22 +173,22 @@ def bsr_bsc_matmul(bsr:torch.Tensor, bsc:torch.Tensor):
                     nz = True
             if nz:
                 result_step += 1
-                result_indices.append(i)
-                result_indices.append(j)
+                coo_indices.append(i)
+                coo_indices.append(j)
     source = torch.tensor(source, dtype=idx_dtype, device=bsr.device).view(-1, 2)
     index = torch.tensor(index, dtype=idx_dtype, device=bsr.device)
     prod = torch.bmm(csr_values[source[:, 0]], csc_values[source[:, 1]])
     values_shape = (result_step, dense_m, dense_p)
     reduced = torch.zeros(values_shape, dtype=prod.dtype, device=prod.device)
     reduced.scatter_add_(0, index.unsqueeze(-1).unsqueeze(-1).expand_as(prod), prod)
-    result_indices = torch.tensor(result_indices, dtype=idx_dtype, device=bsr.device)
-    result_indices = result_indices.view(-1, 2).T
+    coo_indices = torch.tensor(coo_indices, dtype=idx_dtype, device=bsr.device)
+    coo_indices = coo_indices.view(-1, 2).T
     # return torch.sparse_coo_tensor(indices=result_indices,
     #                                values=reduced,
     #                                size=(sparse_m, sparse_p, dense_m, dense_p)).coalesce()
     # use fake coo
-    dummy_val = torch.zeros(result_indices.shape[-1], dtype=prod.dtype, device=prod.device)
-    dummy = torch.sparse_coo_tensor(indices=result_indices,
+    dummy_val = torch.zeros(coo_indices.shape[-1], dtype=prod.dtype, device=prod.device)
+    dummy = torch.sparse_coo_tensor(indices=coo_indices,
                                     values=dummy_val,
                                     size=(sparse_m, sparse_p)).coalesce()
     dummy_csr = dummy.to_sparse_csr()
