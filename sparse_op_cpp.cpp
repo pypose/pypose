478845d7d7322e48f7ec3aacde70da149a949729
Zitong M1MBA <zitongz3@illinois.edu>
2023-12-28 12:25:23 -0600

    amend

--- sparse_op_cpp.cpp
+++ sparse_op_cpp.cpp
@@ -17,14 +17,16 @@ torch::Tensor sparse_bsr_mm(const torch::Tensor& a, const torch::Tensor& b) {
   return at::native::_sparse_csr_mm(a, b);
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("forward", &sparse_bsr_mm, "spgemv forward");
-}
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {}
 
 // https://pytorch.org/cppdocs/api/classtorch_1_1_library.html
 TORCH_LIBRARY_IMPL(aten, SparseCsrCPU, m) {
   auto my_module = pybind11::module::import("pypose.sparse.ops");
   auto script_function = my_module.attr("bsr_bsc_matmul").cast<torch::jit::StrongFunctionPtr>();
   _sparse_bsr_bsc_matmul = script_function.function_;
+  m.impl("mm", sparse_bsr_mm);
+}
+
+TORCH_LIBRARY_IMPL(aten, SparseCsrCUDA, m) {
   m.impl("mm", sparse_bsr_mm);
 }
